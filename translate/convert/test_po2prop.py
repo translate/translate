07c0c949e935173a9e11e8b9ea5ff454964132e1
Dwayne Bailey <dwayne@translate.org.za>
2006-08-28 07:06:15 +0000

    Test to ensure that we preserve space around the equal signs from the template properties file in the new output properties file.
    svn path=/src/trunk/; revision=3807

---
+++ translate/convert/test_po2prop.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from translate.convert import po2prop
+from translate.convert import prop2po
+from translate.convert import test_convert
+from translate.misc import wStringIO
+from translate.storage import po
+from translate.storage import properties
+from py import test
+
+class TestPO2Prop:
+    def po2prop(self, posource):
+        """helper that converts po source to .properties source without requiring files"""
+        inputfile = wStringIO.StringIO(posource)
+        inputpo = po.pofile(inputfile)
+        convertor = po2prop.po2prop()
+        outputprop = convertor.convertfile(inputpo)
+        return outputprop
+
+    def merge2prop(self, propsource, posource):
+        """helper that merges po translations to .properties source without requiring files"""
+        inputfile = wStringIO.StringIO(posource)
+        inputpo = po.pofile(inputfile)
+        templatefile = wStringIO.StringIO(propsource)
+        #templateprop = properties.propfile(templatefile)
+        convertor = po2prop.reprop(templatefile)
+        outputprop = convertor.convertfile(inputpo)
+        print outputprop
+        return outputprop
+
+    def test_hard_newlines_preserved(self):
+        """check that we preserver hard coded newlines at the start and end of sentence"""
+        posource = '''#: prop\nmsgid "\\nvalue\\n\\n"\nmsgstr "\\nwaarde\\n\\n"\n'''
+        proptemplate = '''prop=\\nvalue\\n\\n\n'''
+        propexpected = '''prop=\\nwaarde\\n\\n\n'''
+        propfile = self.merge2prop(proptemplate, posource)
+        print propfile
+        assert propfile == [propexpected]
+
+    def test_space_preservation(self):
+        """check that we preserve any spacing in properties files when merging"""
+        posource = '''#: prop\nmsgid "value"\nmsgstr "waarde"\n'''
+        proptemplate = '''prop  =  value\n'''
+        propexpected = '''prop  =  waarde\n'''
+        propfile = self.merge2prop(proptemplate, posource)
+        print propfile
+        assert propfile == [propexpected]
+
+class TestPO2PropCommand(test_convert.TestConvertCommand, TestPO2Prop):
+    """Tests running actual po2prop commands on files"""
+    convertmodule = po2prop
+    defaultoptions = {"progress": "none"}
+
+    def test_help(self):
+        """tests getting help"""
+        options = test_convert.TestConvertCommand.test_help(self)
+        options = self.help_check(options, "-tTEMPLATE, --template=TEMPLATE")
+        options = self.help_check(options, "--fuzzy")
+        options = self.help_check(options, "--nofuzzy", last=True)
+
