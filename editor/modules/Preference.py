084d2b11c8405f2030063f8f76b19d04c70b5351
Hok Kakada <hokkakada@khmeros.info>
2006-10-18 04:46:00 +0000

    commit to update font while clicking on Cancel
    svn path=/src/trunk/; revision=4085

--- editor/modules/Preference.py
+++ editor/modules/Preference.py
@@ -30,15 +30,16 @@ class Preference(QtGui.QDialog):
         self.ui = Ui_frmPreference()
         self.ui.setupUi(self)  
         self.setModal(True)        
+        self.default = False
 
         #Personal Setting
         settings = QtCore.QSettings("WordForge", "Translation Editor")
         self.ui.UserName.setPlainText(settings.value("UserName").toString())
         self.ui.EmailAddress.setPlainText(settings.value("EmailAddress").toString())
-        self.ui.FullLanguage.setPlainText(settings.value("FullLanguage").toString())
-        self.ui.Code.setPlainText(settings.value("Code").toString())
+        self.ui.cbxFullLanguage.setEditText(settings.value("FullLanguage").toString())
+        self.ui.cbxLanguageCode.setEditText(settings.value("Code").toString())
         self.ui.SupportTeam.setPlainText(settings.value("SupportTeam").toString())
-        self.ui.TimeZone.setPlainText(settings.value("TimeZone").toString())      
+        self.ui.cbxTimeZone.setEditText(settings.value("TimeZone").toString())      
         
        #Font Setting
        
@@ -46,17 +47,22 @@ class Preference(QtGui.QDialog):
         self.connect(self.ui.bntSource, QtCore.SIGNAL("clicked()"), self.fontSource) 
         self.connect(self.ui.bntTarget, QtCore.SIGNAL("clicked()"), self.fontTarget) 
         self.connect(self.ui.bntComment, QtCore.SIGNAL("clicked()"), self.fontComment) 
+        self.connect(self.ui.bntDefaults, QtCore.SIGNAL("clicked()"), self.defaultFonts) 
         
         self.connect(self.ui.okButton, QtCore.SIGNAL("clicked()"), self.accepted)
-##self.operator.emitSetFont
-##        if not profile.isEmpty():
-##            profile = ""
-##        self.ui.textEdit1.setPlainText(unicode(profile))
+##        self.connect(self.ui.cancelButton, QtCore.SIGNAL("clicked()"), self.rejected)
+
         # set font modify as false
         self.overviewFont = None
         self.tuSourceFont = None
         self.tuTargetFont = None
         self.commentFont = None
+        
+##    def rejected(self):
+##        "remove all settings"
+##        print "remove"
+##        settings = QtCore.QSettings("WordForge", "Translation Editor")
+##        settings.remove("")
         
     def initFonts(self):
         # get font for display
@@ -79,16 +85,44 @@ class Preference(QtGui.QDialog):
         self.tuTargetFont = None
         self.commentFont = None
         
+        
     def accepted(self):
+        #personal setting
+        self.getUserProfile()
+        
+        # font setting
+        if self.default:
+            self.default = False
+            self.overviewFont = self.tuSourceFont = self.tuTargetFont = self.commentFont = QtGui.QFont("Serif", 10)
+                        
         if (self.overviewFont):
             self.emit(QtCore.SIGNAL("fontChanged"), "overview" , self.overviewFont)
+            print self.overviewFont.family()
         if (self.tuSourceFont):
             self.emit(QtCore.SIGNAL("fontChanged"), "tuSource" , self.tuSourceFont)
         if (self.tuTargetFont):
             self.emit(QtCore.SIGNAL("fontChanged"), "tuTarget" , self.tuTargetFont)
         if (self.commentFont):
             self.emit(QtCore.SIGNAL("fontChanged"), "comment" , self.commentFont)
-    
+        # unless the ok button on dialog is pressed, save all configuration.
+        self.rememberChanges("overview", self.overviewFont)
+        self.rememberChanges("tuSource", self.tuSourceFont)
+        self.rememberChanges("tuTarget", self.tuTargetFont)
+        self.rememberChanges("comment", self.commentFont)
+   
+    def rememberChanges(self, obj, fontObj):
+        """input obj as string"""        
+        # store font settings
+        if (fontObj != None):    
+            fontFamily = fontObj.family()
+            fontSize = fontObj.pointSize()
+            currentFont = fontFamily +",  "+ str(fontSize)
+            settings = QtCore.QSettings("WordForge", "Translation Editor")
+            settings.setValue(str(obj + "FontFamily"), QtCore.QVariant(fontFamily))
+            settings.setValue(str(obj + "FontSize"), QtCore.QVariant(fontSize))
+            settings.setValue(str(obj + "CurrentFont"), QtCore.QVariant(currentFont))
+            
+        
     def fontOverview(self):
         self.overviewFont, fontCaption = self.setFont("overview")
         self.ui.lblOverView.setText(fontCaption)
@@ -107,14 +141,26 @@ class Preference(QtGui.QDialog):
         settings = QtCore.QSettings("WordForge", "Translation Editor")
         fontFamily = settings.value(str(obj + "FontFamily")).toString()        
         fontSize = settings.value(str(obj + "FontSize")).toString()
+        currentFont = settings.value(str(obj + "CurrentFont")).toString()
         if (fontSize):
             fontSize = int(fontSize)
         else:
             fontSize = 10
         font = QtGui.QFont(fontFamily, fontSize)
-        currentFont = fontFamily +",  "+ str(fontSize)
+##        currentFont = fontFamily +",  "+ str(fontSize)
         return [font, currentFont]
     
+    def defaultFonts(self):
+        """Set default fonts"""
+        self.ui.lblOverView.setText("Serif, 10")        
+        self.ui.lblSource.setText("Serif, 10")        
+        self.ui.lblTarget.setText("Serif, 10")        
+        self.ui.lblComment.setText("Serif, 10")   
+        self.default = True
+        return self.default
+
+        
+        
     def setFont(self, obj):
         """set font to other widgets, return current font name and size"""
         #get font settings
@@ -125,42 +171,37 @@ class Preference(QtGui.QDialog):
         if (newFont[1]):
             fontFamily = newFont[0].family()
             fontSize = newFont[0].pointSize()
-            # store font settings
-            settings = QtCore.QSettings("WordForge", "Translation Editor")
-            settings.setValue(str(obj + "FontFamily"), QtCore.QVariant(fontFamily))
-            settings.setValue(str(obj + "FontSize"), QtCore.QVariant(fontSize))
             self.fontChanged = True
         currentFont = fontFamily +",  "+ str(fontSize)
+        self.rememberChanges("overview", newFont[0])
+        self.rememberChanges("tuSource", newFont[0])
+        self.rememberChanges("tuTarget", newFont[0])
+        self.rememberChanges("comment", newFont[0])
         return [newFont[0], currentFont]
-
+      
         
-    def getText(self):        
-        UserName = str(self.ui.UserName.toPlainText())
-        EmailAddress = str(self.ui.EmailAddress.toPlainText())
-        FullLanguage = str(self.ui.FullLanguage.toPlainText())
-        Code = str(self.ui.Code.toPlainText())
-        SupportTeam = str(self.ui.SupportTeam.toPlainText())
-        TimeZone = str(self.ui.TimeZone.toPlainText())        
+    def getUserProfile(self):  
+        """emit updateProfile signal and return userprofile as dictionary"""
+##        UserName = str(self.ui.UserName.toPlainText())
+##        EmailAddress = str(self.ui.EmailAddress.toPlainText())
+####        FullLanguage = str(self.ui.FullLanguage.toPlainText())
+##        Code = str(self.ui.Code.toPlainText())
+##        SupportTeam = str(self.ui.SupportTeam.toPlainText())
+##        TimeZone = str(self.ui.TimeZone.toPlainText())
+##        
+##        settings = QtCore.QSettings("WordForge", "Translation Editor")
+##        settings.setValue("UserName",QtCore.QVariant(UserName))
+##        settings.setValue("EmailAddress",QtCore.QVariant(EmailAddress))
+##        settings.setValue("FullLanguage",QtCore.QVariant(FullLanguage))
+##        settings.setValue("Code",QtCore.QVariant(Code))
+##        settings.setValue("SupportTeam",QtCore.QVariant(SupportTeam))
+##        settings.setValue("TimeZone",QtCore.QVariant(TimeZone))
         
-        settings = QtCore.QSettings("WordForge", "Translation Editor")
-        settings.setValue("UserName",QtCore.QVariant(UserName))
-        settings.setValue("EmailAddress",QtCore.QVariant(EmailAddress))
-        settings.setValue("FullLanguage",QtCore.QVariant(FullLanguage))
-        settings.setValue("Code",QtCore.QVariant(Code))
-        settings.setValue("SupportTeam",QtCore.QVariant(SupportTeam))
-        settings.setValue("TimeZone",QtCore.QVariant(TimeZone))
-        
-##        data = QtGui.QInputDialog.getText("QInputDialog.getText()"), self.tr("UserName:"),
-##                                            self.tr("Your email address:"),
-##                                            self.tr("Full Language Name:"),
-##                                            self.tr("Language Code:"),
-##                                            self.tr("Support Team:"),
-##                                            self.tr("Time Zone:"), 
-##                                            self.tr("Number of Singular/Plular Forms:"), QtGui.QLineEdit.Normal,
-##                                            QtCore.QDir.home().dirName())
-##        if datavaule and not text.isEmpty():
-##            self.textLabel.setText(text)
-##                self.emitUserprofile()
+##        userprofile = "UserName:" + UserName + "\nEmailAddress:" + EmailAddress + "\nFullLanguage:" + FullLanguage + "\nCode:" + Code + "\nSupportTeam:" + SupportTeam + "\nTimeZone:" + TimeZone +"\n"
+##        userprofile = dictutils.cidict()
+##        userprofile = {"charset":"CHARSET", "encoding":"ENCODING", "project_id_version":None, "pot_creation_date":None, "po_revision_date":None, "last_translator":UserName, "language_team":Code, "mime_version":None, "plural_forms":None, "report_msgid_bugs_to":SupportTeam}
+        userprofile = {"charset":"CHARSET", "encoding":"ENCODING", "project_id_version":None, "pot_creation_date":None, "po_revision_date":None, "last_translator":"Hok Kakada", "language_team":"Khmer", "mime_version":None, "plural_forms":None, "report_msgid_bugs_to": "support@khmeros.info"}
+        self.emit(QtCore.SIGNAL("updateProfile"), userprofile)
         
     def emitUserprofile(self, filename):
         self.store = factory.getobject(fileName)
