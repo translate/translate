aa8410f319a214496a9bac0f56d5ff3a0f5e9a0d
Hok Kakada <hokkakada@khmeros.info>
2006-11-02 08:05:21 +0000

    rename module comment
    svn path=/src/trunk/; revision=4166

---
+++ editor/ui/Ui_Comment.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'unknown'
+#
+# Created: Thu Oct  5 14:54:03 2006
+#      by: PyQt4 UI code generator 4.0
+#
+# WARNING! All changes made in this file will be lost!
+
+import sys
+from PyQt4 import QtCore, QtGui
+
+class Ui_frmComment(object):
+    def setupUi(self, frmComment):
+        frmComment.setObjectName("frmComment")
+        frmComment.resize(QtCore.QSize(QtCore.QRect(0,0,534,395).size()).expandedTo(frmComment.minimumSizeHint()))
+
+        self.gridlayout = QtGui.QGridLayout(frmComment)
+        self.gridlayout.setMargin(2)
+        self.gridlayout.setSpacing(6)
+        self.gridlayout.setObjectName("gridlayout")
+
+        self.txtComment = QtGui.QTextEdit(frmComment)
+        self.txtComment.setEnabled(False)
+        self.txtComment.setObjectName("txtComment")
+        self.gridlayout.addWidget(self.txtComment,0,0,1,1)
+
+        self.retranslateUi(frmComment)
+        QtCore.QMetaObject.connectSlotsByName(frmComment)
+
+    def tr(self, string):
+        return QtGui.QApplication.translate("frmComment", string, None, QtGui.QApplication.UnicodeUTF8)
+
+    def retranslateUi(self, frmComment):
+        frmComment.setWindowTitle(self.tr("Comments"))
