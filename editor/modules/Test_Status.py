88df1a8822e7340dc15ece083b8886e7abe69398
Keo Sophon <keosophon@khmeros.info>
2006-12-08 11:03:50 +0000

    update code of test status to a better one.
    svn path=/src/trunk/; revision=4461

--- editor/modules/Test_Status.py
+++ editor/modules/Test_Status.py
@@ -1,46 +1,56 @@
-from translate.storage import factory
-from translate.tools import pocount
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#
+
 import sys
 import os.path
 sys.path.append(os.path.join(sys.path[0], ".."))
+from translate.tools import pocount
+from translate.misc import wStringIO
 from Status import *
+from translate.storage import po
 import unittest
 import World
 
 class TestStatus(unittest.TestCase):
 
     def setUp(self):
-        self.store = factory.getobject("../example/webarchiver.po")
-        units = self.store.units
+        unit = '''# aaaaa
+#: kfaximage.cpp:189
+#, fuzzy
+msgid "Unable to open file for reading."
+msgstr "unable to read file"
+'''
+        self.pofile = self.poparse(unit)
+        units = self.pofile.units
         self.total = len(units)
         self.fuzzy = len(pocount.fuzzymessages(units))
         self.translated = len(pocount.translatedmessages(units))
         self.untranslated = self.total - self.translated
-        self.status = Status(self.store.units)
+        self.status = Status(self.pofile.units)
+
+    def poparse(self, posource):
+        """helper that parses po source without requiring files"""
+        dummyfile = wStringIO.StringIO(posource)
+        return po.pofile(dummyfile)
     
     def testAddNumFuzzy(self):
         self.status.addNumFuzzy(1)
         self.assertEqual(self.status.numFuzzy, self.fuzzy + 1)
-    
+   
     def testAddnumTranslated(self):
         self.status.addNumTranslated(2)
         self.assertEqual(self.status.numTranslated, self.translated + 2)
-    
+        
     def testGetStatus(self):
-        unitstate = 0
-        unit = self.store.units[1]
-        if unit.isfuzzy():
-            unitstate += World.fuzzy
-        if unit.istranslated():
-            unitstate += World.translated
-        else:
-            unitstate += World.untranslated
+        unitstate = World.fuzzy + World.untranslated
+        unit = self.pofile.units[0]
         self.assertEqual(self.status.getStatus(unit), unitstate)
     
     def testStatusString(self):
-        status = "Total: " + str(self.total)  +  "  |  Fuzzy: " +  str(self.fuzzy) + \
-                "  |  Translated: " +  str(self.translated) + "  |  Untranslated: " + str(self.untranslated)
-        self.assertEqual(self.status.statusString(), status)
+        status = self.status.statusString()
+        self.assertEqual(len(status) > 0, True )
+        self.assertEqual(type(status), str)
         
 if __name__ == '__main__':
     unittest.main()
