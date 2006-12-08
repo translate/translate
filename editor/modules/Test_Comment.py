6ad0eeab8146d213c89016cb7ee0b530432b71d3
Hok Kakada <hokkakada@khmeros.info>
2006-12-08 09:58:44 +0000

    testCheckModified at last work
    svn path=/src/trunk/; revision=4459

--- editor/modules/Test_Comment.py
+++ editor/modules/Test_Comment.py
@@ -11,25 +11,23 @@ import Comment
 from PyQt4 import QtCore, QtGui
 import unittest
 
-
 class TestComment(unittest.TestCase):
     def setUp(self):
         self.commentObj = Comment.CommentDock(None)
         self.signalObj = testQObject()
         
     def testCheckModified(self):
+        self.signalObj.connect(self.commentObj, QtCore.SIGNAL("commentChanged"), self.slot)
         self.commentObj.ui.txtTranslatorComment.document().setModified(True)
-        self.signalObj.connect(self.commentObj, QtCore.SIGNAL("commentChanged"), self.signalObj.slot)
         self.commentObj.checkModified()
         self.assertEqual(self.signalObj.slotReached, True)
+        
+    def slot(self, text):
+        self.signalObj.slotReached = True
         
 class testQObject(QtCore.QObject):
     def __init__(self):
         self.slotReached = False
-        
-    def slot(self):
-        self.slotReached = True
-        print "hello"
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
