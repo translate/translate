9521236644b160792fa437b03054a84035d2affb
Jens Herden <jens@khmeros.info>
2006-12-12 04:38:48 +0000

    Write only as much code as needed. There is no need for a second class, the slot can be in the first class.
    svn path=/src/trunk/; revision=4472

--- editor/modules/Test_Comment.py
+++ editor/modules/Test_Comment.py
@@ -11,24 +11,20 @@ import Comment
 from PyQt4 import QtCore, QtGui
 import unittest
 
-class TestComment(unittest.TestCase, QtCore.QObject):
+class TestComment(unittest.TestCase):
     def setUp(self):
         self.commentObj = Comment.CommentDock(None)
-        self.signalObj = testQObject()
-        
+        self.slotReached = False
+
     def testCheckModified(self):
-        self.connect(self.commentObj, QtCore.SIGNAL("commentChanged"), self.signalObj.slot)
+        QtCore.QObject.connect(self.commentObj, QtCore.SIGNAL("commentChanged"), self.slot)
         self.commentObj.ui.txtTranslatorComment.document().setModified(True)
         self.commentObj.checkModified()
-        self.assertEqual(self.signalObj.slotReached, True)
-        
-class testQObject:
-    def __init__(self):
-        self.slotReached = False
+        self.assertEqual(self.slotReached, True)
         
     def slot(self, text):
         self.slotReached = True
-    
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     unittest.main()
