73da985f3d157598a15cb89100f54643e8a1e1ee
Hok Kakada <hokkakada@khmeros.info>
2006-12-13 02:22:23 +0000

    update the existing test and add two tests
    svn path=/src/trunk/; revision=4504

--- editor/modules/Test_FileAction.py
+++ editor/modules/Test_FileAction.py
@@ -13,19 +13,35 @@ import unittest
 class TestFileAction(unittest.TestCase):
     def setUp(self):
         self.fileActionObj = FileAction.FileAction(None)
-        self.isFileName = False
+        self.slotReached = False
         self.fileName = ""
         
     def testOpenFile(self):
         QtCore.QObject.connect(self.fileActionObj, QtCore.SIGNAL("fileOpened"), self.slot)
-        self.isFileName = self.fileActionObj.openFile()
-        if self.isFileName:
-            self.assertEqual(self.fileName, "example")
+        isFileName = self.fileActionObj.openFile()
+        if isFileName:
+            self.assertEqual(self.slotReached, True)
+            self.assertEqual(len(self.fileName) > 0, True)
         else:
-            self.assertEqual(self.fileName, "")
+            self.assertEqual(self.slotReached, False)
+    
+    def testSave(self):
+        QtCore.QObject.connect(self.fileActionObj, QtCore.SIGNAL("fileSaved"), self.slot)
+        self.fileActionObj.fileName = QtCore.QString("example")
+        self.fileActionObj.save()
+        self.assertEqual(self.fileName, self.fileActionObj.fileName)
         
-    def slot(self):
-        self.fileName = "example"
+    def testSaveAs(self):
+        self.fileActionObj.fileName = "example.po"
+        self.fileActionObj.fileExtension = ".po"
+        QtCore.QObject.connect(self.fileActionObj, QtCore.SIGNAL("fileSaved"), self.slot)
+        self.fileActionObj.saveAs()
+        if (len(self.fileName) > 0):
+            self.assertEqual(QtCore.QString(self.fileName).endsWith("po",  QtCore.Qt.CaseInsensitive), True)
+     
+    def slot(self, fileName):
+        self.fileName = fileName
+        self.slotReached = True    
         
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
