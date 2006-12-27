2bce34e840eba14886169f41ff4c335f989673e7
Keo Sophon <keosophon@khmeros.info>
2006-12-27 10:35:55 +0000

    improve test code
    svn path=/src/trunk/; revision=4626

--- editor/modules/Test_Operator.py
+++ editor/modules/Test_Operator.py
@@ -26,22 +26,21 @@ msgstr "unable, to read file"
 msgid "Could not open a temporary file"
 msgstr "Could not open any"
 '''
-        self.operator.store = self.poparse(self.message)
         
-    def testGetUnits(self):
+    def testsetNewStore(self):
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("newUnits"), self.slot)
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.assertEqual(self.slotReached, True)
     
     def testEmitStatus(self):
-        self.operator.status = Status.Status(self.operator.store.units)
+        self.operator.status = Status.Status(po.pofile.parsestring(self.message).units)
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("currentStatus"), self.slot)
         self.operator.emitStatus()
         self.assertEqual(self.slotReached, True)
     
     def testEmitUnit(self):
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("currentUnit"), self.slot)
-        unit = self.operator.store.units[0]
+        unit = po.pofile.parsestring(self.message).units[0]
         
         # test case unit has no attribute x_editor_index
         self.operator.emitUnit(unit)
@@ -55,7 +54,7 @@ msgstr "Could not open any"
         self.assertEqual(self.slotReached, True)
         
     def testFilteredFuzzy(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.filter = World.filterAll
         #test if filter fuzzy is checked
         self.operator.filterFuzzy(True)
@@ -66,7 +65,7 @@ msgstr "Could not open any"
         self.assertEqual(self.operator.filter, 6)
     
     def testFilterTranslated(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.status = Status.Status(self.operator.store.units)
         
         self.operator.filter = World.filterAll
@@ -79,7 +78,7 @@ msgstr "Could not open any"
         self.assertEqual(self.operator.filter, 5)
     
     def testFilterUntranslated(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.status = Status.Status(self.operator.store.units)
         
         self.operator.filter = World.filterAll
@@ -92,7 +91,7 @@ msgstr "Could not open any"
         self.assertEqual(self.operator.filter, 3)
     
     def testEmitFiltered(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("filterChanged"), self.slot)
         self.operator.emitFiltered(World.fuzzy + World.translated + World.untranslated)
         self.assertEqual(self.slotReached, True)
@@ -101,6 +100,7 @@ msgstr "Could not open any"
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("updateUnit"), self.slot)
      
         #test case self.store is valid
+        self.operator.store = po.pofile.parsestring(self.message)
         self.operator.emitUpdateUnit()
         self.assertEqual(self.slotReached, True)
      
@@ -113,6 +113,7 @@ msgstr "Could not open any"
     def testHeaderData(self):
     
         # test message Header which has no data
+        self.operator.store = po.pofile.parsestring(self.message)
         self.assertEqual(self.operator.headerData(), ('', {}))
      
         # test message Header which has data
@@ -127,7 +128,7 @@ msgstr ""
 msgid "Unable to open file for reading."
 msgstr "unable to read file"
 '''
-        self.operator.store = self.poparse(self.message)
+        self.operator.store = po.pofile.parsestring(self.message)
         self.assertEqual(self.operator.headerData(), ('', {'POT-Creation-Date': u'2005-05-18 21:23+0200', 'PO-Revision-Date': u'2006-11-27 11:50+0700', 'Project-Id-Version': u'cupsdconf'}))
     
     def testMakeNewHeader(self):
@@ -140,7 +141,7 @@ msgstr "unable to read file"
         pass
         
     def testModified(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         # test it will return True, if _modified is true
         self.operator._modified = True
         self.assertEqual(self.operator.modified(), True)
@@ -150,19 +151,19 @@ msgstr "unable to read file"
         self.assertEqual(self.operator.modified(), False)
         
     def testSetComment(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.currentUnitIndex = 1
         self.operator.setComment('comments')
         self.assertEqual(self.operator.filteredList[self.operator.currentUnitIndex].getnotes(), u'comments')
     
     def testSetTarget(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.currentUnitIndex = 1
         self.operator.setTarget('target')
         self.assertEqual(self.operator.filteredList[self.operator.currentUnitIndex].target, u'target')
     
     def testToggleFuzzy(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.toggleFuzzy()
         self.assertEqual(self.operator.store.units[0].isfuzzy(), False)
     
@@ -173,7 +174,7 @@ msgstr "unable to read file"
         self.assertEqual(self.operator.matchCase, False)
         
     def testSearchNext(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.initSearch('To', [World.source, World.target, World.comment], False)
         
         # first found search will encounter in source
@@ -193,7 +194,7 @@ msgstr "unable to read file"
         self.assertEqual(self.operator.searchPointer, 1)
     
     def testSearchPrevious(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.searchPointer = 1
         self.operator.initSearch('To', [World.source, World.target, World.comment], False)
         
@@ -214,7 +215,7 @@ msgstr "unable to read file"
         self.assertEqual(self.operator.searchPointer, 0)
     
     def testReplace(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("replaceText"), self.slot)
         self.operator.initSearch("unable,", [World.source, World.target, World.comment], False)
         self.operator.replace("to")
@@ -222,8 +223,8 @@ msgstr "unable to read file"
         self.assertEqual(self.operator.searchableText[self.operator.currentTextField], 2)
         self.assertEqual(self.operator.foundPosition, 0)
     
-    def test_GetUnitString(self):
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+    def test_getUnitString(self):
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.searchableText = [World.source, World.target]
         self.operator.matchCase = True
         self.operator.currentTextField = 1
@@ -231,7 +232,7 @@ msgstr "unable to read file"
     
     def testSearchFound(self):
         QtCore.QObject.connect(self.operator, QtCore.SIGNAL("searchResult"), self.slot)
-        self.operator.getUnits(wStringIO.StringIO(self.message))
+        self.operator.setNewStore(po.pofile.parsestring(self.message))
         self.operator.initSearch("temporary", [World.source, World.target], True)
         self.operator.searchNext()
         self.assertEqual(self.slotReached, True)
@@ -243,11 +244,6 @@ msgstr "unable to read file"
         self.operator.currentTextField = 0
         self.operator._searchNotFound()
         self.assertEqual(self.slotReached, True)
-        
-    def poparse(self, posource):
-        """helper that parses po source without requiring files"""
-        dummyfile = wStringIO.StringIO(posource)
-        return po.pofile(dummyfile)
     
     def slot(self):
         self.slotReached = True
