eb3de09d0896599e9e65e4e0ad8fb77968f4e51f
Friedel Wolff <friedel@translate.org.za>
2007-02-11 07:01:39 +0000

    Expand test with None as language code
    svn path=/src/trunk/; revision=4989

--- translate/lang/test_factory.py
+++ translate/lang/test_factory.py
@@ -9,5 +9,10 @@ def test_getlanguage():
     assert kmlanguage.code == 'km'
     assert kmlanguage.fullname == 'Khmer'
     
+    # Test a non-exisint code
     language = factory.getlanguage('zz')
+    assert language.code == ''
+
+    # Test with None as language code
+    language = factory.getlanguage(None)
     assert language.code == ''
