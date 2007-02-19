0073350c08b3c68f5b9d32fcf34fb54c27668a6c
Friedel Wolff <friedel@translate.org.za>
2007-02-19 12:18:45 +0000

    Test sentence segmentation.
    svn path=/src/trunk/; revision=5071

--- translate/lang/test_common.py
+++ translate/lang/test_common.py
@@ -16,3 +16,25 @@ def test_words():
     words = language.words(u"This is a weird test .")
     assert words == [u"This", u"is", u"a", u"weird", u"test"]
 
+def test_sentences():
+    """Tests basic functionality of sentence segmentation."""
+    language = common.Common
+    sentences = language.sentences(u"This is a sentence.")
+    assert sentences == [u"This is a sentence."]
+    sentences = language.sentences(u"This is a sentence")
+    assert sentences == [u"This is a sentence"]
+    sentences = language.sentences(u"This is a sentence. Another one.")
+    assert sentences == [u"This is a sentence.", "Another one."]
+    sentences = language.sentences(u"This is a sentence. Another one. Bla.")
+    assert sentences == [u"This is a sentence.", "Another one.", "Bla."]
+    sentences = language.sentences(u"This is a sentence.Not another one.")
+    assert sentences == [u"This is a sentence.Not another one."]
+    sentences = language.sentences(u"Exclamation! Really? No...")
+    assert sentences == [u"Exclamation!", "Really?", "No..."]
+    sentences = language.sentences(u"Four i.e. 1+3. See?")
+    assert sentences == [u"Four i.e. 1+3.", "See?"]
+    sentences = language.sentences(u"Apples, bananas, etc. are nice.")
+    assert sentences == [u"Apples, bananas, etc. are nice."]
+    sentences = language.sentences(u"Apples, bananas, etc.\nNext part")
+    assert sentences == [u"Apples, bananas, etc.", "Next part"]
+
