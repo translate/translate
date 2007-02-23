385025ba519437f966d59a8d8cc7ea00888e44b0
San Titvirak <titvirak@khmeros.info>
2007-02-23 10:39:40 +0000

    Major change from table widget to tree widget for future extension.
    svn path=/src/trunk/; revision=5150

--- pootling/modules/Catalog.py
+++ pootling/modules/Catalog.py
@@ -34,9 +34,18 @@ class Catalog(QtGui.QMainWindow):
     """
     The Catalog Manager which holds the toolviews.
     """
-
+    
     def __init__(self, parent = None):
         QtGui.QMainWindow.__init__(self, parent)
+        self.ui = None
+    
+    def lazyInit(self):
+        """
+        Initialize only at time of calling Catalog.
+        """
+        if (self.ui):
+            return
+        
         self.ui = Ui_Catalog()
         self.ui.setupUi(self)
         self.resize(720,400)
@@ -45,43 +54,44 @@ class Catalog(QtGui.QMainWindow):
         self.autoRefresh = True
 
         # set up table appearance and behavior
-        self.headerLabels = [self.tr("Name"), self.tr("Fuzzy"), self.tr("Untranslated"), self.tr("Total"), self.tr("CVS/SVN Status"), self.tr("Last Revision"), self.tr("Last Translator")]
-        self.ui.tableCatalog.setColumnCount(len(self.headerLabels))
-        self.ui.tableCatalog.setRowCount(0)
-        self.ui.tableCatalog.setHorizontalHeaderLabels(self.headerLabels)
-        self.ui.tableCatalog.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
-        self.ui.tableCatalog.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
-        self.ui.tableCatalog.horizontalHeader().setSortIndicatorShown(True)
-        self.ui.tableCatalog.horizontalHeader().setHighlightSections(False)
-        self.ui.tableCatalog.verticalHeader().hide()
-
+        self.headerLabels = [self.tr("Name"),
+                            self.tr("Fuzzy"),
+                            self.tr("Untranslated"),
+                            self.tr("Total"),
+                            self.tr("CVS/SVN Status"),
+                            self.tr("Last Revision"),
+                            self.tr("Last Translator")]
+        self.ui.treeCatalog.setColumnCount(len(self.headerLabels))
+        self.ui.treeCatalog.setHeaderLabels(self.headerLabels)
+        self.ui.treeCatalog.header().setResizeMode(QtGui.QHeaderView.Interactive)
+        
         # File menu action
         self.connect(self.ui.actionQuit, QtCore.SIGNAL("triggered()"), QtCore.SLOT("close()"))
-
+        
         # Edit menu action
         self.ui.actionReload.setEnabled(True)
         self.connect(self.ui.actionReload, QtCore.SIGNAL("triggered()"), self.refresh)
+        
+        # Catalog setting's checkboxes action.
+        self.catSetting = CatalogSetting(self)
+        self.connect(self.ui.actionConfigure, QtCore.SIGNAL("triggered()"), self.catSetting.show)
+        self.connect(self.catSetting.ui.chbname, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chbfuzzy, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chblastrevision, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chbtranslator, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chbuntranslated, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chbtotal, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
+        self.connect(self.catSetting.ui.chbSVN, QtCore.SIGNAL("stateChanged(int)"), self.toggleHeaderItem)
 
-        # Project menu action
-        self.Catalog = CatalogSetting(self)
-        self.connect(self.ui.actionConfigure, QtCore.SIGNAL("triggered()"), self.Catalog.show)
-        self.connect(self.Catalog.ui.chbname, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chbfuzzy, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chblastrevision, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chbtranslator, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chbuntranslated, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chbtotal, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-        self.connect(self.Catalog.ui.chbSVN, QtCore.SIGNAL("stateChanged(int)"), self.setHeaderLabel)
-
-        # progress bar
-        self.progressBar = QtGui.QProgressBar()
-        self.progressBar.setEnabled(True)
-        self.progressBar.setProperty("value",QtCore.QVariant(0))
-        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
-        self.progressBar.setObjectName("progressBar")
-        self.progressBar.setVisible(False)
-        self.ui.statusbar.addPermanentWidget(self.progressBar)
-
+##        # progress bar
+##        self.progressBar = QtGui.QProgressBar()
+##        self.progressBar.setEnabled(True)
+##        self.progressBar.setProperty("value",QtCore.QVariant(0))
+##        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
+##        self.progressBar.setObjectName("progressBar")
+##        self.progressBar.setVisible(False)
+##        self.ui.statusbar.addPermanentWidget(self.progressBar)
+##
         # Help menu of aboutQt
         self.ui.menuHelp.addSeparator()
         action = QtGui.QWhatsThis.createAction(self)
@@ -90,11 +100,152 @@ class Catalog(QtGui.QMainWindow):
         self.connect(self.ui.actionAbout, QtCore.SIGNAL("triggered()"), self.aboutDialog.showDialog)
         self.connect(self.ui.actionAboutQt, QtCore.SIGNAL("triggered()"), QtGui.qApp, QtCore.SLOT("aboutQt()"))
         
-        self.connect(self.Catalog, QtCore.SIGNAL("updateCatalog"), self.updateCatalog)
-        self.connect(self.ui.tableCatalog, QtCore.SIGNAL("cellDoubleClicked(int, int)"), self.emitOpenFile)
-        
-        self.normalState = QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        self.connect(self.catSetting, QtCore.SIGNAL("updateCatalog"), self.updateCatalog)
+##        self.connect(self.ui.treeCatalog, QtCore.SIGNAL("cellDoubleClicked(int, int)"), self.emitOpenFile)
+##        
         self.setupCheckbox()
+    
+    def toggleHeaderItem(self):
+        if (isinstance(self.sender(), QtGui.QCheckBox)):
+            text = self.sender().text()
+            if text in self.headerLabels:
+                if (self.sender().isChecked()):
+                    self.ui.treeCatalog.showColumn(self.headerLabels.index(text))
+                    World.settings.setValue("Catalog." + text, QtCore.QVariant(True))
+                else:
+                    self.ui.treeCatalog.hideColumn(self.headerLabels.index(text))
+                    World.settings.setValue("Catalog." + text, QtCore.QVariant(False))
+
+##    def updateProgress(self, value):
+##        if (not self.progressBar.isVisible()):
+##            self.progressBar.setVisible(True)
+##        elif (value == 100):
+##            self.progressBar.setVisible(False)
+##        self.progressBar.setValue(value)
+##        
+    def showDialog(self):
+        self.lazyInit()
+        self.show()
+        
+        cats = World.settings.value("CatalogPath").toStringList()
+        if (cats) and (self.ui.treeCatalog.topLevelItemCount() == 0):
+            self.updateCatalog()
+        
+        return
+        
+        cats = World.settings.value("CatalogPath").toStringList()
+        if (cats) and (self.ui.treeCatalog.rowCount() == 0):
+            self.updateCatalog()
+    
+    def updateCatalog(self):
+        """
+        Read data from world's "CatalogPath" and display statistic of files
+        in tree view.
+        """
+        self.ui.treeCatalog.clear()
+        cats = World.settings.value("CatalogPath").toStringList()
+        includeSub = World.settings.value("diveIntoSubCatalog").toBool()
+        
+        for path in cats:
+            path = str(path)
+            item = QtGui.QTreeWidgetItem()
+            item.setText(0, path)
+            if (os.path.isdir(path)):
+                if (not path.endswith("/")):
+                    path += "/"
+                for root, dirs, files in os.walk(path):
+                    for file in files:
+                        if (not root.endswith("/")):
+                            root += "/"
+                        childState = self.getState(root + file)
+                        childItem = QtGui.QTreeWidgetItem()
+                        childItem.setText(0, childState[0])
+                        childItem.setText(1, childState[1])
+                        childItem.setText(2, childState[2])
+                        childItem.setText(3, childState[3])
+                        childItem.setText(4, childState[4])
+                        childItem.setText(5, childState[5])
+                        childItem.setText(6, childState[6])
+                        item.addChild(childItem)
+##                        currentFileNum += 1
+##                        self.updateProgress(int((currentFileNum / maxFilesNum) * 100))
+                    if (not includeSub):
+                        break
+            self.ui.treeCatalog.addTopLevelItem(item)
+        
+    def getState(self, filename):
+        """
+        return stats as list of text of current file name.
+        @param filename: path and file name.
+        """
+        try:
+            store = factory.getobject(filename)
+        except:
+            return ["", "", "", "", "", "", ""]
+        
+        name = os.path.basename(filename)
+        fuzzyUnitCount = store.fuzzy_units()
+        untranslatedUnitCount = store.untranslated_unitcount()
+        totalUnitCount = fuzzyUnitCount + untranslatedUnitCount + store.translated_unitcount()
+        
+        cvsSvn = "?"
+        
+        if hasattr(store, "parseheader"):
+            headerDic = store.parseheader()
+            try:
+                revisionDate = str(headerDic["PO-Revision-Date"])
+            except:
+                pass
+            try:
+                lastTranslator = str(headerDic["Last-Translator"])
+            except:
+                pass
+        else:
+            revisionDate = ""
+            lastTranslator = ""
+        
+        return [name, str(fuzzyUnitCount), str(untranslatedUnitCount), str(totalUnitCount), cvsSvn, revisionDate, lastTranslator]
+
+    def setupCheckbox(self):
+        if not (World.settings.value("Catalog.Name").toBool()):
+            self.catSetting.ui.chbname.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbname.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.Fuzzy").toBool()):
+            self.catSetting.ui.chbfuzzy.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbfuzzy.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.Untranslated").toBool()):
+            self.catSetting.ui.chbuntranslated.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbuntranslated.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.Total").toBool()):
+            self.catSetting.ui.chbtotal.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbtotal.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.CVS/SVN Status").toBool()):
+            self.catSetting.ui.chbSVN.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbSVN.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.Last Revision").toBool()):
+            self.catSetting.ui.chblastrevision.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chblastrevision.setCheckState(QtCore.Qt.Checked)
+        
+        if not (World.settings.value("Catalog.Last Translator").toBool()):
+            self.catSetting.ui.chbtranslator.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.catSetting.ui.chbtranslator.setCheckState(QtCore.Qt.Checked)
+        
+##    def emitOpenFile(self, row, col):
+##        item = self.ui.treeCatalog.item(row, 0)
+##        filename = str(item.text())
+##        self.emit(QtCore.SIGNAL("openFile"), filename)
 
     def refresh(self):
         self.settings = QtCore.QSettings()
@@ -105,156 +256,9 @@ class Catalog(QtGui.QMainWindow):
             self.settings.sync()
             self.refreshTimer.stop() 
 
-    def setHeaderLabel(self):
-        if (isinstance(self.sender(), QtGui.QCheckBox)):
-            text = self.sender().text()
-            if text in self.headerLabels:
-                if (self.sender().isChecked()):
-                    self.ui.tableCatalog.showColumn(self.headerLabels.index(text))
-                    World.settings.setValue("Catalog." + text, QtCore.QVariant(True))
-                else:
-                    self.ui.tableCatalog.hideColumn(self.headerLabels.index(text))
-                    World.settings.setValue("Catalog." + text, QtCore.QVariant(False))
-
-    def updateProgress(self, value):
-        if (not self.progressBar.isVisible()):
-            self.progressBar.setVisible(True)
-        elif (value == 100):
-            self.progressBar.setVisible(False)
-        self.progressBar.setValue(value)
-        
-    def showDialog(self):
-        self.show()
-        cats = World.settings.value("CatalogPath").toStringList()
-        if (cats) and (self.ui.tableCatalog.rowCount() == 0):
-            self.updateCatalog()
-    
-    def updateCatalog(self):
-        self.ui.tableCatalog.clear()
-        self.ui.tableCatalog.setHorizontalHeaderLabels(self.headerLabels)
-        self.ui.tableCatalog.setRowCount(0)
-        self.ui.tableCatalog.setSortingEnabled(False)
-        
-        cats = World.settings.value("CatalogPath").toStringList()
-        includeSub = World.settings.value("diveIntoSubCatalog").toBool()
-        
-        maxFilesNum = 0.0
-        for path in cats:
-            path = str(path)
-            if (os.path.isdir(path)):
-                if (not path.endswith("/")):
-                    path = path + "/"
-                for root, dirs, files in os.walk(path):
-                    for file in files:
-                        maxFilesNum += 1
-                    if (not includeSub):
-                        break
-        
-        currentFileNum = 0.0
-        for path in cats:
-            path = str(path)
-            if (os.path.isdir(path)):
-                if (not path.endswith("/")):
-                    path = path + "/"
-                for root, dirs, files in os.walk(path):
-                    for file in files:
-                        self.addUnit(root + file)
-                        currentFileNum += 1
-                        self.updateProgress(int((currentFileNum / maxFilesNum) * 100))
-                    if (not includeSub):
-                        break
-        self.ui.tableCatalog.setSortingEnabled(False)
-    
-    def addUnit(self, filename):
-        """
-        add the unit to table.
-        @param filename: path and file name.
-        """
-##        try:
-##            print versioncontrol.getcleanfile(filename)
-##        except:
-##            pass
-##        
-        try:
-            store = factory.getobject(filename)
-        except:
-            return
-        row = self.ui.tableCatalog.rowCount()
-        self.ui.tableCatalog.setRowCount(row + 1)
-        
-        item = QtGui.QTableWidgetItem(filename)
-        item.setFlags(self.normalState)
-        self.ui.tableCatalog.setItem(row, 0, item)
-        item = QtGui.QTableWidgetItem(str(store.fuzzy_units()))
-        item.setFlags(self.normalState)
-        self.ui.tableCatalog.setItem(row, 1, item)
-        item = QtGui.QTableWidgetItem(str(store.untranslated_unitcount()))
-        item.setFlags(self.normalState)
-        self.ui.tableCatalog.setItem(row, 2, item)
-        totalUnitCount = store.fuzzy_units() + store.untranslated_unitcount() + store.translated_unitcount()
-        item = QtGui.QTableWidgetItem(str(totalUnitCount))
-        item.setFlags(self.normalState)
-        self.ui.tableCatalog.setItem(row, 3, item)
-        
-        if hasattr(store, "parseheader"):
-            headerDic = store.parseheader()
-            try:
-                item = QtGui.QTableWidgetItem(str(headerDic["Last-Translator"]))
-                item.setFlags(self.normalState)
-                self.ui.tableCatalog.setItem(row, 6, item)
-            except:
-                pass
-            try:
-                item = QtGui.QTableWidgetItem(str(headerDic["PO-Revision-Date"]))
-                item.setFlags(self.normalState)
-                self.ui.tableCatalog.setItem(row, 5, item)
-            except:
-                pass
-
-    def setupCheckbox(self):
-        if not (World.settings.value("Catalog.Name").toBool()):
-            self.Catalog.ui.chbname.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbname.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.Fuzzy").toBool()):
-            self.Catalog.ui.chbfuzzy.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbfuzzy.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.Untranslated").toBool()):
-            self.Catalog.ui.chbuntranslated.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbuntranslated.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.Total").toBool()):
-            self.Catalog.ui.chbtotal.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbtotal.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.CVS/SVN Status").toBool()):
-            self.Catalog.ui.chbSVN.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbSVN.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.Last Revision").toBool()):
-            self.Catalog.ui.chblastrevision.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chblastrevision.setCheckState(QtCore.Qt.Checked)
-        
-        if not (World.settings.value("Catalog.Last Translator").toBool()):
-            self.Catalog.ui.chbtranslator.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.Catalog.ui.chbtranslator.setCheckState(QtCore.Qt.Checked)
-    
-    def emitOpenFile(self, row, col):
-        item = self.ui.tableCatalog.item(row, 0)
-        filename = str(item.text())
-        self.emit(QtCore.SIGNAL("openFile"), filename)
-    
-if __name__ == "__main__":
-    import sys, os
-    app = QtGui.QApplication(sys.argv)
-    catalog = Catalog(None)
-    catalog.showDialog()
-    sys.exit(catalog.exec_())
+##if __name__ == "__main__":
+##    import sys, os
+##    app = QtGui.QApplication(sys.argv)
+##    catalog = Catalog(None)
+##    catalog.showDialog()
+##    sys.exit(catalog.exec_())
--- pootling/ui/Ui_Catalog.py
+++ pootling/ui/Ui_Catalog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'unknown'
 #
-# Created: Wed Feb  7 17:02:03 2007
+# Created: Fri Feb 23 17:34:53 2007
 #      by: PyQt4 UI code generator 4.0
 #
 # WARNING! All changes made in this file will be lost!
@@ -19,34 +19,38 @@ class Ui_Catalog(object):
         self.centralwidget.setObjectName("centralwidget")
 
         self.gridlayout = QtGui.QGridLayout(self.centralwidget)
-        self.gridlayout.setMargin(0)
-        self.gridlayout.setSpacing(0)
+        self.gridlayout.setMargin(9)
+        self.gridlayout.setSpacing(6)
         self.gridlayout.setObjectName("gridlayout")
 
-        self.tableCatalog = QtGui.QTableWidget(self.centralwidget)
-        self.tableCatalog.setObjectName("tableCatalog")
-        self.gridlayout.addWidget(self.tableCatalog,0,0,1,1)
+        self.treeCatalog = QtGui.QTreeWidget(self.centralwidget)
+
+        font = QtGui.QFont(self.treeCatalog.font())
+        font.setPointSize(10)
+        self.treeCatalog.setFont(font)
+        self.treeCatalog.setAlternatingRowColors(True)
+        self.treeCatalog.setObjectName("treeCatalog")
+        self.gridlayout.addWidget(self.treeCatalog,0,0,1,1)
         Catalog.setCentralWidget(self.centralwidget)
 
         self.menubar = QtGui.QMenuBar(Catalog)
         self.menubar.setGeometry(QtCore.QRect(0,0,531,28))
         self.menubar.setObjectName("menubar")
 
-        self.menuHelp = QtGui.QMenu(self.menubar)
-        self.menuHelp.setObjectName("menuHelp")
-
-        self.menuEdit = QtGui.QMenu(self.menubar)
-        self.menuEdit.setObjectName("menuEdit")
-
         self.menuFile = QtGui.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
 
+        self.menuHelp = QtGui.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+
         self.menuProject = QtGui.QMenu(self.menubar)
         self.menuProject.setObjectName("menuProject")
+
+        self.menuEdit = QtGui.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
         Catalog.setMenuBar(self.menubar)
 
         self.statusbar = QtGui.QStatusBar(Catalog)
-        self.statusbar.setGeometry(QtCore.QRect(0,379,531,21))
         self.statusbar.setObjectName("statusbar")
         Catalog.setStatusBar(self.statusbar)
 
@@ -73,11 +77,11 @@ class Ui_Catalog(object):
         self.actionConfigure = QtGui.QAction(Catalog)
         self.actionConfigure.setIcon(QtGui.QIcon("../images/configure.png"))
         self.actionConfigure.setObjectName("actionConfigure")
+        self.menuFile.addAction(self.actionQuit)
         self.menuHelp.addAction(self.actionAbout)
         self.menuHelp.addAction(self.actionAboutQt)
-        self.menuEdit.addAction(self.actionReload)
-        self.menuFile.addAction(self.actionQuit)
         self.menuProject.addAction(self.actionConfigure)
+        self.menuEdit.addAction(self.actionReload)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
         self.menubar.addAction(self.menuProject.menuAction())
@@ -93,19 +97,16 @@ class Ui_Catalog(object):
 
     def retranslateUi(self, Catalog):
         Catalog.setWindowTitle(self.tr("Catalog Manager"))
-        self.tableCatalog.clear()
-        self.tableCatalog.setColumnCount(0)
-        self.tableCatalog.setRowCount(0)
-        self.menuHelp.setTitle(self.tr("&Help"))
-        self.menuEdit.setTitle(self.tr("&Edit"))
         self.menuFile.setTitle(self.tr("&File"))
+        self.menuHelp.setTitle(self.tr("&Help"))
         self.menuProject.setTitle(self.tr("&Project"))
+        self.menuEdit.setTitle(self.tr("&Edit"))
         self.actionQuit.setText(self.tr("&Quit"))
         self.actionQuit.setShortcut(self.tr("Ctrl+Q"))
         self.actionAbout.setText(self.tr("About"))
         self.actionAboutQt.setText(self.tr("About Qt"))
         self.actionReload.setText(self.tr("&Reload"))
-        self.actionReload.setShortcut(self.tr("Ctrl+R"))
+        self.actionReload.setShortcut(self.tr("F5"))
         self.actionConfigure.setText(self.tr("&Configure..."))
 
 
