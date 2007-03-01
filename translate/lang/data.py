4827f580e7c34927e57923ab65de88d0eb8e7a2e
Friedel Wolff <friedel@translate.org.za>
2007-03-01 11:29:45 +0000

    Fix typo where Turkish plural equation is not a string
    svn path=/src/trunk/; revision=5211

--- translate/lang/data.py
+++ translate/lang/data.py
@@ -96,7 +96,7 @@ languages = {
 'sv': ('Swedish', 2, '(n != 1)'),
 'ta': ('Tamil', 2, '(n != 1)'),
 'tk': ('Turkmen', 2, '(n != 1)'),
-'tr': ('Turkish', 1, 0),
+'tr': ('Turkish', 1, '0'),
 'uk': ('Ukrainian', 3, '(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)'),
 'vi': ('Vietnamese',1 , '0'),
 'wa': ('Walloon', 2, '(n > 1)'),
