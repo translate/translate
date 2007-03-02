94f285bb396114e1bdc93909a7cb0aa1e7ff2645
Seth Chanratha <sethchanratha@khmeros.info>
2007-03-02 10:34:05 +0000

    add Find in Files, Replace in file and Stop Find
    svn path=/src/trunk/; revision=5217

--- pootling/ui/Ui_Catalog.py
+++ pootling/ui/Ui_Catalog.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'unknown'
+# Form implementation generated from reading ui file '/home/ratha/sourceforge.net/translate/trunk/pootling/ui/Catalog.ui'
 #
-# Created: Fri Feb 23 17:34:53 2007
-#      by: PyQt4 UI code generator 4.0
+# Created: Fri Mar  2 17:32:56 2007
+#      by: PyQt4 UI code generator 4-snapshot-20070212
 #
 # WARNING! All changes made in this file will be lost!
 
-import sys
 from PyQt4 import QtCore, QtGui
 
 class Ui_Catalog(object):
@@ -77,10 +76,26 @@ class Ui_Catalog(object):
         self.actionConfigure = QtGui.QAction(Catalog)
         self.actionConfigure.setIcon(QtGui.QIcon("../images/configure.png"))
         self.actionConfigure.setObjectName("actionConfigure")
+
+        self.actionFind_in_Files = QtGui.QAction(Catalog)
+        self.actionFind_in_Files.setIcon(QtGui.QIcon("../images/find.png"))
+        self.actionFind_in_Files.setObjectName("actionFind_in_Files")
+
+        self.actionReplace_in_Files = QtGui.QAction(Catalog)
+        self.actionReplace_in_Files.setIcon(QtGui.QIcon("../images/replace.png"))
+        self.actionReplace_in_Files.setObjectName("actionReplace_in_Files")
+
+        self.actionStop_Find = QtGui.QAction(Catalog)
+        self.actionStop_Find.setIcon(QtGui.QIcon("../images/stop.png"))
+        self.actionStop_Find.setObjectName("actionStop_Find")
         self.menuFile.addAction(self.actionQuit)
         self.menuHelp.addAction(self.actionAbout)
         self.menuHelp.addAction(self.actionAboutQt)
         self.menuProject.addAction(self.actionConfigure)
+        self.menuEdit.addAction(self.actionFind_in_Files)
+        self.menuEdit.addAction(self.actionReplace_in_Files)
+        self.menuEdit.addAction(self.actionStop_Find)
+        self.menuEdit.addSeparator()
         self.menuEdit.addAction(self.actionReload)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
@@ -92,25 +107,30 @@ class Ui_Catalog(object):
         self.retranslateUi(Catalog)
         QtCore.QMetaObject.connectSlotsByName(Catalog)
 
-    def tr(self, string):
-        return QtGui.QApplication.translate("Catalog", string, None, QtGui.QApplication.UnicodeUTF8)
-
     def retranslateUi(self, Catalog):
-        Catalog.setWindowTitle(self.tr("Catalog Manager"))
-        self.menuFile.setTitle(self.tr("&File"))
-        self.menuHelp.setTitle(self.tr("&Help"))
-        self.menuProject.setTitle(self.tr("&Project"))
-        self.menuEdit.setTitle(self.tr("&Edit"))
-        self.actionQuit.setText(self.tr("&Quit"))
-        self.actionQuit.setShortcut(self.tr("Ctrl+Q"))
-        self.actionAbout.setText(self.tr("About"))
-        self.actionAboutQt.setText(self.tr("About Qt"))
-        self.actionReload.setText(self.tr("&Reload"))
-        self.actionReload.setShortcut(self.tr("F5"))
-        self.actionConfigure.setText(self.tr("&Configure..."))
+        Catalog.setWindowTitle(QtGui.QApplication.translate("Catalog", "Catalog Manager", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuFile.setTitle(QtGui.QApplication.translate("Catalog", "&File", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuHelp.setTitle(QtGui.QApplication.translate("Catalog", "&Help", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuProject.setTitle(QtGui.QApplication.translate("Catalog", "&Project", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuEdit.setTitle(QtGui.QApplication.translate("Catalog", "&Edit", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionQuit.setText(QtGui.QApplication.translate("Catalog", "&Quit", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionQuit.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+Q", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionAbout.setText(QtGui.QApplication.translate("Catalog", "About", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionAboutQt.setText(QtGui.QApplication.translate("Catalog", "About Qt", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionReload.setText(QtGui.QApplication.translate("Catalog", "&Reload", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionReload.setShortcut(QtGui.QApplication.translate("Catalog", "F5", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionConfigure.setText(QtGui.QApplication.translate("Catalog", "&Configure...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionFind_in_Files.setText(QtGui.QApplication.translate("Catalog", "&Find in Files...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionFind_in_Files.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+F", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionReplace_in_Files.setText(QtGui.QApplication.translate("Catalog", "&Replace in Files...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionReplace_in_Files.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+R", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionStop_Find.setText(QtGui.QApplication.translate("Catalog", "&Stop Find", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionStop_Find.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+S", None, QtGui.QApplication.UnicodeUTF8))
+
 
 
 if __name__ == "__main__":
+    import sys
     app = QtGui.QApplication(sys.argv)
     Catalog = QtGui.QMainWindow()
     ui = Ui_Catalog()
