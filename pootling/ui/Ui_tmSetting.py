f323212bd57ea24d53b49867e3a80ef75689baaa
Hok Kakada <hokkakada@khmeros.info>
2007-03-21 04:11:11 +0000

    add Glossary option and some what'sthis text
    svn path=/src/trunk/; revision=5282

--- pootling/ui/Ui_tmSetting.py
+++ pootling/ui/Ui_tmSetting.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/ks/programming/wordforge/trunk/pootling/ui/tmSetting.ui'
+# Form implementation generated from reading ui file 'unknown'
 #
-# Created: Tue Mar 20 11:20:37 2007
-#      by: PyQt4 UI code generator 4-snapshot-20070212
+# Created: Wed Mar 21 10:51:24 2007
+#      by: PyQt4 UI code generator 4.0
 #
 # WARNING! All changes made in this file will be lost!
 
+import sys
 from PyQt4 import QtCore, QtGui
 
 class Ui_tmsetting(object):
@@ -19,9 +20,6 @@ class Ui_tmsetting(object):
         self.gridlayout.setSpacing(6)
         self.gridlayout.setObjectName("gridlayout")
 
-        spacerItem = QtGui.QSpacerItem(151,28,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
-        self.gridlayout.addItem(spacerItem,1,0,1,1)
-
         self.btnCancel = QtGui.QPushButton(tmsetting)
         self.btnCancel.setObjectName("btnCancel")
         self.gridlayout.addWidget(self.btnCancel,1,2,1,1)
@@ -29,6 +27,9 @@ class Ui_tmsetting(object):
         self.btnOk = QtGui.QPushButton(tmsetting)
         self.btnOk.setObjectName("btnOk")
         self.gridlayout.addWidget(self.btnOk,1,1,1,1)
+
+        spacerItem = QtGui.QSpacerItem(251,28,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.gridlayout.addItem(spacerItem,1,0,1,1)
 
         self.tabOptions = QtGui.QTabWidget(tmsetting)
         self.tabOptions.setObjectName("tabOptions")
@@ -41,42 +42,19 @@ class Ui_tmsetting(object):
         self.gridlayout1.setSpacing(6)
         self.gridlayout1.setObjectName("gridlayout1")
 
-        self.checkBox = QtGui.QCheckBox(self.tab)
-        self.checkBox.setObjectName("checkBox")
-        self.gridlayout1.addWidget(self.checkBox,6,0,1,1)
+        self.chkGlossary = QtGui.QCheckBox(self.tab)
+        self.chkGlossary.setObjectName("chkGlossary")
+        self.gridlayout1.addWidget(self.chkGlossary,7,0,1,1)
 
-        spacerItem1 = QtGui.QSpacerItem(20,40,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
-        self.gridlayout1.addItem(spacerItem1,10,0,1,1)
+        self.progressBar = QtGui.QProgressBar(self.tab)
+        self.progressBar.setProperty("value",QtCore.QVariant(0))
+        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
+        self.progressBar.setObjectName("progressBar")
+        self.gridlayout1.addWidget(self.progressBar,9,0,1,2)
 
-        self.btnRemove = QtGui.QPushButton(self.tab)
-        self.btnRemove.setIcon(QtGui.QIcon("../images/removeTM.png"))
-        self.btnRemove.setIconSize(QtCore.QSize(16,16))
-        self.btnRemove.setObjectName("btnRemove")
-        self.gridlayout1.addWidget(self.btnRemove,2,1,1,1)
-
-        self.btnDisable = QtGui.QPushButton(self.tab)
-        self.btnDisable.setIconSize(QtCore.QSize(16,16))
-        self.btnDisable.setObjectName("btnDisable")
-        self.gridlayout1.addWidget(self.btnDisable,5,1,1,1)
-
-        self.label_4 = QtGui.QLabel(self.tab)
-        self.label_4.setObjectName("label_4")
-        self.gridlayout1.addWidget(self.label_4,0,0,1,1)
-
-        self.btnRemoveAll = QtGui.QPushButton(self.tab)
-        self.btnRemoveAll.setIcon(QtGui.QIcon("../images/eraser.png"))
-        self.btnRemoveAll.setIconSize(QtCore.QSize(16,16))
-        self.btnRemoveAll.setObjectName("btnRemoveAll")
-        self.gridlayout1.addWidget(self.btnRemoveAll,3,1,1,1)
-
-        self.listWidget = QtGui.QListWidget(self.tab)
-        self.listWidget.setObjectName("listWidget")
-        self.gridlayout1.addWidget(self.listWidget,1,0,5,1)
-
-        self.btnEnable = QtGui.QPushButton(self.tab)
-        self.btnEnable.setIconSize(QtCore.QSize(16,16))
-        self.btnEnable.setObjectName("btnEnable")
-        self.gridlayout1.addWidget(self.btnEnable,4,1,1,1)
+        self.label_5 = QtGui.QLabel(self.tab)
+        self.label_5.setObjectName("label_5")
+        self.gridlayout1.addWidget(self.label_5,8,0,1,1)
 
         self.btnAdd = QtGui.QPushButton(self.tab)
         self.btnAdd.setIcon(QtGui.QIcon("../images/addTM.png"))
@@ -84,16 +62,43 @@ class Ui_tmsetting(object):
         self.btnAdd.setObjectName("btnAdd")
         self.gridlayout1.addWidget(self.btnAdd,1,1,1,1)
 
-        self.label_5 = QtGui.QLabel(self.tab)
-        self.label_5.setObjectName("label_5")
-        self.gridlayout1.addWidget(self.label_5,8,0,1,1)
+        self.btnEnable = QtGui.QPushButton(self.tab)
+        self.btnEnable.setIconSize(QtCore.QSize(16,16))
+        self.btnEnable.setObjectName("btnEnable")
+        self.gridlayout1.addWidget(self.btnEnable,4,1,1,1)
 
-        self.progressBar = QtGui.QProgressBar(self.tab)
-        self.progressBar.setProperty("value",QtCore.QVariant(0))
-        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
-        self.progressBar.setObjectName("progressBar")
-        self.gridlayout1.addWidget(self.progressBar,9,0,1,2)
-        self.tabOptions.addTab(self.tab,"")
+        self.listWidget = QtGui.QListWidget(self.tab)
+        self.listWidget.setObjectName("listWidget")
+        self.gridlayout1.addWidget(self.listWidget,1,0,5,1)
+
+        self.btnRemoveAll = QtGui.QPushButton(self.tab)
+        self.btnRemoveAll.setIcon(QtGui.QIcon("../images/eraser.png"))
+        self.btnRemoveAll.setIconSize(QtCore.QSize(16,16))
+        self.btnRemoveAll.setObjectName("btnRemoveAll")
+        self.gridlayout1.addWidget(self.btnRemoveAll,3,1,1,1)
+
+        self.label_4 = QtGui.QLabel(self.tab)
+        self.label_4.setObjectName("label_4")
+        self.gridlayout1.addWidget(self.label_4,0,0,1,1)
+
+        self.btnDisable = QtGui.QPushButton(self.tab)
+        self.btnDisable.setIconSize(QtCore.QSize(16,16))
+        self.btnDisable.setObjectName("btnDisable")
+        self.gridlayout1.addWidget(self.btnDisable,5,1,1,1)
+
+        self.btnRemove = QtGui.QPushButton(self.tab)
+        self.btnRemove.setIcon(QtGui.QIcon("../images/removeTM.png"))
+        self.btnRemove.setIconSize(QtCore.QSize(16,16))
+        self.btnRemove.setObjectName("btnRemove")
+        self.gridlayout1.addWidget(self.btnRemove,2,1,1,1)
+
+        spacerItem1 = QtGui.QSpacerItem(20,40,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
+        self.gridlayout1.addItem(spacerItem1,10,0,1,1)
+
+        self.checkBox = QtGui.QCheckBox(self.tab)
+        self.checkBox.setObjectName("checkBox")
+        self.gridlayout1.addWidget(self.checkBox,6,0,1,1)
+        self.tabOptions.addTab(self.tab, "")
 
         self.tab_4 = QtGui.QWidget()
         self.tab_4.setObjectName("tab_4")
@@ -148,7 +153,7 @@ class Ui_tmsetting(object):
 
         spacerItem2 = QtGui.QSpacerItem(20,40,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
         self.gridlayout2.addItem(spacerItem2,1,0,1,1)
-        self.tabOptions.addTab(self.tab_4,"")
+        self.tabOptions.addTab(self.tab_4, "")
         self.gridlayout.addWidget(self.tabOptions,0,0,1,3)
 
         self.retranslateUi(tmsetting)
@@ -167,36 +172,40 @@ class Ui_tmsetting(object):
         tmsetting.setTabOrder(self.spinSimilarity,self.spinMaxCandidate)
         tmsetting.setTabOrder(self.spinMaxCandidate,self.spinMaxLen)
 
-    def retranslateUi(self, tmsetting):
-        tmsetting.setWindowTitle(QtGui.QApplication.translate("tmsetting", "Translation Memory Settings", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnCancel.setText(QtGui.QApplication.translate("tmsetting", "&Cancel", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnOk.setText(QtGui.QApplication.translate("tmsetting", "&OK", None, QtGui.QApplication.UnicodeUTF8))
-        self.checkBox.setText(QtGui.QApplication.translate("tmsetting", "Dive into Subfolders", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnRemove.setToolTip(QtGui.QApplication.translate("tmsetting", "remove TM", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnRemove.setText(QtGui.QApplication.translate("tmsetting", " De&lete", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnDisable.setToolTip(QtGui.QApplication.translate("tmsetting", "Disable Translation Memory", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnDisable.setText(QtGui.QApplication.translate("tmsetting", "Di&sable", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("tmsetting", "Locations:", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnRemoveAll.setToolTip(QtGui.QApplication.translate("tmsetting", "clear list", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnRemoveAll.setText(QtGui.QApplication.translate("tmsetting", " Clea&r", None, QtGui.QApplication.UnicodeUTF8))
-        self.listWidget.setToolTip(QtGui.QApplication.translate("tmsetting", "Translation Memory(s)", None, QtGui.QApplication.UnicodeUTF8))
-        self.listWidget.setStatusTip(QtGui.QApplication.translate("tmsetting", "List of Translation Memory Location(s)", None, QtGui.QApplication.UnicodeUTF8))
-        self.listWidget.setWhatsThis(QtGui.QApplication.translate("tmsetting", "<h3>Translation Memory</h3> List of TMs. You can specify TMs which will be used and not used.", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnEnable.setToolTip(QtGui.QApplication.translate("tmsetting", "Enabel Translation Memory", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnEnable.setText(QtGui.QApplication.translate("tmsetting", " &Enable", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnAdd.setToolTip(QtGui.QApplication.translate("tmsetting", "Add TM", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnAdd.setText(QtGui.QApplication.translate("tmsetting", " &Add", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_5.setText(QtGui.QApplication.translate("tmsetting", "Progress", None, QtGui.QApplication.UnicodeUTF8))
-        self.tabOptions.setTabText(self.tabOptions.indexOf(self.tab), QtGui.QApplication.translate("tmsetting", "&File", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("tmsetting", "Maximum string length", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("tmsetting", "Similarity", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("tmsetting", "Maximum candidates", None, QtGui.QApplication.UnicodeUTF8))
-        self.tabOptions.setTabText(self.tabOptions.indexOf(self.tab_4), QtGui.QApplication.translate("tmsetting", "O&ptions", None, QtGui.QApplication.UnicodeUTF8))
+    def tr(self, string):
+        return QtGui.QApplication.translate("tmsetting", string, None, QtGui.QApplication.UnicodeUTF8)
 
+    def retranslateUi(self, tmsetting):
+        tmsetting.setWindowTitle(self.tr("Translation Memory Settings"))
+        self.btnCancel.setText(self.tr("&Cancel"))
+        self.btnOk.setText(self.tr("&OK"))
+        self.chkGlossary.setWhatsThis(self.tr("<h3>Glossary</h3> If this option is checked, then the selected path is a kind of glossary file(units as word), not normal file(units as strings). Check this can make translation memory run faster."))
+        self.chkGlossary.setText(self.tr("Glossary"))
+        self.label_5.setText(self.tr("Progress"))
+        self.btnAdd.setToolTip(self.tr("Add TM"))
+        self.btnAdd.setText(self.tr(" &Add"))
+        self.btnEnable.setToolTip(self.tr("Enabel Translation Memory"))
+        self.btnEnable.setText(self.tr(" &Enable"))
+        self.listWidget.setToolTip(self.tr("Translation Memory(s)"))
+        self.listWidget.setStatusTip(self.tr("List of Translation Memory Location(s)"))
+        self.listWidget.setWhatsThis(self.tr("<h3>Translation Memory</h3> List of TMs. You can specify TMs which will be used and not used."))
+        self.btnRemoveAll.setToolTip(self.tr("clear list"))
+        self.btnRemoveAll.setText(self.tr(" Clea&r"))
+        self.label_4.setText(self.tr("Locations:"))
+        self.btnDisable.setToolTip(self.tr("Disable Translation Memory"))
+        self.btnDisable.setText(self.tr("Di&sable"))
+        self.btnRemove.setToolTip(self.tr("remove TM"))
+        self.btnRemove.setText(self.tr(" De&lete"))
+        self.checkBox.setWhatsThis(self.tr("<h3>Dive into subfolders</h3>If it is checked the process will include subfolders for translation memory scaning."))
+        self.checkBox.setText(self.tr("Dive into Subfolders"))
+        self.tabOptions.setTabText(self.tabOptions.indexOf(self.tab), self.tr("&File"))
+        self.label_3.setText(self.tr("Maximum string length"))
+        self.label.setText(self.tr("Similarity"))
+        self.label_2.setText(self.tr("Maximum candidates"))
+        self.tabOptions.setTabText(self.tabOptions.indexOf(self.tab_4), self.tr("O&ptions"))
 
 
 if __name__ == "__main__":
-    import sys
     app = QtGui.QApplication(sys.argv)
     tmsetting = QtGui.QWidget()
     ui = Ui_tmsetting()
