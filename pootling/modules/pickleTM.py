922f0d0028bb8c3cb793f000cab663c0bbd06c4a
Hok Kakada <hokkakada@khmeros.info>
2007-03-21 04:21:21 +0000

    Change from Qsetting to use ConfigParser in order to make it independent from pyQT, also rewrite pickleTM to be a class.
    svn path=/src/trunk/; revision=5283

--- pootling/modules/pickleTM.py
+++ pootling/modules/pickleTM.py
@@ -20,109 +20,121 @@
 # This module provides interface for pickle and unpickle each matcher candidates of base object files
 
 import os, tempfile, pickle
-from PyQt4 import QtCore
-from pootling.modules import World
 from translate.storage import factory
 from translate.search import match
+from ConfigParser import *
 
-def createStore(file):
-    """Create a base object from file.
-    
-    @param file: as a file path or object
-    @return: store as a base object
-    
-    """
-    try:
-        store = factory.getobject(file)
-    except:
-        store = None
-    return store
-
-def getStore(TMpath):
-    """Return base object as store or storelist.
-    
-    @param TMpath: file or translation memory path as string
-    @return store or storelist
-    
-    """
-    diveSub = World.settings.value("diveIntoSub").toBool()
-    path = str(TMpath)
-    
-    if (os.path.isfile(path)):
-        return createStore(path)
-    
-    if (os.path.isdir(path)):
-        storelist = []
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                store = createStore(os.path.join(root + '/' + file))
-                if (store):
-                    storelist.append(store)
-            # whether dive into subfolder
-            if (not diveSub):
-                # not dive into subfolder
-                break
-        return storelist
-    
-def pickleMatcher(matcher):
-    """Pickle matcher of TM locations.
-    
-    @param matcher: matcher of TM files or locations
-    
-    """
-    filename = World.settings.value("fileStoreMatcher").toString()
-    if (not filename):
-        handle, filename = tempfile.mkstemp('','PKL')
-    tmpFile = open(filename, 'w')
-    if (matcher):
-        pickle.dump(matcher, tmpFile)
-    tmpFile.close()
-    World.settings.setValue("fileStoreMatcher", QtCore.QVariant(filename))
-
-def getMatcher():
-    """Unpickle matcher from file.
-    
-    @return matcher: matcher of TM locations
-    
-    """
-    matcher = None
-    filename = World.settings.value("fileStoreMatcher").toString()
-    if (filename and os.path.exists(filename)):
-        tmpFile = open(filename, 'rb')
+class pickleTM:
+    """This class is for pickling TM"""
+    def __init__(self, confFile = ""):
+        self.config = ConfigParser()
+        self.config.read(confFile)
+        
+    def createStore(self, file):
+        """Create a base object from file.
+        
+        @param file: as a file path or object
+        @return: store as a base object
+        
+        """
         try:
-            matcher = pickle.load(tmpFile)
+            store = factory.getobject(file)
+        except:
+            store = None
+        return store
+    
+    def getStore(self, TMpath):
+        """Return base object as store or storelist.
+        
+        @param TMpath: file or translation memory path as string
+        @return store or storelist
+        
+        """
+        try:
+            diveSub = self.config.get('General','diveIntoSub')
+        except NoOptionError:
+            pass
+        path = str(TMpath)
+        
+        if (os.path.isfile(path)):
+            return self.createStore(path)
+        
+        if (os.path.isdir(path)):
+            storelist = []
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    store = self.createStore(os.path.join(root + '/' + file))
+                    if (store):
+                        storelist.append(store)
+                # whether dive into subfolder
+                if (not diveSub):
+                    # not dive into subfolder
+                    break
+            return storelist
+        
+    def pickleMatcher(self, matcher, prefix = "PKL"):
+        """Pickle matcher of TM locations.
+        
+        @param matcher: matcher of TM files or locations
+        
+        """
+        try:
+            filename = self.config.get('General','fileStoreMatcher')
+        except NoOptionError:
+            handle, filename = tempfile.mkstemp('','PKL')
+        tmpFile = open(filename, 'w')
+        if (matcher):
+            pickle.dump(matcher, tmpFile)
+        tmpFile.close()
+        self.config.set('General', 'fileStoreMatcher', filename)
+    
+    def getMatcher(self):
+        """Unpickle matcher from file.
+        
+        @return matcher: matcher of TM locations
+        
+        """
+        matcher = None
+        try:
+            filename = self.config.get('General','fileStoreMatcher')
+            if (filename and os.path.exists(filename)):
+                tmpFile = open(filename, 'rb')
+                try:
+                    matcher = pickle.load(tmpFile)
+                except:
+                    pass
+                tmpFile.close()
         except:
             pass
-        tmpFile.close()
-    return matcher
-
-def buildMatcher(stringlist, max_candidates, min_similarity,  max_string_len):
-    """Build new matcher of TM locations and dump it to file.
+        return matcher
     
-    @param stringlist: List of TM locations as string of path
-    @param max_candidates: The maximum number of candidates that should be assembled,
-    @param min_similarity: The minimum similarity that must be attained to be included in
-    @param max_string_len: maximum length of source string to be searched.
-    @return: matcher object
-    """
-    store = None
-    storelist = []
-    for i in range(len(stringlist)):
-        store = getStore(stringlist[i])
-        if (store):
-            if (not isinstance(store, list)):
-                storelist.append(store)
-            else:
-                storelist += store
-    matcher = match.matcher(storelist, max_candidates, min_similarity,  max_string_len)
-    pickleMatcher(matcher)
-    return matcher
-
-def removeFile():
-    filename = World.settings.value("fileStoreMatcher").toString()
-    try:
-        os.remove(filename)
-        World.settings.remove("fileStoreMatcher")
-    except:
-        pass
+    def buildMatcher(self, stringlist, max_candidates, min_similarity,  max_string_len):
+        """Build new matcher of TM locations and dump it to file.
+        
+        @param stringlist: List of TM locations as string of path
+        @param max_candidates: The maximum number of candidates that should be assembled,
+        @param min_similarity: The minimum similarity that must be attained to be included in
+        @param max_string_len: maximum length of source string to be searched.
+        @return: matcher object
+        """
+        store = None
+        storelist = []
+        for i in range(len(stringlist)):
+            store = self.getStore(stringlist[i])
+            if (store):
+                if (not isinstance(store, list)):
+                    storelist.append(store)
+                else:
+                    storelist += store
+        matcher = match.matcher(storelist, max_candidates, min_similarity,  max_string_len)
+        self.pickleMatcher(matcher)
+        return matcher
     
+    def removeFile(self):
+        try:
+            filename = self.config.get('General','fileStoreMatcher')
+            os.remove(filename)
+            self.config.remove_option('General', 'fileStoreMatcher')
+        except:
+            pass
+            
