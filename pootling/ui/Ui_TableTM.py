07daaeb58635a00e9c9eee836c30dfdf7ba716c7
Hok Kakada <hokkakada@khmeros.info>
2007-03-28 03:49:27 +0000

    better interface for tableTM
    svn path=/src/trunk/; revision=5302

--- pootling/ui/Ui_TableTM.py
+++ pootling/ui/Ui_TableTM.py
@@ -1,81 +1,78 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'unknown'
+# Form implementation generated from reading ui file '/home/ratha/sourceforge.net/translate/trunk/pootling/ui/TableTM.ui'
 #
-# Created: Mon Mar 26 10:25:17 2007
-#      by: PyQt4 UI code generator 4.0
+# Created: Tue Mar 27 11:30:47 2007
+#      by: PyQt4 UI code generator 4-snapshot-20070212
 #
 # WARNING! All changes made in this file will be lost!
 
-import sys
 from PyQt4 import QtCore, QtGui
 
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(QtCore.QSize(QtCore.QRect(0,0,400,300).size()).expandedTo(Form.minimumSizeHint()))
+        Form.resize(QtCore.QSize(QtCore.QRect(0,0,335,439).size()).expandedTo(Form.minimumSizeHint()))
 
         self.gridlayout = QtGui.QGridLayout(Form)
         self.gridlayout.setMargin(9)
         self.gridlayout.setSpacing(6)
         self.gridlayout.setObjectName("gridlayout")
 
-        self.label_3 = QtGui.QLabel(Form)
-        self.label_3.setObjectName("label_3")
-        self.gridlayout.addWidget(self.label_3,2,0,1,1)
+        self.tblTM = QtGui.QTableWidget(Form)
+        self.tblTM.setObjectName("tblTM")
+        self.gridlayout.addWidget(self.tblTM,3,0,1,3)
 
-        self.label_2 = QtGui.QLabel(Form)
-        self.label_2.setObjectName("label_2")
-        self.gridlayout.addWidget(self.label_2,1,0,1,1)
+        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.gridlayout.addItem(spacerItem,1,2,2,1)
+
+        self.lblTranslator = QtGui.QLabel(Form)
+        self.lblTranslator.setFrameShape(QtGui.QFrame.NoFrame)
+        self.lblTranslator.setObjectName("lblTranslator")
+        self.gridlayout.addWidget(self.lblTranslator,1,1,1,1)
+
+        self.lblDate = QtGui.QLabel(Form)
+        self.lblDate.setFrameShape(QtGui.QFrame.NoFrame)
+        self.lblDate.setObjectName("lblDate")
+        self.gridlayout.addWidget(self.lblDate,2,1,1,1)
+
+        self.lblPath = QtGui.QLabel(Form)
+        self.lblPath.setWindowModality(QtCore.Qt.NonModal)
+        self.lblPath.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
+        self.lblPath.setFrameShape(QtGui.QFrame.NoFrame)
+        self.lblPath.setObjectName("lblPath")
+        self.gridlayout.addWidget(self.lblPath,0,1,1,1)
 
         self.label = QtGui.QLabel(Form)
         self.label.setObjectName("label")
         self.gridlayout.addWidget(self.label,0,0,1,1)
 
+        self.label_3 = QtGui.QLabel(Form)
+        self.label_3.setObjectName("label_3")
+        self.gridlayout.addWidget(self.label_3,1,0,1,1)
+
         self.label_4 = QtGui.QLabel(Form)
         self.label_4.setObjectName("label_4")
-        self.gridlayout.addWidget(self.label_4,3,0,1,1)
-
-        self.lblTranslator = QtGui.QLabel(Form)
-        self.lblTranslator.setObjectName("lblTranslator")
-        self.gridlayout.addWidget(self.lblTranslator,2,1,1,1)
-
-        self.lblPath = QtGui.QLabel(Form)
-        self.lblPath.setObjectName("lblPath")
-        self.gridlayout.addWidget(self.lblPath,0,1,1,1)
-
-        self.lblFile = QtGui.QLabel(Form)
-        self.lblFile.setObjectName("lblFile")
-        self.gridlayout.addWidget(self.lblFile,1,1,1,1)
-
-        self.lblDate = QtGui.QLabel(Form)
-        self.lblDate.setObjectName("lblDate")
-        self.gridlayout.addWidget(self.lblDate,3,1,1,1)
-
-        self.tblTM = QtGui.QTableWidget(Form)
-        self.tblTM.setObjectName("tblTM")
-        self.gridlayout.addWidget(self.tblTM,4,0,1,2)
+        self.gridlayout.addWidget(self.label_4,2,0,1,1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
-    def tr(self, string):
-        return QtGui.QApplication.translate("Form", string, None, QtGui.QApplication.UnicodeUTF8)
-
     def retranslateUi(self, Form):
-        Form.setWindowTitle(self.tr("Lookup"))
-        self.label_3.setText(self.tr("Translator:"))
-        self.label_2.setText(self.tr("Found file:"))
-        self.label.setText(self.tr("Found path:"))
-        self.label_4.setText(self.tr("Date:"))
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Lookup", None, QtGui.QApplication.UnicodeUTF8))
         self.tblTM.setRowCount(0)
         self.tblTM.setColumnCount(2)
         self.tblTM.clear()
         self.tblTM.setColumnCount(2)
         self.tblTM.setRowCount(0)
+        self.label.setText(QtGui.QApplication.translate("Form", "Found in:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("Form", "Translator:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("Form", "Date:", None, QtGui.QApplication.UnicodeUTF8))
+
 
 
 if __name__ == "__main__":
+    import sys
     app = QtGui.QApplication(sys.argv)
     Form = QtGui.QWidget()
     ui = Ui_Form()
