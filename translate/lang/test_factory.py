61dc5c9a67d2b6760f7f7d55ea54beff9de4f984
Friedel Wolff <friedel@translate.org.za>
2007-04-13 14:28:20 +0000

    Test that we can correctly import Icelandic (the code 'is' is a reserved word in python and used to cause problems)
    svn path=/src/trunk/; revision=5485

--- translate/lang/test_factory.py
+++ translate/lang/test_factory.py
@@ -27,3 +27,7 @@ def test_getlanguage():
     # Test with None as language code
     language = factory.getlanguage(None)
     assert language.code == ''
+    
+    #Test with a language code that is a reserved word in Python
+    language = factory.getlanguage('is')
+    assert language.nplurals == 2
