cb1a7287126c9542d5fbdd1c4239639d338d699c
Friedel Wolff <friedel@translate.org.za>
2007-04-13 14:28:58 +0000

    Fix syntax error in joining of punctuation lists
    svn path=/src/trunk/; revision=5486

--- translate/lang/common.py
+++ translate/lang/common.py
@@ -125,8 +125,8 @@ class Common(object):
     miscpunc = u"±°¹²³·©®×£¥€"
     """The middle dot (·) is used by Greek and Georgian."""
 
-    punctuation = u"".join(commonpunc, quotes, invertedpunc, rtlpunc, CJKpunc, 
-            khmerpunc, armenianpunc, miscpunc)
+    punctuation = u"".join([commonpunc, quotes, invertedpunc, rtlpunc, CJKpunc,\
+            khmerpunc, armenianpunc, miscpunc])
     """We include many types of puntuation here, simply since this is only 
     meant to determine if something is punctuation. Hopefully we catch some 
     languages which might not be represented with modules. Most languages won't 
