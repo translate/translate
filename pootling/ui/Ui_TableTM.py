f39253cdcf4ab1cc17cf12c3c815f73631ce5ebf
Hok Kakada <hokkakada@khmeros.info>
2007-05-28 09:17:45 +0000

    make lookup view nicer
    svn path=/src/trunk/; revision=5709

--- pootling/ui/Ui_TableTM.py
+++ pootling/ui/Ui_TableTM.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/k-da/Documents/poxole/trunk/pootling/ui/TableTM.ui'
 #
-# Created: Thu May 10 14:43:56 2007
+# Created: Mon May 28 16:17:10 2007
 #      by: PyQt4 UI code generator 4-snapshot-20070212
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PyQt4 import QtCore, QtGui
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(QtCore.QSize(QtCore.QRect(0,0,192,151).size()).expandedTo(Form.minimumSizeHint()))
+        Form.resize(QtCore.QSize(QtCore.QRect(0,0,227,175).size()).expandedTo(Form.minimumSizeHint()))
 
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(13),QtGui.QSizePolicy.Policy(13))
         sizePolicy.setHorizontalStretch(0)
@@ -29,21 +29,15 @@ class Ui_Form(object):
         self.tblTM = QtGui.QTableWidget(Form)
         self.tblTM.setDragEnabled(True)
         self.tblTM.setObjectName("tblTM")
-        self.gridlayout.addWidget(self.tblTM,0,0,1,2)
+        self.gridlayout.addWidget(self.tblTM,0,0,1,3)
 
-        self.lblTranslator = QtGui.QLabel(Form)
-        self.lblTranslator.setFrameShape(QtGui.QFrame.NoFrame)
-        self.lblTranslator.setObjectName("lblTranslator")
-        self.gridlayout.addWidget(self.lblTranslator,2,1,1,1)
+        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.gridlayout.addItem(spacerItem,1,2,1,1)
 
-        self.label_3 = QtGui.QLabel(Form)
-        self.label_3.setObjectName("label_3")
-        self.gridlayout.addWidget(self.label_3,2,0,1,1)
-
-        self.lblDate = QtGui.QLabel(Form)
-        self.lblDate.setFrameShape(QtGui.QFrame.NoFrame)
-        self.lblDate.setObjectName("lblDate")
-        self.gridlayout.addWidget(self.lblDate,4,1,1,1)
+        self.vboxlayout = QtGui.QVBoxLayout()
+        self.vboxlayout.setMargin(0)
+        self.vboxlayout.setSpacing(6)
+        self.vboxlayout.setObjectName("vboxlayout")
 
         self.lblPath = QtGui.QLabel(Form)
         self.lblPath.setWindowModality(QtCore.Qt.NonModal)
@@ -51,29 +45,60 @@ class Ui_Form(object):
         self.lblPath.setFrameShape(QtGui.QFrame.NoFrame)
         self.lblPath.setWordWrap(False)
         self.lblPath.setObjectName("lblPath")
-        self.gridlayout.addWidget(self.lblPath,1,1,1,1)
+        self.vboxlayout.addWidget(self.lblPath)
 
-        self.label_4 = QtGui.QLabel(Form)
-        self.label_4.setObjectName("label_4")
-        self.gridlayout.addWidget(self.label_4,3,0,2,1)
+        self.lblTranslator = QtGui.QLabel(Form)
+        self.lblTranslator.setFrameShape(QtGui.QFrame.NoFrame)
+        self.lblTranslator.setObjectName("lblTranslator")
+        self.vboxlayout.addWidget(self.lblTranslator)
+
+        self.lblDate = QtGui.QLabel(Form)
+        self.lblDate.setFrameShape(QtGui.QFrame.NoFrame)
+        self.lblDate.setObjectName("lblDate")
+        self.vboxlayout.addWidget(self.lblDate)
+        self.gridlayout.addLayout(self.vboxlayout,1,1,1,1)
+
+        self.vboxlayout1 = QtGui.QVBoxLayout()
+        self.vboxlayout1.setMargin(0)
+        self.vboxlayout1.setSpacing(6)
+        self.vboxlayout1.setObjectName("vboxlayout1")
 
         self.label = QtGui.QLabel(Form)
         self.label.setObjectName("label")
-        self.gridlayout.addWidget(self.label,1,0,1,1)
+        self.vboxlayout1.addWidget(self.label)
+
+        self.label_3 = QtGui.QLabel(Form)
+        self.label_3.setObjectName("label_3")
+        self.vboxlayout1.addWidget(self.label_3)
+
+        self.label_4 = QtGui.QLabel(Form)
+        self.label_4.setObjectName("label_4")
+        self.vboxlayout1.addWidget(self.label_4)
+        self.gridlayout.addLayout(self.vboxlayout1,1,0,1,1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Lookup", None, QtGui.QApplication.UnicodeUTF8))
+        self.tblTM.setWhatsThis(QtGui.QApplication.translate("Form", "<h3>Search Results</h3>This table shows the results of searching in translation memory. Similarity tells you about the seach score. 100% means the source is identical in TM. At the buttom is displayed the location, translator, and translated date of each candidate, row. This table is automatically hiden if the option \" Automatically lookup translation in TM\" under Settings/Preference/TM-Glossary is unchecked.", None, QtGui.QApplication.UnicodeUTF8))
         self.tblTM.setRowCount(0)
         self.tblTM.setColumnCount(2)
         self.tblTM.clear()
         self.tblTM.setColumnCount(2)
         self.tblTM.setRowCount(0)
-        self.label_3.setText(QtGui.QApplication.translate("Form", "Translator:", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("Form", "Date:", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Form", "Found in:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Form", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+        "p, li { white-space: pre-wrap; }\n"
+        "</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Found in:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("Form", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+        "p, li { white-space: pre-wrap; }\n"
+        "</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Translator:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("Form", "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+        "p, li { white-space: pre-wrap; }\n"
+        "</style></head><body style=\" font-family:\'Sans Serif\'; font-size:9pt; font-weight:400; font-style:normal; text-decoration:none;\">\n"
+        "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Date:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
 
 
