0ded96c649f92eeaebac5e815319f6319f6bcb2d
Seth Chanratha <sethchanratha@khmeros.info>
2007-05-31 07:31:40 +0000

    move Configure and buildTM to setting menu and create new and open in project menu
    svn path=/src/trunk/; revision=5719

--- pootling/ui/Ui_Catalog.py
+++ pootling/ui/Ui_Catalog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/ratha/sourceforge.net/translate/trunk/pootling/ui/Catalog.ui'
 #
-# Created: Fri Mar 30 17:21:32 2007
+# Created: Thu May 31 14:27:41 2007
 #      by: PyQt4 UI code generator 4-snapshot-20070212
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,8 @@ from PyQt4 import QtCore, QtGui
 class Ui_Catalog(object):
     def setupUi(self, Catalog):
         Catalog.setObjectName("Catalog")
-        Catalog.resize(QtCore.QSize(QtCore.QRect(0,0,531,400).size()).expandedTo(Catalog.minimumSizeHint()))
+        Catalog.setWindowModality(QtCore.Qt.NonModal)
+        Catalog.resize(QtCore.QSize(QtCore.QRect(0,0,581,412).size()).expandedTo(Catalog.minimumSizeHint()))
 
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(7),QtGui.QSizePolicy.Policy(7))
         sizePolicy.setHorizontalStretch(0)
@@ -33,26 +34,32 @@ class Ui_Catalog(object):
         font = QtGui.QFont(self.treeCatalog.font())
         font.setPointSize(10)
         self.treeCatalog.setFont(font)
+        self.treeCatalog.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.treeCatalog.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.treeCatalog.setAcceptDrops(False)
         self.treeCatalog.setAlternatingRowColors(True)
         self.treeCatalog.setObjectName("treeCatalog")
         self.gridlayout.addWidget(self.treeCatalog,0,0,1,1)
         Catalog.setCentralWidget(self.centralwidget)
 
         self.menubar = QtGui.QMenuBar(Catalog)
-        self.menubar.setGeometry(QtCore.QRect(0,0,531,28))
+        self.menubar.setGeometry(QtCore.QRect(0,0,581,28))
         self.menubar.setObjectName("menubar")
 
         self.menuFile = QtGui.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
 
-        self.menuHelp = QtGui.QMenu(self.menubar)
-        self.menuHelp.setObjectName("menuHelp")
+        self.menuEdit = QtGui.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
 
         self.menuProject = QtGui.QMenu(self.menubar)
         self.menuProject.setObjectName("menuProject")
 
-        self.menuEdit = QtGui.QMenu(self.menubar)
-        self.menuEdit.setObjectName("menuEdit")
+        self.menuHelp = QtGui.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+
+        self.menuSettings = QtGui.QMenu(self.menubar)
+        self.menuSettings.setObjectName("menuSettings")
         Catalog.setMenuBar(self.menubar)
 
         self.statusbar = QtGui.QStatusBar(Catalog)
@@ -61,7 +68,7 @@ class Ui_Catalog(object):
 
         self.toolBar = QtGui.QToolBar(Catalog)
 
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(1),QtGui.QSizePolicy.Policy(1))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(5),QtGui.QSizePolicy.Policy(5))
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.toolBar.sizePolicy().hasHeightForWidth())
@@ -92,28 +99,46 @@ class Ui_Catalog(object):
         self.actionConfigure.setObjectName("actionConfigure")
 
         self.actionFind_in_Files = QtGui.QAction(Catalog)
-        self.actionFind_in_Files.setEnabled(True)
+        self.actionFind_in_Files.setEnabled(False)
         self.actionFind_in_Files.setIcon(QtGui.QIcon("../images/find.png"))
         self.actionFind_in_Files.setObjectName("actionFind_in_Files")
 
         self.actionStatistics = QtGui.QAction(Catalog)
+        self.actionStatistics.setEnabled(False)
         self.actionStatistics.setIcon(QtGui.QIcon("../images/statistic.png"))
         self.actionStatistics.setObjectName("actionStatistics")
 
         self.actionBuildTM = QtGui.QAction(Catalog)
         self.actionBuildTM.setObjectName("actionBuildTM")
+
+        self.actionNew = QtGui.QAction(Catalog)
+        self.actionNew.setObjectName("actionNew")
+
+        self.actionOpen = QtGui.QAction(Catalog)
+        self.actionOpen.setObjectName("actionOpen")
+
+        self.actionBuild = QtGui.QAction(Catalog)
+        self.actionBuild.setIcon(QtGui.QIcon("../images/memory.png"))
+        self.actionBuild.setObjectName("actionBuild")
+
+        self.actionConfigure1 = QtGui.QAction(Catalog)
+        self.actionConfigure1.setIcon(QtGui.QIcon("../images/configure.png"))
+        self.actionConfigure1.setObjectName("actionConfigure1")
         self.menuFile.addAction(self.actionQuit)
-        self.menuHelp.addAction(self.actionAbout)
-        self.menuHelp.addAction(self.actionAboutQt)
-        self.menuProject.addAction(self.actionConfigure)
-        self.menuProject.addAction(self.actionBuildTM)
         self.menuEdit.addAction(self.actionFind_in_Files)
         self.menuEdit.addSeparator()
         self.menuEdit.addAction(self.actionStatistics)
         self.menuEdit.addAction(self.actionReload)
+        self.menuProject.addAction(self.actionNew)
+        self.menuProject.addAction(self.actionOpen)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menuHelp.addAction(self.actionAboutQt)
+        self.menuSettings.addAction(self.actionBuild)
+        self.menuSettings.addAction(self.actionConfigure)
         self.menubar.addAction(self.menuFile.menuAction())
         self.menubar.addAction(self.menuEdit.menuAction())
         self.menubar.addAction(self.menuProject.menuAction())
+        self.menubar.addAction(self.menuSettings.menuAction())
         self.menubar.addAction(self.menuHelp.menuAction())
         self.toolBar.addAction(self.actionQuit)
         self.toolBar.addAction(self.actionFind_in_Files)
@@ -127,9 +152,10 @@ class Ui_Catalog(object):
     def retranslateUi(self, Catalog):
         Catalog.setWindowTitle(QtGui.QApplication.translate("Catalog", "Catalog Manager", None, QtGui.QApplication.UnicodeUTF8))
         self.menuFile.setTitle(QtGui.QApplication.translate("Catalog", "&File", None, QtGui.QApplication.UnicodeUTF8))
-        self.menuHelp.setTitle(QtGui.QApplication.translate("Catalog", "&Help", None, QtGui.QApplication.UnicodeUTF8))
-        self.menuProject.setTitle(QtGui.QApplication.translate("Catalog", "&Project", None, QtGui.QApplication.UnicodeUTF8))
         self.menuEdit.setTitle(QtGui.QApplication.translate("Catalog", "&Edit", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuProject.setTitle(QtGui.QApplication.translate("Catalog", "&Project", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuHelp.setTitle(QtGui.QApplication.translate("Catalog", "&Help", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuSettings.setTitle(QtGui.QApplication.translate("Catalog", "&Settings", None, QtGui.QApplication.UnicodeUTF8))
         self.actionQuit.setText(QtGui.QApplication.translate("Catalog", "&Quit", None, QtGui.QApplication.UnicodeUTF8))
         self.actionQuit.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+Q", None, QtGui.QApplication.UnicodeUTF8))
         self.actionAbout.setText(QtGui.QApplication.translate("Catalog", "About", None, QtGui.QApplication.UnicodeUTF8))
@@ -142,6 +168,10 @@ class Ui_Catalog(object):
         self.actionStatistics.setText(QtGui.QApplication.translate("Catalog", "&Statistics", None, QtGui.QApplication.UnicodeUTF8))
         self.actionStatistics.setShortcut(QtGui.QApplication.translate("Catalog", "Ctrl+S", None, QtGui.QApplication.UnicodeUTF8))
         self.actionBuildTM.setText(QtGui.QApplication.translate("Catalog", "&Build TM", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionNew.setText(QtGui.QApplication.translate("Catalog", "New...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionOpen.setText(QtGui.QApplication.translate("Catalog", "Open...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionBuild.setText(QtGui.QApplication.translate("Catalog", "Build TM...", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionConfigure1.setText(QtGui.QApplication.translate("Catalog", "Configure...", None, QtGui.QApplication.UnicodeUTF8))
 
 
 
