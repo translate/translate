f4cac66906ed638b82fd72fbc7d8ab1db4710eba
Friedel Wolff <friedel@translate.org.za>
2007-06-07 12:27:57 +0000

    Test that the indefinite article('n) doesn't confuse capsstart()
    svn path=/src/trunk/; revision=5774

--- translate/lang/test_af.py
+++ translate/lang/test_af.py
@@ -10,3 +10,14 @@ def test_sentences():
     assert sentences == [u"Normal case.", "Nothing interesting."]
     sentences = language.sentences(u"Wat? 'n Fout?")
     assert sentences == [u"Wat?", "'n Fout?"]
+
+def test_capsstart():
+    """Tests that the indefinite article ('n) doesn't confuse startcaps()."""
+    language = factory.getlanguage('af')
+    assert language.capsstart("Koeie kraam koeie")
+    assert language.capsstart("'Koeie' kraam koeie")
+    assert not language.capsstart("koeie kraam koeie")
+    assert language.capsstart("'n Koei kraam koeie")
+    assert language.capsstart("'n 'Koei' kraam koeie")
+    assert not language.capsstart("'n oei kraam koeie")
+
