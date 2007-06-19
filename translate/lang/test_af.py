f051a5c75460a72986c228959af5ba224c36a828
Friedel Wolff <friedel@translate.org.za>
2007-06-19 08:24:28 +0000

    Test for more cases of the indefinite article ('n)
    svn path=/src/trunk/; revision=5887

--- translate/lang/test_af.py
+++ translate/lang/test_af.py
@@ -10,6 +10,8 @@ def test_sentences():
     assert sentences == [u"Normal case.", "Nothing interesting."]
     sentences = language.sentences(u"Wat? 'n Fout?")
     assert sentences == [u"Wat?", "'n Fout?"]
+    sentences = language.sentences(u"Dit sal a.g.v. 'n fout gebeur.")
+    assert sentences == [u"Dit sal a.g.v. 'n fout gebeur."]
 
 def test_capsstart():
     """Tests that the indefinite article ('n) doesn't confuse startcaps()."""
@@ -17,7 +19,9 @@ def test_capsstart():
     assert language.capsstart("Koeie kraam koeie")
     assert language.capsstart("'Koeie' kraam koeie")
     assert not language.capsstart("koeie kraam koeie")
+    assert language.capsstart("\n\nKoeie kraam koeie")
     assert language.capsstart("'n Koei kraam koeie")
     assert language.capsstart("'n 'Koei' kraam koeie")
-    assert not language.capsstart("'n oei kraam koeie")
+    assert not language.capsstart("'n koei kraam koeie")
+    assert language.capsstart("\n\n'n Koei kraam koeie")
 
