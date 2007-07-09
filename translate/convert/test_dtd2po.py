4493547da963c4228cc30592603638a48d269b58
Friedel Wolff <friedel@translate.org.za>
2007-07-09 08:23:19 +0000

    Specify the file encoding to make python 2.5 happy
    svn path=/src/trunk/; revision=6083

--- translate/convert/test_dtd2po.py
+++ translate/convert/test_dtd2po.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from translate.convert import dtd2po
 from translate.convert import test_convert
--- translate/convert/test_po2prop.py
+++ translate/convert/test_po2prop.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from translate.convert import po2prop
 from translate.convert import prop2po
