abf3333bb8d651da2bdb27aea8dd26ca0e3b14e8
Friedel Wolff <friedel@translate.org.za>
2007-09-11 08:07:55 +0000

    Test Afrikaans specific startcaps more exhaustively
    svn path=/src/trunk/; revision=6506

--- translate/filters/test_checks.py
+++ translate/filters/test_checks.py
@@ -523,7 +523,16 @@ def test_startcaps():
     # Language specific stuff
     stdchecker = checks.StandardChecker(checks.CheckerConfig(targetlanguage='af'))
     assert passes(stdchecker.startcaps, "A cow", "'n Koei")
+    assert passes(stdchecker.startcaps, "A list of ", "'n Lys van ")
+    # should pass:
+    #assert passes(stdchecker.startcaps, "A 1k file", u"'n 1k-lêer")
+    assert passes(stdchecker.startcaps, "'Do it'", "'Doen dit'")
+    assert fails(stdchecker.startcaps, "'Closer than'", "'nader as'")
+    assert passes(stdchecker.startcaps, "List", "Lys")
+    assert passes(stdchecker.startcaps, "a cow", "'n koei")
     assert fails(stdchecker.startcaps, "a cow", "'n Koei")
+    assert passes(stdchecker.startcaps, "(A cow)", "('n Koei)")
+    assert fails(stdchecker.startcaps, "(a cow)", "('n Koei)")
 
 def test_startpunc():
     """tests startpunc"""
