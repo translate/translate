e3b1036e0eb3e8bd0d46a2701c958b373b6b4349
Hok Kakada <hokkakada@khmeros.info>
2007-09-11 04:21:50 +0000

    change title from  Setting Catalog Manager to Catalog Manager Preferences
    svn path=/src/trunk/; revision=6501

--- pootling/ui/Ui_CatalogSetting.py
+++ pootling/ui/Ui_CatalogSetting.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'unknown'
+# Form implementation generated from reading ui file '/home/k-da/Documents/poxole/trunk/pootling/ui/CatalogSetting.ui'
 #
-# Created: Fri Sep  7 17:00:15 2007
-#      by: PyQt4 UI code generator 4.0
+# Created: Tue Sep 11 10:39:26 2007
+#      by: PyQt4 UI code generator 4-snapshot-20070212
 #
 # WARNING! All changes made in this file will be lost!
 
-import sys
 from PyQt4 import QtCore, QtGui
 
-class Ui_catalogSetting(object):
-    def setupUi(self, catalogSetting):
-        catalogSetting.setObjectName("catalogSetting")
-        catalogSetting.resize(QtCore.QSize(QtCore.QRect(0,0,359,250).size()).expandedTo(catalogSetting.minimumSizeHint()))
+class Ui_catPreferences(object):
+    def setupUi(self, catPreferences):
+        catPreferences.setObjectName("catPreferences")
+        catPreferences.resize(QtCore.QSize(QtCore.QRect(0,0,359,250).size()).expandedTo(catPreferences.minimumSizeHint()))
 
-        self.gridlayout = QtGui.QGridLayout(catalogSetting)
+        self.gridlayout = QtGui.QGridLayout(catPreferences)
         self.gridlayout.setMargin(9)
         self.gridlayout.setSpacing(6)
         self.gridlayout.setObjectName("gridlayout")
 
-        self.frame = QtGui.QFrame(catalogSetting)
+        self.frame = QtGui.QFrame(catPreferences)
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
         self.frame.setObjectName("frame")
@@ -77,7 +76,7 @@ class Ui_catalogSetting(object):
         spacerItem1 = QtGui.QSpacerItem(191,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
         self.gridlayout.addItem(spacerItem1,2,0,1,1)
 
-        self.label = QtGui.QLabel(catalogSetting)
+        self.label = QtGui.QLabel(catPreferences)
 
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(5),QtGui.QSizePolicy.Policy(0))
         sizePolicy.setHorizontalStretch(0)
@@ -87,58 +86,57 @@ class Ui_catalogSetting(object):
         self.label.setObjectName("label")
         self.gridlayout.addWidget(self.label,0,0,1,2)
 
-        self.btnOk = QtGui.QPushButton(catalogSetting)
+        self.btnOk = QtGui.QPushButton(catPreferences)
         self.btnOk.setObjectName("btnOk")
         self.gridlayout.addWidget(self.btnOk,2,1,1,1)
 
-        self.retranslateUi(catalogSetting)
-        QtCore.QMetaObject.connectSlotsByName(catalogSetting)
-        catalogSetting.setTabOrder(self.chbname,self.chbtranslated)
-        catalogSetting.setTabOrder(self.chbtranslated,self.chbfuzzy)
-        catalogSetting.setTabOrder(self.chbfuzzy,self.chbuntranslated)
-        catalogSetting.setTabOrder(self.chbuntranslated,self.chbtotal)
-        catalogSetting.setTabOrder(self.chbtotal,self.chbSVN)
-        catalogSetting.setTabOrder(self.chbSVN,self.chblastrevision)
-        catalogSetting.setTabOrder(self.chblastrevision,self.chbtranslator)
-        catalogSetting.setTabOrder(self.chbtranslator,self.btnOk)
+        self.retranslateUi(catPreferences)
+        QtCore.QMetaObject.connectSlotsByName(catPreferences)
+        catPreferences.setTabOrder(self.chbname,self.chbtranslated)
+        catPreferences.setTabOrder(self.chbtranslated,self.chbfuzzy)
+        catPreferences.setTabOrder(self.chbfuzzy,self.chbuntranslated)
+        catPreferences.setTabOrder(self.chbuntranslated,self.chbtotal)
+        catPreferences.setTabOrder(self.chbtotal,self.chbSVN)
+        catPreferences.setTabOrder(self.chbSVN,self.chblastrevision)
+        catPreferences.setTabOrder(self.chblastrevision,self.chbtranslator)
+        catPreferences.setTabOrder(self.chbtranslator,self.btnOk)
 
-    def tr(self, string):
-        return QtGui.QApplication.translate("catalogSetting", string, None, QtGui.QApplication.UnicodeUTF8)
+    def retranslateUi(self, catPreferences):
+        catPreferences.setWindowTitle(QtGui.QApplication.translate("catPreferences", "Catalog Manager Preferences", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbSVN.setToolTip(QtGui.QApplication.translate("catPreferences", "CVS/SVN Status", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbSVN.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>CVS/SVN Status</h3>Use this to checked/unchecked to display status of file in local or cvs/svn server on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbSVN.setText(QtGui.QApplication.translate("catPreferences", "CVS/SVN Status", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbname.setToolTip(QtGui.QApplication.translate("catPreferences", "Name", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbname.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Name</h3>Use this to checked/unchecked to display file name on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbname.setText(QtGui.QApplication.translate("catPreferences", "Name", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslator.setToolTip(QtGui.QApplication.translate("catPreferences", "Last Translator", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslator.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Last Translator</h3>Use this to checked/unchecked to display last translator\'s name in file on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslator.setText(QtGui.QApplication.translate("catPreferences", "Last Translator", None, QtGui.QApplication.UnicodeUTF8))
+        self.chblastrevision.setToolTip(QtGui.QApplication.translate("catPreferences", "Last Revision", None, QtGui.QApplication.UnicodeUTF8))
+        self.chblastrevision.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Last Revision</h3>Use this to checked/unchecked to display date/time in file was saved by the last translator on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chblastrevision.setText(QtGui.QApplication.translate("catPreferences", "Last Revision", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbuntranslated.setToolTip(QtGui.QApplication.translate("catPreferences", "Untranslated", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbuntranslated.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Untranslated</h3>Use this to checked/unchecked to display number of string in file was untranslated on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbuntranslated.setText(QtGui.QApplication.translate("catPreferences", "Untranslated", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbfuzzy.setToolTip(QtGui.QApplication.translate("catPreferences", "Fuzzy", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbfuzzy.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Fuzzy</h3>Use this to checked/unchecked to display fuzzy number in file on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbfuzzy.setText(QtGui.QApplication.translate("catPreferences", "Fuzzy", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslated.setToolTip(QtGui.QApplication.translate("catPreferences", "Translated", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslated.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Translated</h3>Use this to checked/unchecked to display number of string in file was translated on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtranslated.setText(QtGui.QApplication.translate("catPreferences", "Translated", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtotal.setToolTip(QtGui.QApplication.translate("catPreferences", "Total", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtotal.setWhatsThis(QtGui.QApplication.translate("catPreferences", "<h3>Total</h3>Use this to checked/unchecked to display total number of strings in file on the catalog manager.", None, QtGui.QApplication.UnicodeUTF8))
+        self.chbtotal.setText(QtGui.QApplication.translate("catPreferences", "Total", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("catPreferences", "Show Columns:", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnOk.setText(QtGui.QApplication.translate("catPreferences", "&OK", None, QtGui.QApplication.UnicodeUTF8))
 
-    def retranslateUi(self, catalogSetting):
-        catalogSetting.setWindowTitle(self.tr("Catalog Settings"))
-        self.chbSVN.setToolTip(self.tr("CVS/SVN Status"))
-        self.chbSVN.setWhatsThis(self.tr("<h3>CVS/SVN Status</h3>Use this to checked/unchecked to display status of file in local or cvs/svn server on the catalog manager."))
-        self.chbSVN.setText(self.tr("CVS/SVN Status"))
-        self.chbname.setToolTip(self.tr("Name"))
-        self.chbname.setWhatsThis(self.tr("<h3>Name</h3>Use this to checked/unchecked to display file name on the catalog manager."))
-        self.chbname.setText(self.tr("Name"))
-        self.chbtranslator.setToolTip(self.tr("Last Translator"))
-        self.chbtranslator.setWhatsThis(self.tr("<h3>Last Translator</h3>Use this to checked/unchecked to display last translator\'s name in file on the catalog manager."))
-        self.chbtranslator.setText(self.tr("Last Translator"))
-        self.chblastrevision.setToolTip(self.tr("Last Revision"))
-        self.chblastrevision.setWhatsThis(self.tr("<h3>Last Revision</h3>Use this to checked/unchecked to display date/time in file was saved by the last translator on the catalog manager."))
-        self.chblastrevision.setText(self.tr("Last Revision"))
-        self.chbuntranslated.setToolTip(self.tr("Untranslated"))
-        self.chbuntranslated.setWhatsThis(self.tr("<h3>Untranslated</h3>Use this to checked/unchecked to display number of string in file was untranslated on the catalog manager."))
-        self.chbuntranslated.setText(self.tr("Untranslated"))
-        self.chbfuzzy.setToolTip(self.tr("Fuzzy"))
-        self.chbfuzzy.setWhatsThis(self.tr("<h3>Fuzzy</h3>Use this to checked/unchecked to display fuzzy number in file on the catalog manager."))
-        self.chbfuzzy.setText(self.tr("Fuzzy"))
-        self.chbtranslated.setToolTip(self.tr("Translated"))
-        self.chbtranslated.setWhatsThis(self.tr("<h3>Translated</h3>Use this to checked/unchecked to display number of string in file was translated on the catalog manager."))
-        self.chbtranslated.setText(self.tr("Translated"))
-        self.chbtotal.setToolTip(self.tr("Total"))
-        self.chbtotal.setWhatsThis(self.tr("<h3>Total</h3>Use this to checked/unchecked to display total number of strings in file on the catalog manager."))
-        self.chbtotal.setText(self.tr("Total"))
-        self.label.setText(self.tr("Show Columns:"))
-        self.btnOk.setText(self.tr("&OK"))
 
 
 if __name__ == "__main__":
+    import sys
     app = QtGui.QApplication(sys.argv)
-    catalogSetting = QtGui.QWidget()
-    ui = Ui_catalogSetting()
-    ui.setupUi(catalogSetting)
-    catalogSetting.show()
+    catPreferences = QtGui.QWidget()
+    ui = Ui_catPreferences()
+    ui.setupUi(catPreferences)
+    catPreferences.show()
     sys.exit(app.exec_())
