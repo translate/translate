36893a31d746e18653cd4c01f064359a34484813
Friedel Wolff <friedel@translate.org.za>
2008-01-09 06:21:11 +0000

    Take differences between differernt versions of optparse into account for testing converter help text in test_help()
    svn path=/src/trunk/; revision=6799

--- translate/convert/test_convert.py
+++ translate/convert/test_convert.py
@@ -105,8 +105,12 @@ class TestConvertCommand:
         # the convertsummary might be wrapped. this will probably unwrap it
         assert convertsummary in help_string.replace("\n", " ")
         usageline = help_string[:help_string.find("\n")]
-        assert usageline.startswith("Usage: ") and "[--version] [-h|--help]" in usageline
-        options = help_string[help_string.find("Options:\n"):]
+        # Different versions of optparse might contain either upper or 
+        # lowercase versions of 'Usage:' and 'Options:', so we need to take 
+        # that into account
+        assert (usageline.startswith("Usage: ") or usageline.startswith("usage: ")) \
+            and "[--version] [-h|--help]" in usageline
+        options = help_string[help_string.find("ptions:\n"):]
         options = options[options.find("\n")+1:]
         options = self.help_check(options, "--progress=PROGRESS")
         options = self.help_check(options, "--version")
