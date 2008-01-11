a62ff4d1e67faf893e383dc617e1220a7679eb26
Friedel Wolff <friedel@translate.org.za>
2008-01-11 08:18:38 +0000

    Change current spacing convention of double spaces to four spaces
    svn path=/src/trunk/; revision=6837

--- translate/tools/pocompile.py
+++ translate/tools/pocompile.py
@@ -27,40 +27,40 @@ from translate.storage import mo
 
 class POCompile:
 
-  def convertfile(self, inputfile, includefuzzy=False):
-    outputfile = mo.mofile()
-    for unit in inputfile.units:
-      if unit.istranslated() or (unit.isfuzzy() and includefuzzy and unit.target):
-        mounit = mo.mounit()
-        if unit.isheader():
-          mounit.source = ""
-        else:
-          mounit.source = unit.source
-          if hasattr(unit, "msgidcomments"):
-            mounit.source.strings[0] = po.unquotefrompo(unit.msgidcomments) + mounit.source.strings[0]
-          if hasattr(unit, "msgctxt"):
-            mounit.msgctxt = po.unquotefrompo(unit.msgctxt)
-        mounit.target = unit.target
-        outputfile.addunit(mounit)
-    return str(outputfile)
+    def convertfile(self, inputfile, includefuzzy=False):
+        outputfile = mo.mofile()
+        for unit in inputfile.units:
+            if unit.istranslated() or (unit.isfuzzy() and includefuzzy and unit.target):
+                mounit = mo.mounit()
+                if unit.isheader():
+                    mounit.source = ""
+                else:
+                    mounit.source = unit.source
+                    if hasattr(unit, "msgidcomments"):
+                        mounit.source.strings[0] = po.unquotefrompo(unit.msgidcomments) + mounit.source.strings[0]
+                    if hasattr(unit, "msgctxt"):
+                        mounit.msgctxt = po.unquotefrompo(unit.msgctxt)
+                mounit.target = unit.target
+                outputfile.addunit(mounit)
+        return str(outputfile)
 
 def convertmo(inputfile, outputfile, templatefile, includefuzzy=False):
-  """reads in a base class derived inputfile, converts using pocompile, writes to outputfile"""
-  # note that templatefile is not used, but it is required by the converter...
-  inputstore = factory.getobject(inputfile)
-  if inputstore.isempty():
-    return 0
-  convertor = POCompile()
-  outputmo = convertor.convertfile(inputstore, includefuzzy)
-  outputfile.write(outputmo)
-  return 1
+    """reads in a base class derived inputfile, converts using pocompile, writes to outputfile"""
+    # note that templatefile is not used, but it is required by the converter...
+    inputstore = factory.getobject(inputfile)
+    if inputstore.isempty():
+        return 0
+    convertor = POCompile()
+    outputmo = convertor.convertfile(inputstore, includefuzzy)
+    outputfile.write(outputmo)
+    return 1
 
 def main():
-  from translate.convert import convert
-  formats = {"po":("mo", convertmo), "xlf":("mo", convertmo)}
-  parser = convert.ConvertOptionParser(formats, usepots=False, description=__doc__)
-  parser.add_fuzzy_option()
-  parser.run()
+    from translate.convert import convert
+    formats = {"po":("mo", convertmo), "xlf":("mo", convertmo)}
+    parser = convert.ConvertOptionParser(formats, usepots=False, description=__doc__)
+    parser.add_fuzzy_option()
+    parser.run()
 
 if __name__ == '__main__':
-  main()
+    main()
