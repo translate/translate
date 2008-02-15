d76f9bbafa5f36d144a50495f06be24ddf4cc9d4
Friedel Wolff <friedel@translate.org.za>
2008-02-15 09:28:04 +0000

    Fix incorrect variable name in xerror2_cb()
    svn path=/src/trunk/; revision=6997

--- translate/storage/cpo.py
+++ translate/storage/cpo.py
@@ -81,7 +81,7 @@ def xerror_cb(severity, message, filename, lineno, column, multilint_p, message_
 def xerror2_cb(severity, message1, filename1, lineno1, column1, multiline_p1, message_text1, message2, filename2, lineno2, column2, multiline_p2, message_text2):
     print >> sys.stderr, "xerror2_cb", severity, message1, filename1, lineno1, column1, multiline_p1, message_text1, message2, filename2, lineno2, column2, multiline_p2, message_text2
     if severity == 2:
-        raise ValueError(message_text)
+        raise ValueError(message_text1)
 
 
 
