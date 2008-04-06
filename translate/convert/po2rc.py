5318552cf9d999d7061f82f76fe7fe6e47acdc4e
Dwayne Bailey <dwayne@translate.org.za>
2008-04-06 19:31:28 +0000

    Add --charset output option to allow user to choose the output encoding, defaults to utf-8
    svn path=/src/trunk/; revision=7135

--- translate/convert/po2rc.py
+++ translate/convert/po2rc.py
@@ -33,18 +33,20 @@ from translate.storage import rc
 eol = "\n"
 
 class rerc:
-    def __init__(self, templatefile):
+    def __init__(self, templatefile, charset="utf-8"):
         self.templatefile = templatefile
         self.templatestore = rc.rcfile(templatefile)
         self.inputdict = {}
+        self.charset = charset
 
     def convertstore(self, inputstore, includefuzzy=False):
         self.makestoredict(inputstore, includefuzzy)
         outputblocks = []
         for block in self.templatestore.blocks:
             outputblocks.append(self.convertblock(block))
-        outputblocks.insert(0, "#pragma code_page(65001)\n")
-        outputblocks.append("#pragma code_page(default)")
+        if self.charset == "utf-8":
+            outputblocks.insert(0, "#pragma code_page(65001)\n")
+            outputblocks.append("#pragma code_page(default)")
         return outputblocks
 
     def makestoredict(self, store, includefuzzy=False):
@@ -68,16 +70,16 @@ class rerc:
                     newmatch = unit.match.group().replace(unit.match.groupdict()['value'], self.inputdict[location])
                     newblock = newblock.replace(unit.match.group(), newmatch)
         if isinstance(newblock, unicode):
-            newblock = newblock.encode('utf-8')
+            newblock = newblock.encode(self.charset)
         return newblock
 
-def convertrc(inputfile, outputfile, templatefile, includefuzzy=False):
+def convertrc(inputfile, outputfile, templatefile, includefuzzy=False, charset=None):
     inputstore = po.pofile(inputfile)
     if templatefile is None:
         raise ValueError("must have template file for rc files")
         # convertor = po2rc()
     else:
-        convertor = rerc(templatefile)
+        convertor = rerc(templatefile, charset)
     outputrclines = convertor.convertstore(inputstore, includefuzzy)
     outputfile.writelines(outputrclines)
     return 1
@@ -87,6 +89,10 @@ def main(argv=None):
     from translate.convert import convert
     formats = {("po", "rc"): ("rc", convertrc)}
     parser = convert.ConvertOptionParser(formats, usetemplates=True, description=__doc__)
+    defaultcharset = "utf-8"
+    parser.add_option("", "--charset", dest="charset", default=defaultcharset,
+        help="charset to use to decode the RC files (default: %s)" % defaultcharset, metavar="CHARSET")
+    parser.passthrough.append("charset")
     parser.add_fuzzy_option()
     parser.run(argv)
 
