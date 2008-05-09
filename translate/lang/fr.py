9ebaaf439c944ac1e0cd09604579fdfb7d7f4156
Friedel Wolff <friedel@translate.org.za>
2008-05-09 11:04:41 +0000

    Factor out the guillemets transformation into a separate function for better reuse
    svn path=/src/trunk/; revision=7382

--- translate/lang/fr.py
+++ translate/lang/fr.py
@@ -27,6 +27,21 @@ For more information, see U{http://en.wikipedia.org/wiki/French_language}
 from translate.lang import common
 import re
 
+def guillemets(text):
+    def convertquotation(match):
+        prefix = match.group(1)
+        # Let's see that we didn't perhaps match an XML tag property like
+        # <a href="something">
+        if prefix == u"=":
+            return match.group(0)
+        return u"%s« %s »" % (prefix, match.group(2))
+
+    # Check that there is an even number of double quotes, otherwise it is
+    # probably not safe to convert them.
+    if text.count(u'"') % 2 == 0:
+        text = re.sub('(.|^)"([^"]+)"', convertquotation, text)
+    return text
+
 class fr(common.Common):
     """This class represents French."""
     code = "fr"
@@ -51,19 +66,5 @@ class fr(common.Common):
             - Double spaces might be introduced
         """
         text = super(cls, cls).punctranslate(text)
-
-        def convertquotation(match):
-            prefix = match.group(1)
-            # Let's see that we didn't perhaps match an XML tag property like
-            # <a href="something">
-            if prefix == u"=":
-                return match.group(0)
-            return u"%s« %s »" % (prefix, match.group(2))
-        
-        # Check that there is an even number of double quotes, otherwise it is
-        # probably not safe to convert them.
-        if text.count(u'"') % 2 == 0:
-            text = re.sub('(.|^)"([^"]+)"', convertquotation, text)
-
-        return text
+        return guillemets(text)
     punctranslate = classmethod(punctranslate)
