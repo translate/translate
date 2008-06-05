5d19eff36b74810565399106f95f563a1b0ddbcc
Friedel Wolff <friedel@translate.org.za>
2008-06-05 17:10:29 +0000

    Add new commandline option
    svn path=/src/trunk/; revision=7504

--- translate/convert/test_ts2po.py
+++ translate/convert/test_ts2po.py
@@ -114,4 +114,5 @@ class TestTS2POCommand(test_convert.TestConvertCommand, TestTS2PO):
     def test_help(self):
         """tests getting help"""
         options = test_convert.TestConvertCommand.test_help(self)
+        options = self.help_check(options, "--duplicates=DUPLICATESTYLE")
         options = self.help_check(options, "-P, --pot", last=True)
