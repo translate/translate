67bb983a91a461c109058e65fa0f0b12f8ff93aa
Wynand Winterbach <wynand@translate.org.za>
2008-08-04 09:02:39 +0000

    Added a basic functional test to ensure that our tools produce valid XLIFF 1.1 files.
    Currently, there is only one test, which uses oo2xliff to convert an
    OpenOffice.org SDF file to a set of XLIFF files. xmllint then checks
    each of these files against the XLIFF 1.1 Schema.
    
    svn path=/src/trunk/translate/tests/; revision=7971

---
+++ tests/xliff_conformance/test_xliff_conformance.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2007 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import os
+import os.path as path
+from subprocess import call
+
+def xmllint(fullpath):
+    return call(['xmllint', '--noout', '--schema', 'xliff-core-1.1.xsd', fullpath]) == 0
+
+def setup_module(module):
+    os.chdir(path.dirname(__file__))
+
+def find_files(base, check_ext):
+    for dirpath, _dirnames, filenames in os.walk(base):
+        for filename in filenames:
+            fullpath = path.join(dirpath, filename)
+            _namepath, ext = path.splitext(fullpath)
+            if check_ext == ext:
+                yield fullpath
+
+def test_open_office_to_xliff():
+    assert call(['oo2xliff', 'en-US.sdf', '-l', 'fr', 'fr']) == 0
+    for filepath in find_files('fr', '.xlf'):
+        assert xmllint(filepath)
+
+def teardown_module(module):
+    # TODO: Add code to remove the automatically generated files
+    pass
