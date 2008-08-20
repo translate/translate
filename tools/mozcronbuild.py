172691d87ef908852ebf4912b6fb9fb555bb5bf6
Walter Leibbrandt <walter@translate.org.za>
2008-08-20 09:32:18 +0000

    Removed e-mailing support from mozcronbuild.py; the MAILTO environmental variable will be used in the cron-script.
    svn path=/src/trunk/; revision=8124

--- tools/mozcronbuild.py
+++ tools/mozcronbuild.py
@@ -24,23 +24,17 @@ from tools import moz_l10n_builder
 
 
 MOZDIR = os.path.join( os.path.expanduser('~'), 'mozbuild' )
-# E-mail details
-FROM_ADDRESS = 'mozl10n@translate.org.za'
-SMTP_SERVER = 'localhost'
-SMTP_USER = ''
-SMTP_PASS = ''
 
-
-def build_langs(checkout=False):
+def build_langs():
     olddir = os.getcwd()
     os.chdir(MOZDIR)
 
     moz_l10n_builder.main(
         langs='ALL',
-        mozcheckout=checkout,
+        mozcheckout=True,
         recover=True,
         potpack=True,
-        popack=True
+        popack=True,
         update_trans=True,
     )
 
@@ -49,24 +43,6 @@ def build_langs(checkout=False):
 def check_potpacks():
     """Copy new and check available POT-packs."""
     pass
-
-def send_mail(subject, text, addresses):
-    import email.Message
-    import smtplib
-
-    smtp = smtplib.SMTP(SMTP_SERVER)
-    smtp.login(SMTP_USER, SMTP_PASS)
-
-    for address in addresses:
-        message = email.Message.Message()
-        message['To'] = address
-        message['From'] = FROM_ADDRESS
-        message['Subject'] = subject
-        message.set_payload(text)
-
-        smtp.sendmail(FROM_ADDRESS, address, message.as_string())
-
-    smtp.quit()
 
 def update_rss():
     """Update the RSS feed with the available POT-packs."""
@@ -80,42 +56,21 @@ def create_option_parser():
 
     parser = OptionParser(usage=USAGE)
 
-    parser.add_option(
-        '-m', '--mailto',
-        dest='mailto',
-        action='append',
-        help='Add the given e-mail address to the people that should receive the output via e-mail'
-    )
-
     return parser
 
-def main(mailto=[]):
-    checkout = False
-
+def main():
     if not os.path.isdir(MOZDIR):
         os.makedirs(MOZDIR)
-        checkout = True
 
-    import StringIO
-    import sys
-    output = StringIO.StringIO()
-    streambackups = (sys.stdout, sys.stderr)
-    sys.stdout = output
-    sys.stderr = output
-
-    build_langs(checkout=checkout)
+    build_langs()
     check_potpacks()
     update_rss()
 
-    if mailto:
-        send_mail('mozcronbuild.py output', output.getvalue(), mailto)
-
-    sys.stdout, sys.stderr = streambackups
-
 def main_cmd_line():
+    """Processes command-line arguments and send them to main()."""
     options, args = create_option_parser().parse_args()
 
-    main(mailto=options.mailto)
+    main()
 
 if __name__ == '__main__':
     main_cmd_line()
