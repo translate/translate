6af53d8a73460feea4f047e2090e2b65aabc4c5c
Friedel Wolff <friedel@translate.org.za>
2008-08-27 13:21:09 +0000

    Add the jquery js directory
    svn path=/src/trunk/; revision=8215

--- pootlesetup.py
+++ pootlesetup.py
@@ -201,6 +201,7 @@ def getdatafiles():
   pootlefiles.append(listfiles(join('Pootle', 'html')))
   pootlefiles.append(listfiles(join('Pootle', 'html', 'images')))
   pootlefiles.append(listfiles(join('Pootle', 'html', 'js')))
+  pootlefiles.append(listfiles(join('Pootle', 'html', 'js', 'jquery')))
   pootlefiles.append(listfiles(join('Pootle', 'html', 'doc')))
   pootlefiles.append(listfiles(join('Pootle', 'html', 'doc', 'en')))
   pootlefiles.append(listfiles(join('Pootle', 'templates')))
