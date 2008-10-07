c4c78b32edbac6f64f2f95967b8c6a59f2b6d97d
Friedel Wolff <friedel@translate.org.za>
2008-10-07 12:23:51 +0000

    Massive renaming to clarify store vs file. Also got rid of several PO references.
    svn path=/src/trunk/; revision=8696

--- translate/convert/pot2po.py
+++ translate/convert/pot2po.py
@@ -45,19 +45,21 @@ def memory(tmfiles, max_candidates=1, min_similarity=75, max_length=1000):
         tmmatcher = match.matcher(tmstore, max_candidates=max_candidates, min_similarity=min_similarity, max_length=max_length)
     return tmmatcher
 
-def convertpot(inputpotfile, outputpofile, templatepofile, tm=None, min_similarity=75, fuzzymatching=True, **kwargs):
-    inputpot = po.pofile(inputpotfile)
-    templatepo = None
-    if templatepofile is not None:
-        templatepo = po.pofile(templatepofile)
-    outputpo = convertpot_stores(inputpot, templatepo, tm, min_similarity, fuzzymatching, **kwargs)
-    outputpofile.write(str(outputpo))
+def convertpot(input_file, output_file, template_file, tm=None, min_similarity=75, fuzzymatching=True, **kwargs):
+    input_store = po.pofile(input_file)
+    template_store = None
+    if template_file is not None:
+        template_store = po.pofile(template_file)
+    outputpo = convert_stores(input_store, template_store, tm, min_similarity, fuzzymatching, **kwargs)
+    output_file.write(str(outputpo))
     return 1
 
-def convertpot_stores(inputpot, templatepo, tm=None, min_similarity=75, fuzzymatching=True, **kwargs):
-    """reads in inputpotfile, adjusts header, writes to outputpofile. if templatepofile exists, merge translations from it into outputpofile"""
-    inputpot.makeindex()
-    thetargetfile = po.pofile()
+def convert_stores(input_store, template_store, tm=None, min_similarity=75, fuzzymatching=True, **kwargs):
+    """Adjusts the header of input_store, returns output_store. If 
+    template_store exists, merge translations from it into output_store which 
+    is returned"""
+    input_store.makeindex()
+    output_store = po.pofile()
     # header values
     charset = "UTF-8"
     encoding = "8bit"
@@ -69,21 +71,21 @@ def convertpot_stores(inputpot, templatepo, tm=None, min_similarity=75, fuzzymat
     mime_version = None
     plural_forms = None
     kwargs = {}
-    if templatepo is not None:
+    if template_store is not None:
         fuzzyfilematcher = None
         if fuzzymatching:
-            for unit in templatepo.units:
+            for unit in template_store.units:
                 if unit.isobsolete():
                     unit.resurrect()
             try:
-                fuzzyfilematcher = match.matcher(templatepo, max_candidates=1, min_similarity=min_similarity, max_length=3000, usefuzzy=True)
+                fuzzyfilematcher = match.matcher(template_store, max_candidates=1, min_similarity=min_similarity, max_length=3000, usefuzzy=True)
                 fuzzyfilematcher.addpercentage = False
             except ValueError:
                 # Probably no usable units
                 pass
 
-        templatepo.makeindex()
-        templateheadervalues = templatepo.parseheader()
+        template_store.makeindex()
+        templateheadervalues = template_store.parseheader()
         for key, value in templateheadervalues.iteritems():
             if key == "Project-Id-Version":
                 project_id_version = value
@@ -108,7 +110,7 @@ def convertpot_stores(inputpot, templatepo, tm=None, min_similarity=75, fuzzymat
     if fuzzymatching and tm:
         fuzzyglobalmatcher = memory(tm, max_candidates=1, min_similarity=min_similarity, max_length=1000)
         fuzzyglobalmatcher.addpercentage = False
-    inputheadervalues = inputpot.parseheader()
+    inputheadervalues = input_store.parseheader()
     for key, value in inputheadervalues.iteritems():
         if key in ("Project-Id-Version", "Last-Translator", "Language-Team", "PO-Revision-Date", "Content-Type", "Content-Transfer-Encoding", "Plural-Forms"):
             # want to carry these from the template so we ignore them
@@ -119,77 +121,77 @@ def convertpot_stores(inputpot, templatepo, tm=None, min_similarity=75, fuzzymat
             mime_version = value
         else:
             kwargs[key] = value
-    targetheader = thetargetfile.makeheader(charset=charset, encoding=encoding, project_id_version=project_id_version,
+    output_header = output_store.makeheader(charset=charset, encoding=encoding, project_id_version=project_id_version,
         pot_creation_date=pot_creation_date, po_revision_date=po_revision_date, last_translator=last_translator,
         language_team=language_team, mime_version=mime_version, plural_forms=plural_forms, **kwargs)
     # Get the header comments and fuzziness state
-    if templatepo is not None and len(templatepo.units) > 0:
-        if templatepo.units[0].isheader():
-            if templatepo.units[0].getnotes("translator"):
-                targetheader.addnote(templatepo.units[0].getnotes("translator"), "translator")
-            if inputpot.units[0].getnotes("developer"):
-                targetheader.addnote(inputpot.units[0].getnotes("developer"), "developer")
-            targetheader.markfuzzy(templatepo.units[0].isfuzzy())
-    elif len(inputpot.units) > 0 and inputpot.units[0].isheader():
-        targetheader.addnote(inputpot.units[0].getnotes())
-    thetargetfile.addunit(targetheader)
+    if template_store is not None and len(template_store.units) > 0:
+        if template_store.units[0].isheader():
+            if template_store.units[0].getnotes("translator"):
+                output_header.addnote(template_store.units[0].getnotes("translator"), "translator")
+            if input_store.units[0].getnotes("developer"):
+                output_header.addnote(input_store.units[0].getnotes("developer"), "developer")
+            output_header.markfuzzy(template_store.units[0].isfuzzy())
+    elif len(input_store.units) > 0 and input_store.units[0].isheader():
+        output_header.addnote(input_store.units[0].getnotes())
+    output_store.addunit(output_header)
     # Do matching
-    for inputpotunit in inputpot.units:
-        if not (inputpotunit.isheader() or inputpotunit.isobsolete()):
-            if templatepo:
+    for input_unit in input_store.units:
+        if not (input_unit.isheader() or input_unit.isobsolete()):
+            if template_store:
                 possiblematches = []
-                for location in inputpotunit.getlocations():
-                    templatepounit = templatepo.locationindex.get(location, None)
-                    if templatepounit is not None:
-                        possiblematches.append(templatepounit)
-                if len(inputpotunit.getlocations()) == 0:
-                    templatepounit = templatepo.findunit(inputpotunit.source)
-                if templatepounit:
-                    possiblematches.append(templatepounit)
-                for templatepounit in possiblematches:
-                    if inputpotunit.source == templatepounit.source and templatepounit.target:
-                        inputpotunit.merge(templatepounit, authoritative=True)
+                for location in input_unit.getlocations():
+                    template_unit = template_store.locationindex.get(location, None)
+                    if template_unit is not None:
+                        possiblematches.append(template_unit)
+                if len(input_unit.getlocations()) == 0:
+                    template_unit = template_store.findunit(input_unit.source)
+                if template_unit:
+                    possiblematches.append(template_unit)
+                for template_unit in possiblematches:
+                    if input_unit.source == template_unit.source and template_unit.target:
+                        input_unit.merge(template_unit, authoritative=True)
                         break
                 else:
                     fuzzycandidates = []
                     if fuzzyfilematcher:
-                        fuzzycandidates = fuzzyfilematcher.matches(inputpotunit.source)
+                        fuzzycandidates = fuzzyfilematcher.matches(input_unit.source)
                         if fuzzycandidates:
-                            inputpotunit.merge(fuzzycandidates[0])
-                            original = templatepo.findunit(fuzzycandidates[0].source)
+                            input_unit.merge(fuzzycandidates[0])
+                            original = template_store.findunit(fuzzycandidates[0].source)
                             if original:
                                 original.reused = True
                     if fuzzyglobalmatcher and not fuzzycandidates:
-                        fuzzycandidates = fuzzyglobalmatcher.matches(inputpotunit.source)
+                        fuzzycandidates = fuzzyglobalmatcher.matches(input_unit.source)
                         if fuzzycandidates:
-                            inputpotunit.merge(fuzzycandidates[0])
+                            input_unit.merge(fuzzycandidates[0])
             else:
                 if fuzzyglobalmatcher:
-                    fuzzycandidates = fuzzyglobalmatcher.matches(inputpotunit.source)
+                    fuzzycandidates = fuzzyglobalmatcher.matches(input_unit.source)
                     if fuzzycandidates:
-                        inputpotunit.merge(fuzzycandidates[0])
-            if inputpotunit.hasplural() and len(inputpotunit.target) == 0:
+                        input_unit.merge(fuzzycandidates[0])
+            if input_unit.hasplural() and len(input_unit.target) == 0:
                 # Let's ensure that we have the correct number of plural forms:
-                nplurals, plural = thetargetfile.getheaderplural()
+                nplurals, plural = output_store.getheaderplural()
                 if nplurals and nplurals.isdigit() and nplurals != '2':
-                    inputpotunit.target = multistring([""]*int(nplurals))
-            thetargetfile.addunit(inputpotunit)
+                    input_unit.target = multistring([""]*int(nplurals))
+            output_store.addunit(input_unit)
 
     #Let's take care of obsoleted messages
-    if templatepo:
+    if template_store:
         newlyobsoleted = []
-        for unit in templatepo.units:
+        for unit in template_store.units:
             if unit.isheader():
                 continue
-            if unit.target and not (inputpot.findunit(unit.source) or hasattr(unit, "reused")):
+            if unit.target and not (input_store.findunit(unit.source) or hasattr(unit, "reused")):
                 #not in .pot, make it obsolete
                 unit.makeobsolete()
                 newlyobsoleted.append(unit)
             elif unit.isobsolete():
-                thetargetfile.addunit(unit)
+                output_store.addunit(unit)
         for unit in newlyobsoleted:
-            thetargetfile.addunit(unit)
-    return thetargetfile
+            output_store.addunit(unit)
+    return output_store
 
 def main(argv=None):
     from translate.convert import convert
