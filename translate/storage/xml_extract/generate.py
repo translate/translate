eb7b9db95663eaa42444b6cfba0524787ce0d88b
Wynand Winterbach <wynand@translate.org.za>
2008-10-13 08:48:16 +0000

    Added more comments to the code. More to follow.
    svn path=/src/trunk/; revision=8743

--- translate/storage/xml_extract/generate.py
+++ translate/storage/xml_extract/generate.py
@@ -144,18 +144,39 @@ def _map_source_dom_to_doc_dom(unit_node, source_dom_node):
 
 @accepts(etree._Element, etree._Element)
 def _map_target_dom_to_source_dom(source_dom_node, target_dom_node):
+    """Associate placeables in source_dom_node and target_dom_node which
+    have the same 'id' attributes.
+    
+    We're using XLIFF placeables. The XLIFF standard requires that
+    placeables have unique ids. The id of a placeable is never modified,
+    which means that even if placeables are moved around in a translation,
+    we can easily associate placeables from the source text with placeables
+    in the target text.
+    
+    This function does exactly that.
+    """
+    
     def map_id_to_dom_node(parent_node, node, id_to_dom_node):
+        # If this DOM node has an 'id' attribute, then add an id -> node
+        # mapping to 'id_to_dom_node'.
         if u'id' in node.attrib:
             id_to_dom_node[node.attrib[u'id']] = node
         return id_to_dom_node
     
+    # Build a mapping of id attributes to the DOM nodes which have these ids.
     id_to_dom_node = reduce_dom_tree(map_id_to_dom_node, target_dom_node, {})
     
     def map_target_dom_to_source_dom_aux(parent_node, node, target_dom_to_source_dom):
+        # 
         if u'id' in node.attrib and node.attrib[u'id'] in id_to_dom_node:
             target_dom_to_source_dom[id_to_dom_node[node.attrib[u'id']]] = node
         return target_dom_to_source_dom
     
+    # For each node in the DOM tree rooted at source_dom_node:
+    # 1. Check whether the node has an 'id' attribute.
+    # 2. If so, check whether there is a mapping of this id to a target DOM node
+    #    in id_to_dom_node.
+    # 3. If so, associate this source DOM node with the target DOM node.
     return reduce_dom_tree(map_target_dom_to_source_dom_aux, source_dom_node, {})
 
 def _build_target_dom_to_doc_dom(unit_node, source_dom, target_dom):
@@ -165,24 +186,42 @@ def _build_target_dom_to_doc_dom(unit_node, source_dom, target_dom):
 
 @accepts(etree._Element, {etree._Element: etree._Element})
 def _get_translated_node(target_node, target_dom_to_doc_dom):
+    """Convenience function to get node corresponding to 'target_node'
+    and to assign the tail text of 'target_node' to this node."""
     dom_node = target_dom_to_doc_dom[target_node]
     dom_node.tail = target_node.tail
     return dom_node
 
 @accepts(etree._Element, etree._Element, {etree._Element: etree._Element})
 def _build_translated_dom(dom_node, target_node, target_dom_to_doc_dom):
-    """
+    """Use the "shape" of 'target_node' (which is a DOM tree) to insert nodes
+    into the DOM tree rooted at 'dom_node'.
     
+    The mapping 'target_dom_to_doc_dom' is used to map nodes from 'target_node'
+    to nodes which much be inserted into dom_node.
     """
     dom_node.text = target_node.text
+    # 1. Find all child nodes of target_node.
+    # 2. Filter out the children which map to None.
+    # 3. Call _get_translated_node on the remaining children; this maps a node in
+    #    'target_node' to a node in 'dom_node' and assigns the tail text of 'target_node'
+    #    to the mapped node.
+    # 4. Add all of these mapped nodes to 'dom_node' 
     dom_node.extend(_get_translated_node(child, target_dom_to_doc_dom) for child in target_node 
                     if target_dom_to_doc_dom[child] is not None)
+    # Recursively call this function on pairs of matched children in
+    # dom_node and target_node.
     for dom_child, target_child in zip(dom_node, target_node):
         _build_translated_dom(dom_child, target_child, target_dom_to_doc_dom)
 
 @accepts(IsCallable())
 def replace_dom_text(make_parse_state):
-    """Return a function """
+    """Return a function
+      action: etree_Element x base.TranslationUnit -> None
+    which takes a dom_node and a translation unit. The dom_node is rearranged
+    according to rearrangement of placeables in unit.target (relative to their
+    positions in unit.source).
+    """
     
     @accepts(etree._Element, base.TranslationUnit)
     def action(dom_node, unit):
@@ -190,9 +229,11 @@ def replace_dom_text(make_parse_state):
         to update the text in the dom_node and at the tails of its children."""
         source_dom            = etree.fromstring(u'<source>%s</source>' % unicode(unit.source))
         target_dom            = etree.fromstring(u'<target>%s</target>' % unicode(unit.target or unit.source))
+        # Build a tree of (non-DOM) nodes which correspond to the translatable DOM nodes in 'dom_node'.
+        # Pass in a fresh parse_state every time, so as avoid working with stale parse state info.
         unit_node             = extract.find_translatable_dom_nodes(dom_node, make_parse_state())[0]        
         target_dom_to_doc_dom = _build_target_dom_to_doc_dom(unit_node, source_dom, target_dom)
-        
+        # Before we start reconstructing the sub-tree rooted at dom_node, we must clear out its children
         dom_node[:] = []
         _build_translated_dom(dom_node, target_dom, target_dom_to_doc_dom)
 
