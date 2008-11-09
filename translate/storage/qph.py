564f09175139ea0cdd40680aea2ef7a2d3456ba0
Friedel Wolff <friedel@translate.org.za>
2008-11-09 13:12:11 +0000

    Somewhat simplify qph  - a simple implementation for a simple format
    svn path=/src/trunk/; revision=8892

--- translate/storage/qph.py
+++ translate/storage/qph.py
@@ -77,15 +77,9 @@ class QphUnit(lisa.LISAunit):
 
     def removenotes(self):
         """Remove all the translator notes."""
-        note = self.xmlelement.find(self.namespaced("comment"))
+        note = self.xmlelement.find(self.namespaced("definition"))
         if not note is None:
             self.xmlelement.remove(note)
-
-    def getid(self):
-        return self.source
-
-    def merge(self, otherunit, overwrite=False, comments=True):
-        super(QphUnit, self).merge(otherunit, overwrite, comments)
 
 
 class QphFile(lisa.LISAfile):
@@ -95,77 +89,15 @@ class QphFile(lisa.LISAfile):
     Mimetypes  = ["application/x-qph"]
     Extensions = ["qph"]
     rootNode = "QPH"
-    # We will switch out .body to fit with the context we are working on
-    bodyNode = "context"
+    bodyNode = "QPH"
     XMLskeleton = '''<!DOCTYPE QPH>
 <QPH>
 </QPH>
 '''
     namespace = ''
 
-    def __init__(self, *args, **kwargs):
-        self._contextname = None
-        lisa.LISAfile.__init__(self, *args, **kwargs)
-
     def initbody(self):
-        """Initialises self.body."""
+        """Initialises self.body so it never needs to be retrieved from the XML again."""
         self.namespace = self.document.getroot().nsmap.get(None, None)
-        if self._contextname:
-            self.body = self.getcontextnode(self._contextname)
-        else:
-            self.body = self.document.getroot()
+        self.body = self.document.getroot() # The root node contains the units
 
-    def createcontext(self, contextname, comment=None):
-        """Creates a context node with an optional comment"""
-        context = etree.SubElement(self.document.getroot(), self.namespaced(self.bodyNode))
-        if comment:
-            comment_node = context.SubElement(context, "comment")
-            comment_node.text = comment
-        return context
-
-    def getcontextname(self, contextnode):
-        """Returns the name of the given context."""
-        return filenode.find(self.namespaced("name")).text
-
-    def getcontextnames(self):
-        """Returns all contextnames in this TS file."""
-        contextnodes = self.document.findall(self.namespaced("context"))
-        contextnames = [self.getcontextname(contextnode) for contextnode in contextnodes]
-        contextnames = filter(None, contextnames)
-        if len(contextnames) == 1 and contextnames[0] == '':
-            contextnames = []
-        return contextnames
-
-    def getcontextnode(self, contextname):
-        """Finds the contextnode with the given name."""
-        contextnodes = self.document.findall(self.namespaced("context"))
-        for contextnode in contextnodes:
-            if self.getcontextname(contextnode) == contextname:
-                return contextnode
-        return None
-
-    def addunit(self, unit, new=True, contextname=None, createifmissing=False):
-        """adds the given trans-unit to the last used body node if the contextname has changed it uses the slow method instead (will create the nodes required if asked). Returns success"""
-        if self._contextname != contextname:
-            if not self.switchcontext(contextname, createifmissing):
-                return None
-        super(QphFile, self).addunit(unit, new)
-#        unit._context_node = self.getcontextnode(self._contextname)
-#        lisa.setXMLspace(unit.xmlelement, "preserve")
-        return unit
-
-    def switchcontext(self, contextname, createifmissing=False):
-        """Switch the current context to the one named contextname, optionally 
-        creating it if it doesn't exist."""
-        self._context_name = contextname
-        contextnode = self.getcontextnode(contextname)
-        if contextnode is None:
-            if not createifmissing:
-                return False
-            contextnode = self.createcontextnode(contextname)
-            self.document.getroot().append(contextnode)
-
-        self.body = contextnode
-        if self.body is None:
-            return False
-        return True
