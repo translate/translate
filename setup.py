ba4fdd3cbc1334ba7c8198fa8622712093b8331e
Alaa Abd el Fattah <alaa@translate.org.za>
2008-11-20 15:42:03 +0000

    install pretranslate
    svn path=/src/trunk/; revision=8987

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ translatescripts = [apply(join, ('translate', ) + script) for script in
                   ('tools', 'poswap'),
                   ('tools', 'poclean'),
                   ('tools', 'poterminology'),
+                  ('tools', 'pretranslate'),          
                   ('services', 'lookupclient.py'),
                   ('services', 'lookupservice')]
 
