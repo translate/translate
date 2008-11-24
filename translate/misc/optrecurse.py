e9f97ce96c515d545a0a3a1eb8ebcc1126db88b4
Friedel Wolff <friedel@translate.org.za>
2008-11-24 08:46:09 +0000

    Play safe to ensure we test correctly for psyco's presence even if the module doesn't exist
    svn path=/src/trunk/; revision=9003

--- translate/misc/optrecurse.py
+++ translate/misc/optrecurse.py
@@ -148,7 +148,7 @@ class RecursiveOptionParser(optparse.OptionParser, object):
     def usepsyco(self, options):
         # options.psyco == None means the default, which is "full", but don't give a warning...
         # options.psyco == "none" means don't use psyco at all...
-        if options.psyco == "none":
+        if getattr(options, "psyco", "none") == "none":
             return
         try:
             import psyco
