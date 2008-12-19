de73e8a72f7f13bd77045f7a0acaf4fefd88d55c
Walter Leibbrandt <walter@translate.org.za>
2008-12-19 12:53:48 +0000

    Removed prints and trailing whitespace.
    svn path=/src/trunk/; revision=9444

--- translate/services/opentranclient.py
+++ translate/services/opentranclient.py
@@ -28,25 +28,24 @@ from translate.search.lshtein import LevenshteinComparer
 
 class OpenTranClient(restclient.RESTClient):
     """CRUD operations for TM units and stores"""
-    
+
     def __init__(self, url, target_lang, source_lang="en", max_candidates=3, min_similarity=75):
         restclient.RESTClient.__init__(self)
-        
+
         self.max_candidates = max_candidates
         self.min_similarity = min_similarity
         self.comparer = LevenshteinComparer()
 
         self.url = url
-        
+
         self.source_lang = source_lang
         self.target_lang = target_lang
         #detect supported language
         self._issupported(target_lang)
 
 
-        
+
     def translate_unit(self, unit_source, callback=None):
-        print "target lang", self.target_lang
         if isinstance(unit_source, unicode):
             unit_source = unit_source.encode("utf-8")
 
@@ -58,24 +57,20 @@ class OpenTranClient(restclient.RESTClient):
         request.curl.setopt(pycurl.URL, self.url)
         self.add(request)
         if callback:
-            request.connect("REST-success", 
+            request.connect("REST-success",
                             lambda widget, id, response: callback(widget, id, self.format_suggestions(id, response)))
 
 
     def _handle_language(self, request, language, response):
         (result,), fish = xmlrpclib.loads(response)
-        print "result", result
         if result:
-            print "found the language", language
             self.target_lang = language
         else:
             lang = data.simplercode(language)
-            print language, "failed moving on to", lang
             if lang:
                 self._issupported(lang)
-        
+
     def _issupported(self, language):
-        print "language", language
         request_body = xmlrpclib.dumps((language,), "supported")
         request = restclient.RESTClient.Request(
             self.url, language, "POST", request_body)
@@ -95,7 +90,7 @@ class OpenTranClient(restclient.RESTClient):
                 result['target'] = result['target'].encode("utf-8")
             result['source'] = suggestion['projects'][0]['orig_phrase']
             if isinstance(result['source'], unicode):
-                result['source'] = result['source'].encode("utf-8")                
+                result['source'] = result['source'].encode("utf-8")
             #open-tran often gives too many results with many which can't really be
             #considered to be suitable for translation memory
             result['quality'] = self.comparer.similarity(id, result['source'], self.min_similarity)
