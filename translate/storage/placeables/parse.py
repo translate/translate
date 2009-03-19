7c40fb68bd7471b33e4cd4a99d45358417fb813c
Walter Leibbrandt <walter@translate.org.za>
2009-03-19 13:52:07 +0000

    Documented the important parse() function.
    svn path=/src/trunk/; revision=10607

--- translate/storage/placeables/parse.py
+++ translate/storage/placeables/parse.py
@@ -27,20 +27,43 @@ from translate.storage.placeables import base, StringElem
 
 
 def parse(tree, parse_funcs):
+    """Parse placeables from the given string or sub-tree by using the
+        parsing functions provided.
+
+        The output of this function is B{heavily} dependent on the order of the
+        parsing functions. This is because of the algorithm used.
+
+        An over-simplification of the algorithm: the leaves in the C{StringElem}
+        tree are expanded to the output of the first parsing function in
+        C{parse_funcs}. The next level of recursion is then started on the new
+        set of leaves with the used parsing function removed from
+        C{parse_funcs}.
+
+        @type  tree: str|unicode|StringElem
+        @param tree: The string or string element sub-tree to parse.
+        @type  parse_funcs: A list of parsing functions. It must take exactly
+            one argument (a C{unicode} string to parse) and return a list of
+            C{StringElem}s which, together, form the original string. If nothing
+            could be parsed, it should return C{None}."""
     if isinstance(tree, (str, unicode)):
         tree = StringElem([tree])
     if not parse_funcs:
         return tree
     leaves = [elem for elem in tree.depth_first() if elem.isleaf()]
-    print leaves
     parse_func = parse_funcs[0]
 
     for leaf in leaves:
         subleaves = parse_func(unicode(leaf))
         if subleaves is not None:
             leaf.sub = subleaves
-        parse(leaf, [f for f in parse_funcs if f is not parse_func])
+        parse(leaf, parse_funcs[1:])
 
+        # The rest of this block handles the case in which a StringElem was created by a previous
+        # parsing function and a later one added a StringElem sub-class as a child which consumes
+        # the whole string. That means that the leaf was changed from a StringElem with a single
+        # (unicode) sub-element to a StringElem with a single StringElemSubClass element which, in
+        # turn has a single unicode sub-element, equal to the original StringElem's.
+        # Simbolically it does StringElem([StringElemSubClass(['foo'])]) ->  StringElemSubClass(['foo'])
         if len(leaf.sub) == 1 and \
                 leaf.__class__ is StringElem and \
                 leaf.sub[0].__class__ is not StringElem and \
