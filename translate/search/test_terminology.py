7f44453812a19e123dbdce191f4c2b1d7154327f
Friedel Wolff <friedel@translate.org.za>
2009-03-26 14:45:06 +0000

    A test for an extra long term
    svn path=/src/trunk/; revision=10708

--- translate/search/test_terminology.py
+++ translate/search/test_terminology.py
@@ -6,19 +6,23 @@ class TestTerminology:
         """Tests basic functionality"""
         termmatcher = terminology.TerminologyComparer()
         assert termmatcher.similarity("Open the file", "file") > 75
-    
+
     def test_brackets(self):
         """Tests that brackets at the end of a term are ignored"""
         termmatcher = terminology.TerminologyComparer()
         assert termmatcher.similarity("Open file", "file (noun)") > 75
         assert termmatcher.similarity("Contact your ISP", "ISP (Internet Service Provider)") > 75
+        # Let's make sure that even long things work if the context marker
+        # might push us over some length limits
+        assert termmatcher.similarity("Contact your Internet Service Provider ", \
+                "Internet Service Provider (Internet Service Provider or ISP, as in the place that hooks you up)") > 75
 
     def test_past_tences(self):
         """Tests matching of some past tenses"""
         termmatcher = terminology.TerminologyComparer()
         assert termmatcher.similarity("The bug was submitted", "submit") > 75
         assert termmatcher.similarity("The site is certified", "certify") > 75
-        
+
     def test_space_mismatch(self):
         """Tests that we can match with some spacing mismatch"""
         termmatcher = terminology.TerminologyComparer()
