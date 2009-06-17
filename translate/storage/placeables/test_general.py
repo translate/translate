c4640d38a5a6097343fc7bda30ac2166401a14d9
Dwayne Bailey <dwayne@translate.org.za>
2009-06-17 19:56:59 +0000

    Add test for newline
    svn path=/src/trunk/; revision=11617

--- translate/storage/placeables/test_general.py
+++ translate/storage/placeables/test_general.py
@@ -8,3 +8,6 @@ def test_placeable_numbers():
     assert general.NumberPlaceable([u"-25"]) in general.NumberPlaceable.parse(u"Here is a -25 number")
     assert general.NumberPlaceable([u"+25"]) in general.NumberPlaceable.parse(u"Here is a +25 number")
 
+def test_placeable_newline():
+    assert general.NewlinePlaceable.parse(u"A newline\n")[1] == general.NewlinePlaceable([u"\n"])
+    assert general.NewlinePlaceable.parse(u"First\nSecond")[1] == general.NewlinePlaceable([u"\n"])
