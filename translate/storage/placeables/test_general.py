9f9a0e6692b40ee6146f8ea79f102da82bfeda55
Dwayne Bailey <dwayne@translate.org.za>
2009-06-17 20:09:56 +0000

    Add tests the show different decimal and group seperators: point, comma and non-breaking space
    svn path=/src/trunk/; revision=11619

--- translate/storage/placeables/test_general.py
+++ translate/storage/placeables/test_general.py
@@ -7,6 +7,9 @@ def test_placeable_numbers():
     assert general.NumberPlaceable([u"25"]) in general.NumberPlaceable.parse(u"Here is a 25 number")
     assert general.NumberPlaceable([u"-25"]) in general.NumberPlaceable.parse(u"Here is a -25 number")
     assert general.NumberPlaceable([u"+25"]) in general.NumberPlaceable.parse(u"Here is a +25 number")
+    assert general.NumberPlaceable([u"25.00"]) in general.NumberPlaceable.parse(u"Here is a 25.00 number")
+    assert general.NumberPlaceable([u"2,500.00"]) in general.NumberPlaceable.parse(u"Here is a 2,500.00 number")
+    assert general.NumberPlaceable([u"1\u00a0000,99"]) in general.NumberPlaceable.parse(u"Here is a 1\u00a0000,99 number")
 
 def test_placeable_newline():
     assert general.NewlinePlaceable.parse(u"A newline\n")[1] == general.NewlinePlaceable([u"\n"])
