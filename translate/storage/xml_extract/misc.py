91b247bffedd6cbb35434c9382e2381ba19190f8
Friedel Wolff <friedel@translate.org.za>
2009-07-21 08:32:51 +0000

    Clean up copyright header, whitespace
    svn path=/src/trunk/; revision=11955

--- translate/storage/xml_extract/misc.py
+++ translate/storage/xml_extract/misc.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2002-2006 Zuza Software Foundation
-# 
-# This file is part of translate.
+# Copyright 2008 Zuza Software Foundation
 #
-# translate is free software; you can redistribute it and/or modify
+# This file is part of the Translate Toolkit.
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# translate is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with translate; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import re
 
@@ -27,10 +25,10 @@ from translate.misc.typecheck import accepts, Self, IsCallable, IsOneOf, Any
 @accepts(IsCallable(), Any(), Any(), IsCallable(), state=[Any()])
 def reduce_tree(f, parent_unit_node, unit_node, get_children, *state):
     """Enumerate a tree, applying f to in a pre-order fashion to each node.
-    
+
     parent_unit_node contains the parent of unit_node. For the root of the tree,
     parent_unit_node == unit_node.
-    
+
     get_children is a single argument function applied to a unit_node to
     get a list/iterator to its children.
 
@@ -42,7 +40,7 @@ def reduce_tree(f, parent_unit_node, unit_node, get_children, *state):
             return x
         else:
             return (x,)
-    
+
     state = f(parent_unit_node, unit_node, *state)
     for child_unit_node in get_children(unit_node):
         state = reduce_tree(f, unit_node, child_unit_node, get_children, *as_tuple(state))
@@ -73,4 +71,3 @@ def parse_tag(full_tag):
         return unicode(match.groupdict()['namespace']), unicode(match.groupdict()['tag'])
     else:
         raise Exception('Passed an invalid tag')
-
