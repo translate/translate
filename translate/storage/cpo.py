translate/translate

commit b36875d39e892df486f49ff2ea196c78071733cd

Ensure that we work with unicodes when dealing with msgid_comments
svn path=/src/trunk/; revision=13068

translate/storage/cpo.py
@@ -451,22 +451,19 @@ class pounit(pocommon.pounit):
 
     def _extract_msgidcomments(self, text=None):
         """Extract KDE style msgid comments from the unit.
-        
+
         @rtype: String
         @return: Returns the extracted msgidcomments found in this unit's msgid.
-        
         """
-    
         if not text:
-            text = gpo.po_message_msgid(self._gpo_message)
+            text = gpo.po_message_msgid(self._gpo_message).decode(self._encoding)
         if text:
             return pocommon.extract_msgid_comment(text)
         return u""
 
     def setmsgidcomment(self, msgidcomment):
         if msgidcomment:
-            newsource = "_: " + msgidcomment + "\n" + self.source
-            self.source = newsource
+            self.source = u"_: %s\n%s" (msgidcomment, self.source)
     msgidcomment = property(_extract_msgidcomments, setmsgidcomment)
 
     def __str__(self):

commit 4ebcb6d27a62b160880a3bc69c3b2cc5d6dbc84b

Typo introduced in r13068
svn path=/src/trunk/; revision=13134

translate/storage/cpo.py
@@ -463,7 +463,7 @@ class pounit(pocommon.pounit):
 
     def setmsgidcomment(self, msgidcomment):
         if msgidcomment:
-            self.source = u"_: %s\n%s" (msgidcomment, self.source)
+            self.source = u"_: %s\n%s" % (msgidcomment, self.source)
     msgidcomment = property(_extract_msgidcomments, setmsgidcomment)
 
     def __str__(self):