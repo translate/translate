c438dd95e64b3afbd51cbd242ef69588b9fc37dd
Alaa Abd el Fattah <alaa@translate.org.za>
2010-02-05 15:53:10 +0000

    return the note when origin=None also
    svn path=/src/trunk/; revision=13770

--- translate/storage/subtitles.py
+++ translate/storage/subtitles.py
@@ -44,7 +44,7 @@ class SubtitleUnit(base.TranslationUnit):
         super(SubtitleUnit, self).__init__(source)
 
     def getnotes(self, origin=None):
-        if origin in ['programmer', 'developer', 'source code']:
+        if origin in ['programmer', 'developer', 'source code', None]:
             return "visible for %d seconds" % self._duration
         else:
             return ''
