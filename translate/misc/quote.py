ceb97920e5c760694e5e9c054b87c95c872a990e
Dwayne Bailey <dwayne@translate.org.za>
2010-04-17 14:17:15 +0000

    PEP8: line length in comments, spaces between functions, space around =, whitespace at end
    svn path=/src/trunk/; revision=14287

--- translate/misc/quote.py
+++ translate/misc/quote.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2002-2006 Zuza Software Foundation
-# 
+#
 # This file is part of translate.
 #
 # translate is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # translate is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,13 +19,15 @@
 # along with translate; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""string processing utilities for extracting strings with various kinds of delimiters"""
+"""String processing utilities for extracting strings with various kinds
+of delimiters"""
 
 import logging
 import htmlentitydefs
 
+
 def find_all(searchin, substr):
-    """returns a list of locations where substr occurs in searchin
+    """Returns a list of locations where substr occurs in searchin
     locations are not allowed to overlap"""
     location = 0
     locations = []
@@ -36,10 +38,14 @@ def find_all(searchin, substr):
             location += len(substr)
     return locations
 
-def extract(source, startdelim, enddelim, escape=None, startinstring=False, allowreentry=True):
-    """Extracts a doublequote-delimited string from a string, allowing for backslash-escaping
-    returns tuple of (quoted string with quotes, still in string at end)"""
-    # note that this returns the quote characters as well... even internally
+
+def extract(source, startdelim, enddelim,
+            escape=None, startinstring=False, allowreentry=True):
+    """Extracts a doublequote-delimited string from a string, allowing for
+    backslash-escaping returns tuple of (quoted string with quotes, still in
+    string at end).
+    """
+    # Note that this returns the quote characters as well... even internally
     instring = startinstring
     enteredonce = False
     lenstart = len(startdelim)
@@ -52,7 +58,7 @@ def extract(source, startdelim, enddelim, escape=None, startinstring=False, allo
     if escape is not None:
         lenescape = len(escape)
         escape_places = find_all(source, escape)
-        # filter escaped escapes
+        # Filter escaped escapes
         true_escape = False
         true_escape_places = []
         for escape_pos in escape_places:
@@ -66,14 +72,15 @@ def extract(source, startdelim, enddelim, escape=None, startinstring=False, allo
         enddelim_places = [pos + lenend for pos in enddelim_places if pos - lenescape not in true_escape_places]
     else:
         enddelim_places = [pos + lenend for pos in enddelim_places]
-    # get a unique sorted list of the significant places in the string
+    # Get a unique sorted list of the significant places in the string
     significant_places = [0] + startdelim_places + enddelim_places + [len(source)-1]
     significant_places.sort()
     extracted = ""
     lastpos = None
     for pos in significant_places:
         if instring and pos in enddelim_places:
-            # make sure that if startdelim == enddelim we don't get confused and count the same string as start and end
+            # Make sure that if startdelim == enddelim we don't get confused
+            # and count the same string as start and end.
             if lastpos == pos - lenstart and lastpos in startdelim_places:
                 continue
             extracted += source[lastpos:pos]
@@ -87,14 +94,22 @@ def extract(source, startdelim, enddelim, escape=None, startinstring=False, allo
         extracted += source[lastpos:]
     return (extracted, instring)
 
+
 def extractstr(source):
-    "Extracts a doublequote-delimited string from a string, allowing for backslash-escaping"
+    """Extracts a doublequote-delimited string from a string, allowing for
+    backslash-escaping.
+    """
     (string, instring) = extract(source, '"', '"', '\\')
     return string
 
-def extractwithoutquotes(source, startdelim, enddelim, escape=None, startinstring=False, includeescapes=True, allowreentry=True):
-    """Extracts a doublequote-delimited string from a string, allowing for backslash-escaping
-    includeescapes can also be a function that takes the whole escaped string and returns the replaced version"""
+
+def extractwithoutquotes(source, startdelim, enddelim, escape=None,
+                         startinstring=False, includeescapes=True,
+                         allowreentry=True):
+    """Extracts a doublequote-delimited string from a string, allowing for
+    backslash-escaping includeescapes can also be a function that takes the
+    whole escaped string and returns the replaced version.
+    """
     instring = startinstring
     enteredonce = False
     lenstart = len(startdelim)
@@ -140,13 +155,14 @@ def extractwithoutquotes(source, startdelim, enddelim, escape=None, startinstrin
                 for epos in escape_list:
                     new_section += section[last_epos:epos]
                     if callable_includeescapes:
-                        replace_escape = includeescapes(section[epos:epos+lenescape+1])
-                        # TODO: deprecate old method of returning boolean from includeescape, by removing this if block
+                        replace_escape = includeescapes(section[epos:epos + lenescape + 1])
+                        # TODO: deprecate old method of returning boolean from
+                        # includeescape, by removing this if block
                         if not isinstance(replace_escape, basestring):
                             if replace_escape:
-                                replace_escape = section[epos:epos+lenescape+1]
+                                replace_escape = section[epos:epos + lenescape + 1]
                             else:
-                                replace_escape = section[epos+lenescape:epos+lenescape+1]
+                                replace_escape = section[epos + lenescape:epos + lenescape + 1]
                         new_section += replace_escape
                         last_epos = epos + lenescape + 1
                     else:
@@ -168,7 +184,7 @@ def extractwithoutquotes(source, startdelim, enddelim, escape=None, startinstrin
             last_epos = 0
             for epos in escape_list:
                 new_section += section[last_epos:epos]
-                if callable_includeescapes and includeescapes(section[epos:epos+lenescape+1]):
+                if callable_includeescapes and includeescapes(section[epos:epos + lenescape + 1]):
                     last_epos = epos
                 else:
                     last_epos = epos + lenescape
@@ -176,16 +192,19 @@ def extractwithoutquotes(source, startdelim, enddelim, escape=None, startinstrin
         extracted += section
     return (extracted, instring)
 
+
 def escapequotes(source, escapeescapes=0):
     "Returns the same string, with double quotes escaped with backslash"
     if escapeescapes:
         return source.replace('\\', '\\\\').replace('"', '\\"')
     else:
-        return source.replace('"','\\"')
+        return source.replace('"', '\\"')
+
 
 def escapesinglequotes(source):
     "Returns the same string, with single quotes doubled"
-    return source.replace("'","''")
+    return source.replace("'", "''")
+
 
 def htmlentityencode(source):
     """encodes source using HTML entities e.g. © -> &copy;"""
@@ -197,6 +216,7 @@ def htmlentityencode(source):
         else:
             output += str(char)
     return output
+
 
 def htmlentitydecode(source):
     """decodes source using HTML entities e.g. &copy; -> ©"""
@@ -224,8 +244,11 @@ def htmlentitydecode(source):
             output += char
     return output
 
+
 def javapropertiesencode(source):
-    """encodes source in the escaped-unicode encoding used by Java .properties files"""
+    """Encodes source in the escaped-unicode encoding used by Java
+    .properties files
+    """
     output = u""
     for char in source:
         charnum = ord(char)
@@ -237,8 +260,11 @@ def javapropertiesencode(source):
             output += u"\\u%04X" % charnum
     return output
 
+
 def mozillapropertiesencode(source):
-    """encodes source in the escaped-unicode encoding used by Mozilla .properties files"""
+    """Encodes source in the escaped-unicode encoding used by Mozilla
+    .properties files.
+    """
     output = u""
     for char in source:
         if char in controlchars:
@@ -260,25 +286,33 @@ controlchars = {
     "\f": "\\f", "\n": "\\n", "\r": "\\r", "\t": "\\t"
     }
 
+
 def escapecontrols(source):
     """escape control characters in the given string"""
     for key, value in controlchars.iteritems():
         source = source.replace(key, value)
     return source
 
-def propertiesdecode(source):
-    """Decodes source from the escaped-unicode encoding used by .properties files.
 
-    Java uses Latin1 by default, and Mozilla uses UTF-8 by default."""
-    # since the .decode("unicode-escape") routine decodes everything, and we don't want to
-    # we reimplemented the algorithm from Python Objects/unicode.c in Python here
-    # and modified it to retain escaped control characters
+def propertiesdecode(source):
+    """Decodes source from the escaped-unicode encoding used by .properties
+    files.
+
+    Java uses Latin1 by default, and Mozilla uses UTF-8 by default.
+
+    Since the .decode("unicode-escape") routine decodes everything, and we
+    don't want to we reimplemented the algorithm from Python Objects/unicode.c
+    in Python and modify it to retain escaped control characters.
+    """
     output = u""
     s = 0
     if isinstance(source, str):
         source = source.decode(encoding)
+
     def unichr2(i):
-        """Returns a Unicode string of one character with ordinal 32 <= i, otherwise an escaped control character"""
+        """Returns a Unicode string of one character with ordinal 32 <= i,
+        otherwise an escaped control character.
+        """
         if 32 <= i:
             return unichr(i)
         elif unichr(i) in controlchars:
@@ -287,6 +321,7 @@ def propertiesdecode(source):
             return unichr(i)
         else:
             return "\\u%04x" % i
+
     while s < len(source):
         c = source[s]
         if c != '\\':
@@ -295,8 +330,8 @@ def propertiesdecode(source):
             continue
         s += 1
         if s >= len(source):
-            # this is an escape at the end of the line, which implies a continuation...
-            # return the escape to inform the parser
+            # this is an escape at the end of the line, which implies
+            # a continuation..., return the escape to inform the parser
             output += c
             continue
         c = source[s]
@@ -316,7 +351,7 @@ def propertiesdecode(source):
                 if s + digit >= len(source):
                     digits = digit
                     break
-                c = source[s+digit].lower()
+                c = source[s + digit].lower()
                 if c.isdigit():
                     x += ord(c) - ord('0')
                 elif c in "abcdef":
@@ -344,8 +379,11 @@ def propertiesdecode(source):
             output += c # Drop any \ that we don't specifically handle
     return output
 
+
 def quotestr(source, escapeescapes=0):
-    "Returns a doublequote-delimited quoted string, escaping double quotes with backslash"
+    """Returns a doublequote-delimited quoted string, escaping double
+    quotes with backslash.
+    """
     if isinstance(source, list):
         firstline = True
         for line in source:
@@ -353,21 +391,29 @@ def quotestr(source, escapeescapes=0):
                 newsource = '"' + escapequotes(line, escapeescapes) + '"'
                 firstline = False
             else:
-                newsource = newsource + '\n' + '"' + escapequotes(line, escapeescapes) + '"'
+                newsource = newsource + '\n' + \
+                            '"' + escapequotes(line, escapeescapes) + '"'
         return newsource
     else:
         return '"' + escapequotes(source, escapeescapes) + '"'
 
+
 def singlequotestr(source):
-    "Returns a doublequote-delimited quoted string, escaping single quotes with themselves"
+    """Returns a doublequote-delimited quoted string, escaping single quotes
+    with themselves.
+    """
     return "'" + escapesinglequotes(source) + "'"
 
+
 def eitherquotestr(source):
-    "Returns a singlequote- or doublequote-delimited string, depending on what quotes it contains"
+    """Returns a singlequote- or doublequote-delimited string, depending on
+    what quotes it contains.
+    """
     if '"' in source:
         return singlequotestr(source)
     else:
         return quotestr(source)
+
 
 def findend(string, substring):
     s = string.find(substring)
@@ -375,8 +421,10 @@ def findend(string, substring):
         s += len(substring)
     return s
 
+
 def rstripeol(string):
     return string.rstrip("\r\n")
+
 
 def stripcomment(comment, startstring="<!--", endstring="-->"):
     cstart = comment.find(startstring)
@@ -387,8 +435,10 @@ def stripcomment(comment, startstring="<!--", endstring="-->"):
     cend = comment.find(endstring, cstart)
     return comment[cstart:cend].strip()
 
+
 def unstripcomment(comment, startstring="<!-- ", endstring=" -->\n"):
-    return startstring+comment.strip()+endstring
+    return startstring + comment.strip() + endstring
+
 
 def encodewithdict(unencoded, encodedict):
     """encodes certain characters in the string using an encode dictionary"""
@@ -397,6 +447,7 @@ def encodewithdict(unencoded, encodedict):
         if key in encoded:
             encoded = encoded.replace(key, value)
     return encoded
+
 
 def makeutf8(d):
     """convert numbers to utf8 codes in the values of a dictionary"""
