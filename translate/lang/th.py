22b9916bdc42d47ad3c8f38f210c8c7506ff4231
Friedel Wolff <friedel@translate.org.za>
2010-05-04 14:07:14 +0000

    Some initial customisations for Thai (th)
    svn path=/src/trunk/; revision=14413

---
+++ translate/lang/th.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2007 Zuza Software Foundation
+# 
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""This module represents Thai language.
+
+For more information, see U{http://en.wikipedia.org/wiki/Thai_language}
+"""
+
+import re
+
+from translate.lang import common
+
+class th(common.Common):
+    """This class represents Thai."""
+
+    puncdict = {
+        u". ": u" ",
+        #u"; ": u" ", # Test interaction with XML entities
+    }
+
+    # No capitalisation. While we can't do sentence segmentation, sentencecount
+    # is useless.
+    ignoretests = ["startcaps", "simplecaps", "sentencecount"]
