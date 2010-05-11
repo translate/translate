ec64e0cb21fe7a5db06a7d395cbfda2e3727e7ec
Dwayne Bailey <dwayne@translate.org.za>
2010-05-11 14:21:41 +0000

    We require unicode strings
    svn path=/src/trunk/; revision=14468

--- translate/lang/test_team.py
+++ translate/lang/test_team.py
@@ -7,9 +7,9 @@ from translate.lang.team import guess_language
 def test_simple():
     """test the regex, team snippet and language name snippets at a high
     level"""
-    assert guess_language("ab@li.org") == "ab"
-    assert guess_language("en@li.org") == None
-    assert guess_language("C@li.org") == None
-    assert guess_language("assam@mm.assam-glug.org") == "as"
-    assert guess_language("Hawaiian") == "haw"
-    assert guess_language("Bork bork") is None
+    assert guess_language(u"ab@li.org") == "ab"
+    assert guess_language(u"en@li.org") == None
+    assert guess_language(u"C@li.org") == None
+    assert guess_language(u"assam@mm.assam-glug.org") == "as"
+    assert guess_language(u"Hawaiian") == "haw"
+    assert guess_language(u"Bork bork") is None
