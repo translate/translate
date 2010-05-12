97bc4d6639193eb1e52c22f265769854154cb608
Dwayne Bailey <dwayne@translate.org.za>
2010-05-12 18:47:27 +0000

    Add some more tests
    svn path=/src/trunk/; revision=14491

--- translate/lang/test_data.py
+++ translate/lang/test_data.py
@@ -20,4 +20,13 @@ def test_languagematch():
     assert data.languagematch(None, "en_GB@Latn")
     assert not data.languagematch(None, "not-a-lang-code")
 
-    
+def test_normalise_code():
+    """test the normalisation of language codes"""
+    assert data.normalize_code("af_ZA") == "af-za"
+    assert data.normalize_code("xx@Latin") == "xx-latin"
+
+def test_simplify_to_common():
+    """test language code simplification"""
+    assert data.simplify_to_common("af_ZA") == "af"
+    assert data.simplify_to_common("pt_PT") == "pt"
+    assert data.simplify_to_common("pt_BR") == "pt_BR"
