e91cdcb318aef43fe13af0d31ed360def1615fa4
Friedel Wolff <friedel@translate.org.za>
2010-07-02 16:42:46 +0000

    Correct porting of markfuzzy() from pocommon
    svn path=/src/trunk/; revision=14904

--- translate/storage/pypo.py
+++ translate/storage/pypo.py
@@ -492,9 +492,9 @@ class pounit(pocommon.pounit):
         if present:
             self.set_state_n(self.STATE[self.S_FUZZY][0])
         elif is_null(self.msgstr):
-            self.set_state_n(self.STATE[self.S_TRANSLATED][0])
-        else:
             self.set_state_n(self.STATE[self.S_UNTRANSLATED][0])
+        else:
+            self.set_state_n(self.STATE[self.S_TRANSLATED][0])
 
     def _domarkfuzzy(self, present=True):
         self.settypecomment("fuzzy", present)
