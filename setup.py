dd9474fd28628312a6f6fc856795cad1902545e1
Friedel Wolff <friedel@translate.org.za>
2010-08-05 10:26:01 +0000

    Don't install lookupclient and lookupservice now that they're removed
    svn path=/src/trunk/; revision=15325

--- setup.py
+++ setup.py
@@ -83,8 +83,6 @@ translatescripts = [apply(join, ('translate', ) + script) for script in
                   ('tools', 'poclean'),
                   ('tools', 'poterminology'),
                   ('tools', 'pretranslate'),
-                  ('services', 'lookupclient.py'),
-                  ('services', 'lookupservice'),
                   ('services', 'tmserver'),
                   ('tools', 'build_tmdb')]
 
