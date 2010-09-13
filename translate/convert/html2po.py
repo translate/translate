4f3780eede3293ec5818dee5b5617051cd7c33ba
Dwayne Bailey <dwayne@translate.org.za>
2010-09-13 14:46:36 +0000

    PEP8: line length cleanups
    svn path=/src/trunk/; revision=15749

--- translate/convert/html2po.py
+++ translate/convert/html2po.py
@@ -32,10 +32,12 @@ from translate.storage import html
 
 class html2po:
 
-    def convertfile(self, inputfile, filename, includeuntagged=False, duplicatestyle="msgctxt", keepcomments=False):
+    def convertfile(self, inputfile, filename, includeuntagged=False,
+                    duplicatestyle="msgctxt", keepcomments=False):
         """converts a html file to .po format"""
         thetargetfile = po.pofile()
-        htmlparser = html.htmlfile(includeuntaggeddata=includeuntagged, inputfile=inputfile)
+        htmlparser = html.htmlfile(includeuntaggeddata=includeuntagged,
+                                   inputfile=inputfile)
         for htmlunit in htmlparser.units:
             thepo = thetargetfile.addsourceunit(htmlunit.source)
             thepo.addlocations(htmlunit.getlocations())
@@ -45,11 +47,17 @@ class html2po:
         return thetargetfile
 
 
-def converthtml(inputfile, outputfile, templates, includeuntagged=False, pot=False, duplicatestyle="msgctxt", keepcomments=False):
-    """reads in stdin using fromfileclass, converts using convertorclass, writes to stdout"""
+def converthtml(inputfile, outputfile, templates, includeuntagged=False,
+                pot=False, duplicatestyle="msgctxt", keepcomments=False):
+    """reads in stdin using fromfileclass, converts using convertorclass,
+    writes to stdout"""
     convertor = html2po()
     outputfilepos = outputfile.tell()
-    outputstore = convertor.convertfile(inputfile, getattr(inputfile, "name", "unknown"), includeuntagged, duplicatestyle=duplicatestyle, keepcomments=keepcomments)
+    outputstore = convertor.convertfile(inputfile, getattr(inputfile, "name",
+                                                           "unknown"),
+                                        includeuntagged,
+                                        duplicatestyle=duplicatestyle,
+                                        keepcomments=keepcomments)
     outputfile.write(str(outputstore))
     return 1
 
@@ -59,13 +67,20 @@ def main(argv=None):
     from translate.misc import stdiotell
     import sys
     sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
-    formats = {"html": ("po", converthtml), "htm": ("po", converthtml), "xhtml": ("po", converthtml), None: ("po", converthtml)}
-    parser = convert.ConvertOptionParser(formats, usepots=True, description=__doc__)
-    parser.add_option("-u", "--untagged", dest="includeuntagged", default=False, action="store_true",
-            help="include untagged sections")
+    formats = {"html": ("po", converthtml),
+               "htm": ("po", converthtml),
+               "xhtml": ("po", converthtml),
+               None: ("po", converthtml),
+              }
+    parser = convert.ConvertOptionParser(formats, usepots=True,
+                                         description=__doc__)
+    parser.add_option("-u", "--untagged", dest="includeuntagged",
+                      default=False, action="store_true",
+                      help="include untagged sections")
     parser.passthrough.append("includeuntagged")
-    parser.add_option("--keepcomments", dest="keepcomments", default=False, action="store_true",
-            help="preserve html comments as translation notes in the output")
+    parser.add_option("--keepcomments", dest="keepcomments", default=False,
+                      action="store_true",
+                      help="preserve html comments as translation notes in the output")
     parser.passthrough.append("keepcomments")
     parser.add_duplicates_option()
     parser.passthrough.append("pot")
