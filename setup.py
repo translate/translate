8a700a00bfeb5ef04e6ede759542a7df6a6099a4
Dwayne Bailey <dwayne@translate.org.za>
2010-10-07 11:15:28 +0000

    Install json2po and po2json by default
    svn path=/src/trunk/; revision=16004

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ translatescripts = [apply(join, ('translate', ) + script) for script in
                   ('convert', 'html2po'), ('convert', 'po2html'),
                   ('convert', 'ical2po'), ('convert', 'po2ical'),
                   ('convert', 'ini2po'), ('convert', 'po2ini'),
+                  ('convert', 'json2po'), ('convert', 'po2json'),
                   ('convert', 'tiki2po'), ('convert', 'po2tiki'),
                   ('convert', 'php2po'), ('convert', 'po2php'),
                   ('convert', 'rc2po'), ('convert', 'po2rc'),
