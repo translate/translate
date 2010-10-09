e6d6bdebf17464c90e0d75a37290b127a45cec55
Dwayne Bailey <dwayne@translate.org.za>
2010-10-09 12:50:45 +0000

    Fix trados import, skeleton (disabled) for file and unit testing
    svn path=/src/trunk/; revision=16067

--- translate/storage/test_trados.py
+++ translate/storage/test_trados.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from translate.storage import trados_txt as trados
+from py.test import mark
+
+from translate.storage import test_base
+from translate.storage import trados
 
 
 def test_unescape():
     # NBSP
     assert trados.unescape(ur"Ordre du jour\~:") == u"Ordre du jour\u00a0:"
     assert trados.unescape(ur"Association for Road Safety \endash  Conference") == u"Association for Road Safety –  Conference"
+
+#@mark.xfail(reason="Lots to implement")
+#class TestTradosTxtTmUnit(test_base.TestTranslationUnit):
+#    UnitClass = trados.TradosUnit
+#
+#@mark.xfail(reason="Lots to implement")
+#class TestTrodosTxtTmFile(test_base.TestTranslationStore):
+#    StoreClass = trados.TradosTxtTmFile
