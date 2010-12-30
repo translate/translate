6a2794cb72505d2b88715807d7f8d82cbf0733a4
Friedel Wolff <friedel@translate.org.za>
2010-12-30 08:00:32 +0000

    Disable the extensions test for omegat, since the test can't handle two storage classes for one file type in the factory
    svn path=/src/trunk/; revision=16998

--- translate/storage/test_omegat.py
+++ translate/storage/test_omegat.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from py.test import mark
+
 from translate.storage import omegat as ot
 from translate.storage import test_base
 
@@ -11,3 +13,8 @@ class TestOtUnit(test_base.TestTranslationUnit):
 
 class TestOtFile(test_base.TestTranslationStore):
     StoreClass = ot.OmegaTFile
+
+    @mark.xfail(reason="This doesn't work, due to two store classes handling different " \
+        "extensions, but factory listing it as one supported file type")
+    def test_extensions(self):
+        assert False
