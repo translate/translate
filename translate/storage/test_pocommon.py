3cb286e7d7b5777df358b88925e853f2379ad606
Dwayne Bailey <dwayne@translate.org.za>
2011-02-13 10:16:41 +0000

    Make the tests simpler to understand
    svn path=/src/trunk/; revision=17270

--- translate/storage/test_pocommon.py
+++ translate/storage/test_pocommon.py
@@ -6,15 +6,11 @@ from translate.storage import pocommon
 
 def test_roundtrip_quote_plus():
     "Test that what we put in is what we get out"""
-    def roundtrip_quote_plus(text):
+    def roundtrip_quote_plus(text, quoted):
         quote = pocommon.quote_plus(text)
+        assert quote == quoted
         unquote = pocommon.unquote_plus(quote)
         assert unquote == text
-    roundtrip_quote_plus("abc")
-    roundtrip_quote_plus("key space")
-    roundtrip_quote_plus(u"key ḓey")
-
-
-def test_quote_escapes():
-    """Test the specific characters that we want to escape"""
-    assert pocommon.quote_plus("space space") == "space+space"
+    roundtrip_quote_plus("abc", "abc")
+    roundtrip_quote_plus("key space", "key+space")
+    roundtrip_quote_plus(u"key ḓey", u"key+%E1%B8%93ey")
