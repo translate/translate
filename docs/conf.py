af4153b918a25bd7e134c4585887d6df86d293ed
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-03-30 00:12:06 +0200

    Enable coverage extension.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -242,3 +242,14 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+
+# -- Coverage checker options --------------------------------------------------
+
+coverage_ignore_modules = []
+
+coverage_ignore_functions = ['main']
+
+coverage_ignore_classes = []
+
+coverage_write_headline = False
