5ec7195fa8917e2df58e4a45aefeccd5a6522ac8
Michal Čihař <mcihar@suse.cz>
2012-08-28 14:01:43 +0200

    Add tests for escaping

--- translate/storage/test_aresource.py
+++ translate/storage/test_aresource.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from py import test
+from lxml import etree
 from py.test import deprecated_call
 
 from translate.misc import wStringIO
@@ -10,6 +11,39 @@ from translate.storage import test_monolingual
 
 class TestPropUnit(test_monolingual.TestMonolingualUnit):
     UnitClass = aresource.AndroidResourceUnit
+
+    escape_data = [
+        ('message\nwith newline', '<string name="Test String">message\\nwith newline</string>\n\n'),
+        ('@twitterescape', '<string name="Test String">\\@twitterescape</string>\n\n'),
+        ('quote \'escape\'', '<string name="Test String">quote \\\'escape\\\'</string>\n\n'),
+    ]
+
+    parse_test_data = escape_data + [
+        ('double quoted text', '<string name="Test String">"double quoted text"</string>\n\n'),
+    ]
+
+    def test_escape(self):
+        unit = self.unit
+        for string, xml in self.escape_data:
+            unit = self.UnitClass("Test String")
+            unit.target = string
+            print "unit.target:", repr(unit.target)
+            print "xml:", repr(xml)
+            assert str(unit) == xml
+
+    def test_parse(self):
+        if etree.LXML_VERSION >= (2, 1, 0):
+            #Since version 2.1.0 we can pass the strip_cdata parameter to
+            #indicate that we don't want cdata to be converted to raw XML
+            parser = etree.XMLParser(strip_cdata=False)
+        else:
+            parser = etree.XMLParser()
+        for string, xml in self.parse_test_data:
+            et = etree.fromstring(xml, parser)
+            unit = self.UnitClass.createfromxmlElement(et)
+            print "unit.target:", repr(unit.target)
+            print "string:", string
+            assert unit.target == string
 
 class TestProp(test_monolingual.TestMonolingualStore):
     StoreClass = aresource.AndroidResourceFile
