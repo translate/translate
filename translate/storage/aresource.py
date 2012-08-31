931bfaf07cf3b9d8e4d8c0f8eb3fe9b20bce7bfb
Michal Čihař <michal@cihar.com>
2012-08-31 10:18:30 +0200

    Fix raising of exception

--- translate/storage/aresource.py
+++ translate/storage/aresource.py
@@ -202,9 +202,9 @@ class AndroidResourceUnit(base.TranslationUnit):
                         # All others, remove, like Android does as well.
                         # However, Android does so silently, we show a
                         # warning so the dev can fix the problem.
-                        raise ValueError('Resource "%s": removing unsupported '
-                                  'escape sequence "%s"' % (
-                                    name, "".join(text[i-1 : i+1])))
+                        raise ValueError('removing unsupported '
+                                  'escape sequence "%s"' %
+                                     "".join(text[i-1 : i+1]))
                         text[i-1 : i+1] = ''
                         i -= 1
                     active_escape = False
