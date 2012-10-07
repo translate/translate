fdea314cbc727d6c0f7f5c04b759c52f8e6d5524
Dwayne Bailey <dwayne@translate.org.za>
2012-10-07 02:55:52 -0700

    Ignore 'chat' and '*~' when using po2moz

--- tools/mozilla/build_firefox.sh
+++ tools/mozilla/build_firefox.sh
@@ -328,7 +328,7 @@ do
 	done
 
 	verbose "po2moz - Create Mozilla l10n layout from migrated PO files."
-	po2moz --progress=$progress --errorlevel=$errorlevel --exclude=".git" --exclude=".hg" --exclude="obsolete" --exclude="editor" --exclude="mail" --exclude="thunderbird" \
+	po2moz --progress=$progress --errorlevel=$errorlevel --exclude=".git" --exclude=".hg" --exclude="obsolete" --exclude="editor" --exclude="mail" --exclude="thunderbird" --exclude="chat" --exclude="*~" \
 		-t ${L10N_DIR}/en-US -i ${POUPDATED_DIR}/${polang} -o ${L10N_DIR}/${lang}
 
 	if [ $opt_copyfiles ]; then
