657fcc41ea60b0008c70a6bb2759bcde0d65fa27
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-10-25 16:52:46 +0200

    Always use the CherryPy server.

--- translate/misc/wsgi.py
+++ translate/misc/wsgi.py
@@ -18,85 +18,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-"""wrapper tht tries to pick the best available wsgi server"""
+"""Wrapper to launch the bundled CherryPy server."""
 
 import logging
-import os
 
 
-def launch_server_wsgiref(host, port, app):
-    """use python's builtin simple_server, this is a last resort since
-    it doesn't support concurrency at all"""
-    from wsgiref import simple_server
+def launch_server(host, port, app, **kwargs):
+    """Use CherryPy's WSGI server, a multithreaded scallable server."""
+    from translate.misc.wsgiserver import CherryPyWSGIServer
 
-    class CustomRequestHandler(simple_server.WSGIRequestHandler):
-        """Custom request handler, disables some inefficient defaults"""
-
-        def address_string(self):
-            """Disable client reverse dns lookup."""
-            return  self.client_address[0]
-
-        def log_error(self, format, *args):
-            """Log errors using logging instead of printing to
-            stderror"""
-            logging.error("%s - - [%s] %s",
-                          self.address_string(), self.log_date_time_string(),
-                          format % args)
-
-        def log_message(self, format, *args):
-            """Log requests using logging instead of printing to
-            stderror."""
-            logging.info("%s - - [%s] %s",
-                         self.address_string(), self.log_date_time_string(),
-                         format % args)
-
-    server = simple_server.make_server(host, port, app,
-                                       handler_class=CustomRequestHandler)
-    logging.info("Starting wsgiref server, listening on port %s", port)
-    server.serve_forever()
-
-
-def launch_server_django(host, port, app):
-    """use django's development server, only works for django apps"""
-    if 'DJANGO_SETTINGS_MODULE' not in os.environ:
-        raise ImportError("no django settings module specified")
-
-    from django.core.servers.basehttp import  run
-
-    logging.info("Starting Django server, listening on port %s", port)
-    run(host, port, app)
-
-
-def launch_server_cherrypy(host, port, app):
-    """use cherrypy's wsgiserver, a multithreaded scallable server"""
-    from cherrypy.wsgiserver import  CherryPyWSGIServer
-
-    server = CherryPyWSGIServer((host, port), app)
+    server = CherryPyWSGIServer((host, port), app, **kwargs)
     logging.info("Starting CherryPy server, listening on port %s", port)
     try:
         server.start()
     except KeyboardInterrupt:
         server.stop()
-
-
-def launch_server_werkzeug(host, port, app):
-    """use werkzeug's simple multiprocess multithreaded server"""
-    from werkzeug import run_simple
-    logging.info("Starting Werkzeug server, listening on port %s", port)
-    run_simple(host, port, app, threaded=True)
-
-
-#FIXME: implement threading http server based on BaseHTTPServer and wsgiref
-
-servers = [launch_server_cherrypy, launch_server_werkzeug,
-           launch_server_django, launch_server_wsgiref]
-
-
-def launch_server(host, port, app):
-    """use the best possible wsgi server"""
-    for server in servers:
-        try:
-            server(host, port, app)
-            break
-        except ImportError:
-            pass
