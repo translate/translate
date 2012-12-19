9d20b358b53f54c36f8e7568d76635b3103f4edc
Dwayne Bailey <dwayne@translate.org.za>
2012-12-19 09:36:30 +0000

    Fix tests to align with option changes

--- translate/convert/test_csv2po.py
+++ translate/convert/test_csv2po.py
@@ -136,6 +136,7 @@ class TestCSV2POCommand(test_convert.TestConvertCommand, TestCSV2PO):
         """tests getting help"""
         options = test_convert.TestConvertCommand.test_help(self)
         options = self.help_check(options, "-t TEMPLATE, --template=TEMPLATE")
+        options = self.help_check(options, "-P, --pot")
         options = self.help_check(options, "--charset=CHARSET")
         options = self.help_check(options, "--columnorder=COLUMNORDER")
         options = self.help_check(options, "--duplicates=DUPLICATESTYLE", last=True)
