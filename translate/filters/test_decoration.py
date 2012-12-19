18c30ba53f35ebbcc974c792245f076f720751b7
Dwayne Bailey <dwayne@translate.org.za>
2012-12-19 09:47:26 +0000

    Zero Width Space is no longer considered as space
    See http://bugs.python.org/issue10567
    ZWS is not categorised as space by unicode and since Python 2.7
    str.isspace() has been using Unicode categorisation.

--- translate/filters/test_decoration.py
+++ translate/filters/test_decoration.py
@@ -11,8 +11,6 @@ def test_spacestart():
     assert decoration.spacestart(u"\u0020\u00a0Start") == u"\u0020\u00a0"
     # non-breaking space
     assert decoration.spacestart(u"\u00a0\u202fStart") == u"\u00a0\u202f"
-    # zero width space
-    assert decoration.spacestart(u"\u200bStart") == u"\u200b"
     # Some exotic spaces
     assert decoration.spacestart(u"\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200aStart") == u"\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a"
 
