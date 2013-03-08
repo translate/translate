fa5c27b340193110711fb96be8ce574263c5082b
Dwayne Bailey <dwayne@translate.org.za>
2013-03-08 10:46:43 +0000

    Ensure that POT files are also reflowed

--- tools/mozilla/build_firefox.sh
+++ tools/mozilla/build_firefox.sh
@@ -307,7 +307,7 @@ do
 	if [ $USECPO -eq 0 ]; then
 		verbose "Migration cleanup - fix migrated PO files using msgcat"
 		(cd ${polang}
-		for po in $(find ${PRODUCT_DIRS} -name "*.po")
+		for po in $(find ${PRODUCT_DIRS} -regex ".*\.po[t]?$")
 		do
 			msgcat -o $po.2 $po 2> >(egrep -v "warning: internationali[zs]ed messages should not contain the .* escape sequence" >&2) && mv $po.2 $po # parallel?
 		done
