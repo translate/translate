8d0f929d64ce559fdf7ccd78c241c81ab3b2123e
Christian Hitz <christian.hitz@aizo.com>
2013-03-28 11:09:17 +0100

    allow the list of units to be of type 'long' as well

--- translate/search/indexing/CommonIndexer.py
+++ translate/search/indexing/CommonIndexer.py
@@ -447,7 +447,7 @@ class CommonDatabase(object):
         if len(ident_list) == 0:
             # no matching items
             return 0
-        if isinstance(ident_list[0], int):
+        if isinstance(ident_list[0], int) or isinstance(ident_list[0], long):
             # create a list of IDs of all successfully removed documents
             success_delete = [match for match in ident_list
                     if self.delete_document_by_id(match)]
