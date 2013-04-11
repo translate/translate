97fd868c6e817e1a76e4e38a65bfe9829019daae
Stuart Prescott <stuart@debian.org>
2013-04-11 14:17:14 -0400

    Scrub extra rst markup from man pages
    Many tools have a --manpage option that generates a man page using an
    autoformatter of the --help output. The output may contain some raw rst
    constructs that are groff errors, so filtering them out makes man happier.

--- translate/misc/optrecurse.py
+++ translate/misc/optrecurse.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import re
 import sys
 import os.path
 import fnmatch
@@ -129,7 +130,8 @@ class RecursiveOptionParser(optparse.OptionParser, object):
         description_lines = self.description.split('\n\n')[1:]
         if description_lines:
             result.append('.SH DESCRIPTION\n')
-            result.append('\n'.join(description_lines))
+            result.append('\n'.join([re.sub('\.\. note::', 'Note:', l)
+                                            for l in description_lines]))
         result.append('.SH OPTIONS\n')
         ManHelpFormatter().store_option_strings(self)
         result.append('.PP\n')
