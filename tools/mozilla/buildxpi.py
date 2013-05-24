68bad0bc68e09b1c29bece96e9b58c63b1f8f6df
Alexander Dupuy <alex.dupuy@mac.com>
2013-05-24 03:16:44 -0400

    Enhancements for Langpack extension action
    Add various things to make this script usable as a module for the
    Langpack extension: mostly logging and exceptions

--- tools/mozilla/buildxpi.py
+++ tools/mozilla/buildxpi.py
@@ -38,34 +38,70 @@ Example usage::
 - "/path/to/xpi_output" is the path to the output directory.
 - "af" is the language (Afrikaans in this case) to build a language pack for.
 
-NOTE: The .mozconfig in the process owner's home directory gets backed up,
+NOTE: The .mozconfig in Firefox source directory gets backed up,
 overwritten and replaced.
 """
 
+import logging
 import os
 from glob       import glob
 from shutil     import move, rmtree
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, CalledProcessError
 from tempfile   import mkdtemp
 
+logger = logging.getLogger(__name__)
 
-HOMEDIR = os.getenv('HOME', '~')
 
+class RunProcessError(CalledProcessError):
+    """Subclass of CalledProcessError exception with custom message strings
+    """
+    _default_message = "Command '%s' returned exit status %d"
 
-def run(cmd, expected_status=0, stdout=None, stderr=None, shell=False):
-    if VERBOSE:
-        print '>>> %s $ %s' % (os.getcwd(), ' '.join(cmd))
-    p = Popen(cmd, stdout=stdout, stderr=stderr, shell=shell)
+    def __init__(self, message=None, **kwargs):
+        """Use and strip string message='' from kwargs"""
+        self._message = message or self._default_message
+        super(RunProcessError, self).__init__(**kwargs)
+
+    def __str__(self):
+        """Format exception message string (avoiding TypeErrors)"""
+        output = ''
+        message = self._message
+        if message.count('%') != 2:
+            output += message + '\n'
+            message = self._default_message
+            
+        output += message % (self.cmd, self.returncode)
+        return output
+
+def run(cmd, expected_status=0, fail_msg=None, stdout=-1, stderr=-1):
+    """Run a command
+    """
+    # Default is to capture (and log) std{out,error} unless run as script
+    if __name__ == '__main__' or logger.getEffectiveLevel() == logging.DEBUG:
+        std = None
+    else:
+        std = PIPE
+
+    if stdout == -1:
+        stdout = std
+    if stderr == -1:
+        stderr = std
+
+    cmdstring = isinstance(str, basestring) and cmd or ' '.join(cmd)
+    logger.debug('>>> %s $ %s', os.getcwd(), cmdstring)
+    p = Popen(cmd, stdout=stdout, stderr=stderr)
+    (output, error) = p.communicate()
     cmd_status = p.wait()
 
     if stdout == PIPE:
-        print p.stdout.read()
+        if cmd_status != expected_status:
+            logger.info('%s', output)
     elif stderr == PIPE:
-        print p.stderr.read()
+        logger.warning('%s', error)
 
     if cmd_status != expected_status:
-        print '!!! "%s" returned unexpected status %d' % \
-              (' '.join(cmd), cmd_status)
+        raise RunProcessError(returncode=cmd_status, cmd=cmdstring,
+                              message=fail_msg)
     return cmd_status
 
 
@@ -80,14 +116,24 @@ def build_xpi(l10nbase, srcdir, outputdir, lang, product, delete_dest=False):
     # Create a temporary directory for building
     builddir = mkdtemp('', 'buildxpi')
 
+    # Per the original instructions, it should be possible to configure the
+    # Mozilla build so that it doesn't require compiler toolchains or
+    # development include/library files - however it is currently broken for
+    # Aurora 22-23; # see https://bugzilla.mozilla.org/show_bug.cgi?id=862770
+    # in case it has been fixed and you can put back:
+    #ac_add_options --disable-compile-environment
+
     try:
         # Create new .mozconfig
         content = """
-#ac_add_options --disable-compile-environment # currently broken
 ac_add_options --disable-ogg
+ac_add_options --disable-wave
+ac_add_options --disable-webm
+ac_add_options --disable-libjpeg-turbo
 mk_add_options MOZ_OBJDIR=%(builddir)s
 ac_add_options --with-l10n-base=%(l10nbase)s
-ac_add_options --enable-application=%(product)s""" % \
+ac_add_options --enable-application=%(product)s
+""" % \
             {
                 'builddir': builddir,
                 'l10nbase': l10nbase,
@@ -96,17 +142,27 @@ ac_add_options --enable-application=%(product)s""" % \
 
         mozconf = open(MOZCONFIG, 'w').write(content)
 
+	# Try to make sure that "environment shell" is defined
+        # (python/mach/mach/mixin/process.py)
+        if not any (var in os.environ
+                    for var in ('SHELL', 'MOZILLABUILD', 'COMSPEC')):
+            os.environ['SHELL'] = '/bin/sh'
+
         # Start building process.
         # See https://developer.mozilla.org/en/Creating_a_Language_Pack for
         # more details.
         olddir = os.getcwd()
         os.chdir(srcdir)
-        if run(['make', '-f', 'client.mk', 'configure']):
-            raise Exception('^^ Fix the errors above and try again.')
+        run(['make', '-f', 'client.mk', 'configure'],
+            fail_msg="Build environment error - "
+                     "check logs, fix errors, and try again")
 
         os.chdir(builddir)
-        if run(['make', '-C', 'config']) or run(['make', '-C', os.path.join(product, 'locales'), 'langpack-%s' % (lang)]):
-            raise Exception('Unable to successfully build XPI!')
+        run(['make', '-C', 'config'],
+            fail_msg="Unable to successfully configure build for XPI!")
+        run(['make', '-C', os.path.join(product, 'locales'),
+             'langpack-%s' % (lang)],
+            fail_msg="Unable to successfully build XPI!")
 
         xpiglob = glob(
             os.path.join(
@@ -118,21 +174,22 @@ ac_add_options --enable-application=%(product)s""" % \
                 '*.%s.langpack.xpi' % lang
             )
         )[0]
+        filename = os.path.split(xpiglob)[1]
+        destfile = os.path.join(outputdir, filename)
         if delete_dest:
-            filename = os.path.split(xpiglob)[1]
-            destfile = os.path.join(outputdir, filename)
             if os.path.isfile(destfile):
                 os.unlink(destfile)
         move(xpiglob, outputdir)
 
-        os.chdir(olddir)
-
     finally:
+        os.chdir(olddir)
         # Clean-up
         rmtree(builddir)
         if backup_name:
             os.remove(MOZCONFIG)
             os.rename(backup_name, MOZCONFIG)
+
+    return destfile
 
 
 def create_option_parser():
@@ -186,9 +243,11 @@ if __name__ == '__main__':
     options, args = create_option_parser().parse_args()
 
     if len(args) < 1:
-        raise ArgumentError('You need to specify at least a language!')
+        from argparse import ArgumentError
+        raise ArgumentError(None, 'You need to specify at least a language!')
 
-    VERBOSE = options.verbose
+    if options.verbose:
+        logging.basicConfig(level=logging.DEBUG)
 
     build_xpi(
         l10nbase=os.path.abspath(options.l10nbase),
