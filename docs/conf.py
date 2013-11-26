5bb5cef9edf536735ae5248213aed3f2d1e63214
Dwayne Bailey <dwayne@translate.org.za>
2013-11-26 16:01:46 +0000

    Ignore releases/README.rst

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ release = '1.10.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '_themes/README.rst']
+exclude_patterns = ['_build', '_themes/README.rst', 'releases/README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
