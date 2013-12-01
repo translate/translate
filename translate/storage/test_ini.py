32ab28037c95c0b7cca9856d564e59f327818f82
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-12-01 19:29:33 +0100

    Inject a default section for section-less INI files
    Fixes bug #342
    Fixes bug #1428

--- translate/storage/test_ini.py
+++ translate/storage/test_ini.py
@@ -24,3 +24,30 @@ class TestINIFile(test_monolingual.TestMonolingualStore):
     def regen_ini(self, ini_source):
         """Helper that converts INI source to inifile object and back."""
         return str(self.parse_ini(ini_source))
+
+    def test_section_less(self):
+        """Check that a section-less INI is parsed correctly."""
+        ini_source = """3dArt=3D art
+About=About
+AdvancedGameOptions=Advanced Options
+"""
+        ini_file = self.parse_ini(ini_source)
+        assert len(ini_file.units) == 3
+        ini_unit = ini_file.units[0]
+        assert ini_unit.location == "[default]3dArt"
+        assert ini_unit.source == "3D art"
+        ini_unit = ini_file.units[1]
+        assert ini_unit.location == "[default]About"
+        assert ini_unit.source == "About"
+        ini_unit = ini_file.units[2]
+        assert ini_unit.location == "[default]AdvancedGameOptions"
+        assert ini_unit.source == "Advanced Options"
+
+    def test_section_less_source(self):
+        """checks that a simple php definition can be regenerated as source"""
+        ini_source = """3dArt=3D art
+About=About
+AdvancedGameOptions=Advanced Options
+"""
+        regen_ini = self.regen_ini(ini_source)
+        assert '[default]\n' + ini_source == regen_ini
