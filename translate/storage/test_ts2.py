e2ac77ae3646bbedf9d512ac6e4f5ae81a9b4a79
Jerome Leclanche <adys.wh@gmail.com>
2014-02-13 16:03:30 +0000

    storage/test_ts2: Add tests for getid()

--- translate/storage/test_ts2.py
+++ translate/storage/test_ts2.py
@@ -18,7 +18,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-"""Tests for Qt Linguist storage class"""
+"""Tests for Qt Linguist storage class
+
+Reference implementation & tests:
+gitorious:qt5-tools/src/qttools/tests/auto/linguist/lconvert/data
+"""
 
 from lxml import etree
 
@@ -185,3 +189,38 @@ class TestTSfile(test_base.TestTranslationStore):
         tsfile2.units[1].merge(tsfile.units[1])  # not fuzzy
         assert tsfile2.units[0].isfuzzy()
         assert not tsfile2.units[1].isfuzzy()
+
+    def test_getid(self):
+        """test that getid works well"""
+        tsstr = """<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Dialog2</name>
+    <message numerus="yes">
+        <source>%n files</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="this_is_some_id" numerus="yes">
+        <source>%n cars</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Age: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="this_is_another_id">
+        <source>func3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>"""
+
+        tsfile = ts.tsfile.parsestring(tsstr)
+        assert tsfile.units[0].getid() == "Dialog2%n files"
+        assert tsfile.units[1].getid() == "Dialog2\nthis_is_some_id%n cars"
+        assert tsfile.units[3].getid() == "Dialog2\nthis_is_another_idfunc3"
