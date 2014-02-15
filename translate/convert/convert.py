b1c71f32b9cfb2c33047422553e278f1ee48db8d
Dwayne Bailey <dwayne@translate.org.za>
2014-02-15 12:14:02 +0000

    argparse: more parse_args cleanups

--- translate/convert/convert.py
+++ translate/convert/convert.py
@@ -157,14 +157,14 @@ class ConvertOptionParser(optrecurse.RecursiveOptionParser, object):
 
     def run(self, argv=None):
         """Parses the command line options and runs the conversion."""
-        (options, args) = self.parse_args(argv)
-        options.inputformats = self.filterinputformats(options)
-        options.outputoptions = self.filteroutputoptions(options)
+        args = self.parse_args(argv)
+        args.inputformats = self.filterinputformats(options)
+        args.outputoptions = self.filteroutputoptions(options)
         try:
-            self.verifyoptions(options)
+            self.verifyoptions(args)
         except Exception as e:
             self.error(str(e))
-        self.recursiveprocess(options)
+        self.recursiveprocess(args)
 
     def processfile(self, fileprocessor, options, fullinputpath,
                     fulloutputpath, fulltemplatepath):
