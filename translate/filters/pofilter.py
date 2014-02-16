912bc14836fa88184d38b099d5ae2d5681407f06
Dwayne Bailey <dwayne@translate.org.za>
2014-02-16 20:56:28 +0000

    argparse: convert listfilter to store_true

--- translate/filters/pofilter.py
+++ translate/filters/pofilter.py
@@ -169,17 +169,9 @@ class FilterOptionParser(optrecurse.RecursiveOptionParser):
         """Construct the specialized Option Parser."""
         optrecurse.RecursiveOptionParser.__init__(self, formats)
 
-        self.add_argument("-l", "--listfilters", action="callback", dest='listfilters',
-            default=False, callback_kwargs={'dest_value': True},
-            callback=self.parse_noinput, help="list filters available")
-
-
-    def parse_noinput(self, option, opt, value, parser, *args, **kwargs):
-        """This sets an option to *True*, but also sets input to *-*
-        to prevent an error."""
-        setattr(parser.values, option.dest, kwargs['dest_value'])
-        parser.values.input = "-"
-
+        self.add_argument("-l", "--listfilters", action="store_true", dest='listfilters',
+            default=False,
+            help="list filters available")
 
     def run(self):
         """Parses the arguments, and runs recursiveprocess with the
