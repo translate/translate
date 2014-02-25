8f7ce7cca4af5b59cd4f3e003b5d0f824918139b
Jerome Leclanche <adys.wh@gmail.com>
2014-02-25 12:01:33 +0000

    py3: tests: Use double backslash instead of ur-string literals

--- translate/storage/test_trados.py
+++ translate/storage/test_trados.py
@@ -8,14 +8,14 @@ from translate.storage import trados
 
 def test_unescape():
     # NBSP
-    assert trados.unescape(ur"Ordre du jour\~:") == u"Ordre du jour\u00a0:"
-    assert trados.unescape(ur"Association for Road Safety \endash  Conference") == u"Association for Road Safety –  Conference"
+    assert trados.unescape(u"Ordre du jour\\~:") == u"Ordre du jour\u00a0:"
+    assert trados.unescape(u"Association for Road Safety \\endash  Conference") == u"Association for Road Safety –  Conference"
 
 
 def test_escape():
     # NBSP
-    assert trados.escape(u"Ordre du jour\u00a0:") == ur"Ordre du jour\~:"
-    assert trados.escape(u"Association for Road Safety –  Conference") == ur"Association for Road Safety \endash  Conference"
+    assert trados.escape(u"Ordre du jour\u00a0:") == u"Ordre du jour\\~:"
+    assert trados.escape(u"Association for Road Safety –  Conference") == u"Association for Road Safety \\endash  Conference"
 
 #@mark.xfail(reason="Lots to implement")
 #class TestTradosTxtTmUnit(test_base.TestTranslationUnit):
