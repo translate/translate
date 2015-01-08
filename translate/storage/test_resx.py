ce7c7e8aa360e8ec4fa736774e7bef1f346098f3
sjhale <sarah.hale@discoveringtime.com>
2015-01-08 10:24:33 +0000

    Updating copyright notices in resx2po / po2resx files Adding author name to copyright notices Adding missing copyright notices

--- translate/storage/test_resx.py
+++ translate/storage/test_resx.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Zuza Software Foundation
+# Copyright 2015 Sarah Hale (sjhale892@gmail.com)
+#
+# This file is part of translate.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from translate.misc import wStringIO
 from translate.storage import test_monolingual, resx
