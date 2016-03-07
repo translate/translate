5f1216cf0dae84da0de0c638261fa95e440302d4
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-03-07 20:31:12 +0100

    Bump up version number for 1.14.0-rc1

--- translate/__version__.py
+++ translate/__version__.py
@@ -19,14 +19,14 @@
 
 """This file contains the version of the Translate Toolkit."""
 
-build = 14000
+build = 14001
 """The build number is used by external users of the Translate Toolkit to
 trigger refreshes.  Thus increase the build number whenever changes are made to
 code touching stats or quality checks.  An increased build number will force a
 toolkit user, like Pootle, to regenerate it's stored stats and check
 results."""
 
-sver = "1.14.0-alpha1"
+sver = "1.14.0-rc1"
 """Human readable version number. Used for version number display."""
 
 ver = (1, 14, 0)
