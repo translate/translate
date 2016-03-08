8fb941e76a7beefe69907266db5b9fee4a5a415c
Dwayne Bailey <dwayne@translate.org.za>
2016-03-08 17:33:24 +0000

    tbx: add ability to read notes
    Note: this is very much not a perfect implementation.  We use notes to
    get information of type 'pos' and 'definition'.  Lets leave it as such
    until more representative TBX files need to be supported.

--- translate/storage/tbx.py
+++ translate/storage/tbx.py
@@ -21,7 +21,7 @@
 
 from lxml import etree
 
-from translate.misc.xml_helpers import setXMLlang
+from translate.misc.xml_helpers import setXMLlang, getXMLspace
 from translate.storage import lisa
 
 
@@ -48,6 +48,60 @@ Provisional work is done to make several languages possible."""
     def getid(self):
         # The id attribute is optional
         return self.xmlelement.get("id") or self.source
+
+    def removenotes(self, origin="translator"):
+        """Remove all the translator notes."""
+        notes = self.xmlelement.iterdescendants(self.namespaced("note"))
+        for note in notes:
+            self.xmlelement.remove(note)
+
+    def addnote(self, text, origin=None, position="append"):
+        """Add a note specifically in a "note" tag"""
+        if position != "append":
+            self.removenotes(origin=origin)
+
+        if text:
+            text = text.strip()
+        if not text:
+            return
+        if isinstance(text, bytes):
+            text = text.decode("utf-8")
+        note = etree.SubElement(self.xmlelement, self.namespaced("note"))
+        note.text = text
+        if origin:
+            note.set("from", origin)
+
+    def _getnotelist(self, origin=None):
+        """Returns the text from notes matching ``origin`` or all notes.
+
+        :param origin: The origin of the note (or note type)
+        :type origin: String
+        :return: The text from notes matching ``origin``
+        :rtype: List
+        """
+        note_nodes = []
+        if origin == 'pos':
+            note_nodes = self.xmlelement.iterdescendants(self.namespaced("termNote"))
+        elif origin == 'definition':
+            note_nodes = self.xmlelement.iterdescendants(self.namespaced("descrip"))
+        else:
+            note_nodes = self.xmlelement.iterdescendants(self.namespaced("note"))
+        # TODO: consider using xpath to construct initial_list directly
+        # or to simply get the correct text from the outset (just remember to
+        # check for duplication.
+        initial_list = [lisa.getText(note,
+                                     getXMLspace(self.xmlelement,
+                                                 self._default_xml_space))
+                        for note in note_nodes]
+
+        # Remove duplicate entries from list:
+        dictset = {}
+        note_list = [dictset.setdefault(note, note) for note in initial_list if note not in dictset]
+
+        return note_list
+
+    def getnotes(self, origin=None):
+        return '\n'.join(self._getnotelist(origin=origin))
 
 
 class tbxfile(lisa.LISAfile):
