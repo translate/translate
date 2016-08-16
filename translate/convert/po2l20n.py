1c8ed7d21c0baeec24f467d3762c7d82ccb5ea41
Taras Semenenko <taras.semenenko@gmail.com>
2016-08-17 02:02:42 +0300

    Adjust po2l20n converter

--- translate/convert/po2l20n.py
+++ translate/convert/po2l20n.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2002-2015 Zuza Software Foundation
+# Copyright 2016 Zuza Software Foundation
 #
 # This file is part of translate.
 #
@@ -18,92 +18,76 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-"""Convert Gettext PO localization files to .l20n files.
+"""Convert Gettext PO localization files to .ftl files.
 """
 
 import logging
-import re
-import warnings
 
-from translate.convert import accesskey, convert
-from translate.lang import data
-from translate.misc import quote
-from translate.storage import po, l20n
-
-
-L20N_PLURAL_RE = re.compile("<l20n:plural>@cldr.plural\(\$([^\s]+)\)</l20n>")
+from translate.convert import convert
+from translate.storage import l20n, po
 
 
 logger = logging.getLogger(__name__)
 
 
-class po2l20n:
+class po2l20n(object):
 
-    def __init__(self, templatefile, inputstore, includefuzzy=False):
-        self.templatestore = l20n.l20nfile(templatefile)
-        self.inputstore = inputstore
+    def __init__(self, inputfile, outputfile, templatefile=None,
+                 includefuzzy=False, outputthreshold=None):
+
+        if templatefile is None:
+            raise ValueError("must have template file for ftl files")
+
+        self.inputstore = po.pofile(inputfile)
+        self.outputfile = outputfile
+        self.template_store = l20n.l20nfile(templatefile)
         self.includefuzzy = includefuzzy
-        self.lang = self.inputstore.gettargetlanguage()
+        self.outputthreshold = outputthreshold
 
-    def mergeunit(self, unit):
-        pass
+    def run(self):
+        should_output_store = convert.should_output_store(self.inputstore,
+                                                          self.outputthreshold)
+        if not should_output_store:
+            return False
 
-    def convertunit(self, unit):
-        l20n_unit = l20n.l20nunit()
-        if not unit.istranslated() and not self.includefuzzy:
-            return None
-        l20n_unit.setid(unit.getlocations()[0])
-        if unit.hasplural():
-            l20n_unit.value_index = [{
-                'type': 'idOrVal',
-                'value': 'plural'
-            }, L20N_PLURAL_RE.findall(unit.getnotes("developer"))[0]]
-            l20n_unit.value = {}
-            if self.lang in data.cldr_plural_languages:
-                categories = data.cldr_plural_languages[self.lang]
-            else:
-                logger.error("'Language:' is unset in header or language ('%s') "
-                             "is not defined for CLDR plural forms defaulting "
-                             "to ['one', 'other']" % self.lang)
-                categories = ['one', 'other']
-            if len(categories) != len(unit.target.strings):
-                logger.warning("Warning: PO plurals does not match expected "
-                               "CLDR plurals")
-            for category, text in zip(categories, unit.target.strings):
-                l20n_unit.value.update({category: str(text)})
-        else:
-            l20n_unit.value = unit.target
+        self.convert_store().serialize(self.outputfile)
+        return True
+
+    def convert_unit(self, unit):
+        use_target = (unit.istranslated()
+                      or unit.isfuzzy() and self.includefuzzy)
+        l20n_unit_value = unit.target if use_target else unit.source
+        l20n_unit = l20n.l20nunit(
+            source=l20n_unit_value,
+            id=unit.getlocations()[0],
+            comment=unit.getnotes("developer")
+        )
         return l20n_unit
 
-    def mergestore(self):
-        pass
-
-    def convertstore(self):
+    def convert_store(self):
         outputstore = l20n.l20nfile()
-        for unit in self.inputstore.units:
-            newunit = self.convertunit(unit)
-            if newunit is not None:
+        self.inputstore.makeindex()
+
+        for l20nunit in self.template_store.units:
+            l20nunit_id = l20nunit.getid()
+
+            if l20nunit_id in self.inputstore.locationindex:
+                po_unit = self.inputstore.locationindex[l20nunit_id]
+                newunit = self.convert_unit(po_unit)
                 outputstore.addunit(newunit)
+
         return outputstore
 
 
 def convertl20n(inputfile, outputfile, templatefile, includefuzzy=False,
                 outputthreshold=None):
-    inputstore = po.pofile(inputfile)
+    return po2l20n(inputfile, outputfile, templatefile, includefuzzy,
+                   outputthreshold).run()
 
-    if templatefile is None:
-        convertor = po2l20n(templatefile, inputstore, includefuzzy)
-        outputl20n = convertor.convertstore()
-    else:
-        pass
-        convertor = po2l20n(templatefile, inputstore, includefuzzy)
-        outputl20n = convertor.mergestore()
-    outputfile.write(str(outputl20n))
-    return 1
 
 formats = {
-    ("po", "l20n"): ("l20n", convertl20n),
-    ("po"): ("l20n", convertl20n),
+    ("po", "ftl"): ("ftl", convertl20n),
+    "po": ("ftl", convertl20n),
 }
 
 
@@ -111,8 +95,10 @@ def main(argv=None):
     # handle command line options
     parser = convert.ConvertOptionParser(formats, usetemplates=True,
                                          description=__doc__)
+    parser.add_threshold_option()
     parser.add_fuzzy_option()
     parser.run(argv)
+
 
 if __name__ == '__main__':
     main()
