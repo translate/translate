4dcb483375c7b2912ff7f4d9dad75239b453d7b1
Taras Semenenko <taras.semenenko@gmail.com>
2016-08-18 13:24:42 +0300

    Add l20n to monolingual formats list in test_base module

--- translate/storage/test_base.py
+++ translate/storage/test_base.py
@@ -81,8 +81,9 @@ class TestTranslationUnit:
         assert unit1 == unit2
         assert unit1 != unit3
         assert unit4 != unit5
-        if unit1.__class__.__name__ in ('RESXUnit', 'dtdunit', 'TxtUnit', 'JsonUnit'):
-            # unit1 will generally equal unit6 for monolingual formats (resx, dtd, txt)
+        if unit1.__class__.__name__ in ('RESXUnit', 'dtdunit', 'TxtUnit',
+                                        'JsonUnit', 'l20nunit'):
+            # unit1 will generally equal unit6 for monolingual formats (resx, dtd, txt, l20n)
             # with the default comparison method which compare units by their
             # target and source properties only.
             # For other monolingual formats:
