abd41deaffb89df7f5e4295d267f7557095968d2
Ryan Northey <ryan@space23.net>
2016-09-14 14:20:56 +0100

    Update moz_lang tests to check headers and tag comments

--- translate/storage/test_mozilla_lang.py
+++ translate/storage/test_mozilla_lang.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import io
+
+import six
+
 import pytest
 
 from translate.storage import mozilla_lang, test_base
@@ -33,11 +37,15 @@ class TestMozLangUnit(test_base.TestTranslationUnit):
         assert not str(unit).endswith(" {ok}")
 
     def test_comments(self):
-        """Comments start with #."""
+        """Comments start with #, tags start with ## TAG:."""
         unit = self.UnitClass("One")
         unit.addnote("Hello")
         assert str(unit).find("Hello") == 2
         assert str(unit).find("# Hello") == 0
+        unit.addnote("# TAG: goodbye")
+        assert (
+            "# TAG: goodbye"
+            in unit.getnotes(origin="developer").split("\n"))
 
 
 class TestMozLangFile(test_base.TestTranslationStore):
@@ -121,3 +129,88 @@ class TestMozLangFile(test_base.TestTranslationStore):
         assert unit.source == "Source"
         assert unit.target == target
         assert unit.istranslated() == istranslated
+
+    def test_headers(self):
+        """Ensure we can handle and preserve file headers"""
+        lang = ("## active ##\n"
+                "## some_tag ##\n"
+                "## another_tag ##\n"
+                "## NOTE: foo\n"
+                ";Source\n"
+                "Target\n"
+                "\n\n")
+        store = self.StoreClass.parsestring(lang)
+        assert (
+            store.getlangheaders()
+            == [u'## some_tag ##',
+                u'## another_tag ##',
+                u'## NOTE: foo'])
+        out = io.BytesIO()
+        store.serialize(out)
+        out.seek(0)
+        assert (
+            out.read()
+            == six.text_type(
+                "## active ##\n"
+                "## some_tag ##\n"
+                "## another_tag ##\n"
+                "## NOTE: foo\n"
+                "\n\n"
+                ";Source\n"
+                "Target\n"
+                "\n\n").encode('utf-8'))
+
+    def test_not_headers(self):
+        """Ensure we dont treat a tag immediately after headers as header"""
+        lang = ("## active ##\n"
+                "## some_tag ##\n"
+                "## another_tag ##\n"
+                "## NOTE: foo\n"
+                "## TAG: fooled_you ##\n"
+                ";Source\n"
+                "Target\n"
+                "\n\n")
+        store = self.StoreClass.parsestring(lang)
+        assert "## TAG: fooled_you ##" not in store.getlangheaders()
+
+    def test_tag_comments(self):
+        """Ensure we can handle comments and distinguish from headers"""
+        lang = ("## active ##\n"
+                "# First comment\n"
+                "## TAG: important_tag\n"
+                "# Second comment\n"
+                "# Third comment\n"
+                "## TAG: another_important_tag\n"
+                ";Source\n"
+                "Target\n"
+                "\n\n")
+        store = self.StoreClass.parsestring(lang)
+        assert not store.getlangheaders()
+        assert bytes(store).decode('utf-8') == lang
+        assert (
+            "# TAG: important_tag"
+            in store.units[0].getnotes(origin="developer").split("\n"))
+        lang = ("## active ##\n"
+                "# First comment\n"
+                "## TAG: important_tag\n"
+                "# Second comment\n"
+                "# Third comment\n"
+                "## TAG: another_important_tag\n"
+                "# Another comment\n"
+                ";Source\n"
+                "Target\n"
+                "\n\n")
+        store = self.StoreClass.parsestring(lang)
+        assert not store.getlangheaders()
+        assert (
+            "First comment"
+            in store.units[0].getnotes(origin="developer").split("\n"))
+        assert (
+            "Second comment"
+            in store.units[0].getnotes(origin="developer").split("\n"))
+        assert (
+            "Another comment"
+            in store.units[0].getnotes(origin="developer").split("\n"))
+        assert (
+            "# TAG: another_important_tag"
+            in store.units[0].getnotes(origin="developer").split("\n"))
