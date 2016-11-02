3b63c5a8dcb054e852ee019d03f6fc5f223f862d
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-11-02 20:30:53 +0100

    Bump up version number for 2.0.0b7

--- translate/__version__.py
+++ translate/__version__.py
@@ -19,14 +19,14 @@
 
 """This file contains the version of the Translate Toolkit."""
 
-build = 20006
+build = 20007
 """The build number is used by external users of the Translate Toolkit to
 trigger refreshes.  Thus increase the build number whenever changes are made to
 code touching stats or quality checks.  An increased build number will force a
 toolkit user, like Pootle, to regenerate it's stored stats and check
 results."""
 
-sver = "2.0.0b6"
+sver = "2.0.0b7"
 """Human readable version number. Used for version number display."""
 
 ver = (2, 0, 0)
