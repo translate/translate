36d7d6caa2f1e0d31611aaf772e5744f0611b53d
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-09-07 21:04:58 +0200

    Tests: Add more test for po2ical

--- translate/convert/test_po2ical.py
+++ translate/convert/test_po2ical.py
@@ -46,9 +46,80 @@ END:VCALENDAR
         assert expected_output == self.format2po_text(input_source,
                                                       template_source)
 
-    # FIXME we should also test for DESCRIPTION, LOCATION and COMMENT
-    # The library handle any special encoding issues, we might want to test
-    # those
+    def test_description(self):
+        """Check that a simple PO converts valid iCalendar DESCRIPTION."""
+        input_source = """
+#: [uid1@example.com]DESCRIPTION
+msgid "My description"
+msgstr "A miña descrición"
+"""
+        icalendar_boilerplate = '''BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+DESCRIPTION:%s
+DTSTAMP:19970714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+END:VEVENT
+END:VCALENDAR
+'''.replace("\n", "\r\n")
+        template_source = icalendar_boilerplate % "My description"
+        expected_output = icalendar_boilerplate % "A miña descrición"
+        assert expected_output == self.format2po_text(input_source,
+                                                      template_source)
+
+    def test_location(self):
+        """Check that a simple PO converts valid iCalendar LOCATION."""
+        input_source = """
+#: [uid1@example.com]LOCATION
+msgid "The location"
+msgstr "O lugar"
+"""
+        icalendar_boilerplate = '''BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+DTSTAMP:19970714T170000Z
+LOCATION:%s
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+END:VEVENT
+END:VCALENDAR
+'''.replace("\n", "\r\n")
+        template_source = icalendar_boilerplate % "The location"
+        expected_output = icalendar_boilerplate % "O lugar"
+        assert expected_output == self.format2po_text(input_source,
+                                                      template_source)
+
+    def test_comment(self):
+        """Check that a simple PO converts valid iCalendar COMMENT."""
+        input_source = """
+#: [uid1@example.com]COMMENT
+msgid "Some comment"
+msgstr "Comentarios ao chou"
+"""
+        icalendar_boilerplate = '''BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+COMMENT:%s
+DTSTAMP:19970714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+END:VEVENT
+END:VCALENDAR
+'''.replace("\n", "\r\n")
+        template_source = icalendar_boilerplate % "Some comment"
+        expected_output = icalendar_boilerplate % "Comentarios ao chou"
+        assert expected_output == self.format2po_text(input_source,
+                                                      template_source)
 
 
 class TestPO2IcalCommand(test_convert.TestConvertCommand, TestPO2Ical):
