c964549cce85487b8b1ce64c9665e1df1df5b589
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-09-08 21:16:17 +0200

    Cleanup for easier read of roundtrip_quote_plus tests

--- translate/storage/test_pocommon.py
+++ translate/storage/test_pocommon.py
@@ -11,12 +11,13 @@ def test_roundtrip_quote_plus():
         assert quote == quoted
         unquote = pocommon.unquote_plus(quoted)
         assert unquote == text
-    roundtrip_quote_plus("abc", "abc")
-    roundtrip_quote_plus("key space", "key+space")
-    roundtrip_quote_plus("key ḓey", "key+%E1%B8%93ey")
-    roundtrip_quote_plus(
-        "path/file.c(2):3,path space/file.h:4",
-        "path/file.c(2):3,path+space/file.h:4"
-    )
+    roundtrip_quote_plus("abc",
+                         "abc")
+    roundtrip_quote_plus("key space",
+                         "key+space")
+    roundtrip_quote_plus("key ḓey",
+                         "key+%E1%B8%93ey")
+    roundtrip_quote_plus("path/file.c(2):3,path space/file.h:4",
+                         "path/file.c(2):3,path+space/file.h:4")
     roundtrip_quote_plus("[uid1@example.com]SUMMARY",
                          "[uid1@example.com]SUMMARY")
