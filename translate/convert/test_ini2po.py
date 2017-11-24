7510f1a831a802799948bfaefe8e9fe7a2d3441a
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-24 17:43:41 +0100

    Tests: Add test for ini2po command

--- translate/convert/test_ini2po.py
+++ translate/convert/test_ini2po.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from pytest import importorskip
 
-from translate.convert import ini2po
+from translate.convert import ini2po, test_convert
 from translate.misc import wStringIO
 
 
@@ -21,3 +21,16 @@ class TestIni2PO(object):
         result = ini2po.run_converter(input_file, output_file, template_file)
         assert result == 0
         assert output_file.getvalue() == ''
+
+
+class TestIni2POCommand(test_convert.TestConvertCommand, TestIni2PO):
+    """Tests running actual ini2po commands on files"""
+    convertmodule = ini2po
+    defaultoptions = {"progress": "none"}
+
+    def test_help(self):
+        """tests getting help"""
+        options = test_convert.TestConvertCommand.test_help(self)
+        options = self.help_check(options, "-t TEMPLATE, --template=TEMPLATE")
+        options = self.help_check(options, "-P, --pot")
+        options = self.help_check(options, "--duplicates=DUPLICATESTYLE")
