2f828ad58fc78d86a71f43d01a68d1cfbe887a0b
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-24 17:52:54 +0100

    Tests: Check ini2po conversion of single file

--- translate/convert/test_ini2po.py
+++ translate/convert/test_ini2po.py
@@ -13,6 +13,19 @@ importorskip("iniparse")
 
 class TestIni2PO(object):
 
+    def _convert(self, input_source, template_source=None, blank_msgstr=False,
+                 duplicate_style="msgctxt", dialect="default"):
+        """Helper that converts format input to PO output without files."""
+        input_file = wStringIO.StringIO(input_source)
+        output_file = wStringIO.StringIO()
+        template_file = None
+        if template_source:
+            template_file = wStringIO.StringIO(template_source)
+        result = ini2po.run_converter(input_file, output_file, template_file,
+                                      blank_msgstr, duplicate_style, dialect)
+        assert result == 1
+        return output_file.getvalue()
+
     def test_convert_empty_file(self):
         """Check converting empty INI returns no output."""
         input_file = wStringIO.StringIO('')
@@ -21,6 +34,19 @@ class TestIni2PO(object):
         result = ini2po.run_converter(input_file, output_file, template_file)
         assert result == 0
         assert output_file.getvalue() == ''
+
+    def test_convert_simple(self):
+        """Check the simplest case of converting a translation."""
+        input_source = """[section]
+key=value
+"""
+        expected_output = """#: [section]key
+msgid "value"
+msgstr ""
+"""
+        output = self._convert(input_source)
+        assert expected_output in output
+        assert "extracted from " in output
 
 
 class TestIni2POCommand(test_convert.TestConvertCommand, TestIni2PO):
