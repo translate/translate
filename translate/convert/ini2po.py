13222d65c2bb537f0e87d9340f3bc385278e4257
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-24 21:34:27 +0100

    ini2po: Refactor setting of extraction message

--- translate/convert/ini2po.py
+++ translate/convert/ini2po.py
@@ -41,6 +41,7 @@ class ini2po(object):
         self.blank_msgstr = blank_msgstr
         self.duplicate_style = duplicate_style
 
+        self.extraction_msg = None
         self.output_file = output_file
         self.source_store = self.SourceStoreClass(input_file, dialect=dialect)
         self.target_store = self.TargetStoreClass()
@@ -60,19 +61,16 @@ class ini2po(object):
 
     def convert_store(self):
         """Convert a single source format file to a target format file."""
-        output_header = self.target_store.header()
-        output_header.addnote("extracted from %s" % self.source_store.filename,
-                              "developer")
+        self.extraction_msg = "extracted from %s" % self.source_store.filename
 
         for source_unit in self.source_store.units:
             self.target_store.addunit(self.convert_unit(source_unit))
 
     def merge_stores(self):
         """Convert two source format files to a target format file."""
-        output_header = self.target_store.header()
-        note = "extracted from %s, %s" % (self.template_store.filename,
-                                          self.source_store.filename)
-        output_header.addnote(note, "developer")
+        self.extraction_msg = ("extracted from %s, %s" %
+                               (self.template_store.filename,
+                                self.source_store.filename))
 
         self.source_store.makeindex()
         for template_unit in self.template_store.units:
@@ -93,6 +91,10 @@ class ini2po(object):
             self.convert_store()
         else:
             self.merge_stores()
+
+        if self.extraction_msg:
+            self.target_store.header().addnote(self.extraction_msg,
+                                               "developer")
 
         self.target_store.removeduplicates(self.duplicate_style)
 
