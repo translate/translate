cd2ef7206d14239c4afb060027ccea8aa758d6e3
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-25 15:12:25 +0100

    Tests: Cleanup a bit po2tiki tests

--- translate/convert/test_po2tiki.py
+++ translate/convert/test_po2tiki.py
@@ -4,15 +4,15 @@
 # Author: Wil Clouser <wclouser@mozilla.com>
 # Date: 2008-12-01
 
-from io import BytesIO
-
 from translate.convert import po2tiki, test_convert
+from translate.misc import wStringIO
 
 
 class TestPo2Tiki(object):
 
-    def test_convertpo(self):
-        inputfile = b"""
+    def test_convert(self):
+        """Check converting simple file."""
+        input_file = """
 #: translated
 msgid "zero_source"
 msgstr "zero_target"
@@ -20,12 +20,10 @@ msgstr "zero_target"
 #: unused
 msgid "one_source"
 msgstr "one_target"
-        """
-        outputfile = BytesIO()
-        po2tiki.convertpo(inputfile, outputfile)
-
-        output = outputfile.getvalue().decode('utf-8')
-
+"""
+        output_file = wStringIO.StringIO()
+        po2tiki.convertpo(input_file, output_file)
+        output = output_file.getvalue().decode('utf-8')
         assert '"one_source" => "one_target",' in output
         assert '"zero_source" => "zero_target",' in output
 
