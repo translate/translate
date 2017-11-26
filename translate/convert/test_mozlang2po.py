60c43aae1da84c04966c0fe18c00b9ca6ad5df82
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-26 01:18:25 +0100

    Tests: Refactor helpers

--- translate/convert/test_mozlang2po.py
+++ translate/convert/test_mozlang2po.py
@@ -11,16 +11,25 @@ class TestLang2PO(object):
 
     ConverterClass = mozlang2po.lang2po
 
-    def _convert_to_store(self, source):
-        """helper that converts .lang source to po source without requiring files"""
-        inputfile = wStringIO.StringIO(source)
+    def _convert_to_store(self, input_string, template_string=None,
+                          blank_msgstr=False, duplicate_style="msgctxt",
+                          encoding="utf-8", success_expected=True):
+        """Helper that converts to target format store without using files."""
+        input_file = wStringIO.StringIO(input_string)
         output_file = wStringIO.StringIO()
-        convertor = self.ConverterClass(inputfile, output_file)
-        assert convertor.run() == 1
-        return convertor.target_store
+        template_file = None
+        if template_string:
+            template_file = wStringIO.StringIO(template_string)
+        expected_result = 1 if success_expected else 0
+        converter = self.ConverterClass(input_file, output_file, template_file,
+                                        blank_msgstr, duplicate_style,
+                                        encoding)
+        assert converter.run() == expected_result
+        return converter.target_store
 
     def _convert_to_string(self, input_string, template_string=None,
-                           success_expected=True):
+                           blank_msgstr=False, duplicate_style="msgctxt",
+                           encoding="utf-8", success_expected=True):
         """Helper that converts to target format string without using files."""
         input_file = wStringIO.StringIO(input_string)
         output_file = wStringIO.StringIO()
@@ -29,15 +38,19 @@ class TestLang2PO(object):
             template_file = wStringIO.StringIO(template_string)
         expected_result = 1 if success_expected else 0
         result = mozlang2po.run_converter(input_file, output_file,
-                                          template_file)
+                                          template_file, blank_msgstr,
+                                          duplicate_style, encoding)
         assert result == expected_result
         return output_file.getvalue().decode('utf-8')
 
-    def _single_element(self, pofile):
-        """checks that the pofile contains a single non-header element, and returns it"""
-        assert len(pofile.units) == 2
-        assert pofile.units[0].isheader()
-        return pofile.units[1]
+    def _single_element(self, po_store):
+        """Helper that returns first non-header unit.
+
+        It also checks that the store has a single non-header unit.
+        """
+        assert len(po_store.units) == 2
+        assert po_store.units[0].isheader()
+        return po_store.units[1]
 
     def test_convert_empty(self):
         """Check converting empty file returns no output."""
