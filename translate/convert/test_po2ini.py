200f8e6d40d550a307a1cd49da95062b95cf2533
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-27 14:29:34 +0100

    Tests: Refactor helper to make it more flexible

--- translate/convert/test_po2ini.py
+++ translate/convert/test_po2ini.py
@@ -14,18 +14,22 @@ importorskip("iniparse")
 
 class TestPO2Ini(object):
 
-    def _convert(self, po_input_source, format_template_source=None,
-                 include_fuzzy=False, dialect="default"):
-        """Helper that converts PO to format without files."""
-        input_file = wStringIO.StringIO(po_input_source)
+    ConverterClass = po2ini.po2ini
+
+    def _convert(self, input_string, template_string=None, include_fuzzy=False,
+                 output_threshold=None, dialect="default",
+                 success_expected=True):
+        """Helper that converts to target format without using files."""
+        input_file = wStringIO.StringIO(input_string)
         output_file = wStringIO.StringIO()
         template_file = None
-        if format_template_source:
-            template_file = wStringIO.StringIO(format_template_source)
-        result = po2ini.run_converter(input_file, output_file, template_file,
-                                      includefuzzy=include_fuzzy,
-                                      dialect=dialect)
-        assert result == 1
+        if template_string:
+            template_file = wStringIO.StringIO(template_string)
+        expected_result = 1 if success_expected else 0
+        converter = self.ConverterClass(input_file, output_file, template_file,
+                                        include_fuzzy, output_threshold,
+                                        dialect)
+        assert converter.run() == expected_result
         return None, output_file
 
     def _convert_to_string(self, *args, **kwargs):
