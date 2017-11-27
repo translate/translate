424abdcf5d43783bdb0e7cd335df8ff40bc3b6ff
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-11-27 16:07:10 +0100

    Tests: Rename variables and simplify

--- translate/convert/test_po2mozlang.py
+++ translate/convert/test_po2mozlang.py
@@ -7,9 +7,9 @@ from translate.storage import po
 
 class TestPO2Lang(object):
 
-    def po2lang(self, posource):
+    def po2lang(self, input_string):
         """helper that converts po source to .lang source without requiring files"""
-        inputfile = wStringIO.StringIO(posource)
+        inputfile = wStringIO.StringIO(input_string)
         inputpo = po.pofile(inputfile)
         convertor = po2mozlang.po2lang(mark_active=False)
         outputlang = convertor.convertstore(inputpo)
@@ -17,31 +17,27 @@ class TestPO2Lang(object):
 
     def test_simple(self):
         """check the simplest case of merging a translation"""
-        posource = '''#: prop\nmsgid "Source"\nmsgstr "Target"\n'''
-        prop_expected = ''';Source\nTarget\n\n\n'''
-        prop_result = self.po2lang(posource)
-        assert prop_result == prop_expected
+        input_string = '''#: prop\nmsgid "Source"\nmsgstr "Target"\n'''
+        expected_output = ''';Source\nTarget\n\n\n'''
+        assert expected_output == self.po2lang(input_string)
 
     def test_comment(self):
         """Simple # comments"""
-        posource = '''#. Comment\n#: prop\nmsgid "Source"\nmsgstr "Target"\n'''
-        prop_expected = '''# Comment\n;Source\nTarget\n\n\n'''
-        prop_result = self.po2lang(posource)
-        assert prop_result == prop_expected
+        input_string = '''#. Comment\n#: prop\nmsgid "Source"\nmsgstr "Target"\n'''
+        expected_output = '''# Comment\n;Source\nTarget\n\n\n'''
+        assert expected_output == self.po2lang(input_string)
 
     def test_fuzzy(self):
         """What happens with a fuzzy string"""
-        posource = '''#. Comment\n#: prop\n#, fuzzy\nmsgid "Source"\nmsgstr "Target"\n'''
-        prop_expected = '''# Comment\n;Source\nSource\n\n\n'''
-        prop_result = self.po2lang(posource)
-        assert prop_result == prop_expected
+        input_string = '''#. Comment\n#: prop\n#, fuzzy\nmsgid "Source"\nmsgstr "Target"\n'''
+        expected_output = '''# Comment\n;Source\nSource\n\n\n'''
+        assert expected_output == self.po2lang(input_string)
 
     def test_ok_marker(self):
         """The {ok} marker"""
-        posource = '''#: prop\nmsgid "Same"\nmsgstr "Same"\n'''
-        prop_expected = ''';Same\nSame {ok}\n\n\n'''
-        prop_result = self.po2lang(posource)
-        assert prop_result == prop_expected
+        input_string = '''#: prop\nmsgid "Same"\nmsgstr "Same"\n'''
+        expected_output = ''';Same\nSame {ok}\n\n\n'''
+        assert expected_output == self.po2lang(input_string)
 
 
 class TestPO2LangCommand(test_convert.TestConvertCommand, TestPO2Lang):
