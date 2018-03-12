e8ba80e8e550f1b73c5aeb8e0fd78c464e1d3cbf
Michal Čihař <michal@cihar.com>
2018-03-12 10:45:26 +0100

    Windows: replace os.path.samefile call
    The os.path.samefile is available on Unix only, workaround it by testing
    that absolute paths are same.

--- translate/storage/versioncontrol/test_svn.py
+++ translate/storage/versioncontrol/test_svn.py
@@ -28,6 +28,8 @@ class TestSVN(HelperTest):
         file_path = os.path.join(self.co_path, "test1.txt")
         o.add(os.path.join(file_path))
         o = get_versioned_object(file_path)
-        assert os.path.samefile(o.location_abs, file_path)
+
+        # The samefile is available on Unix only, compare path only
+        assert os.path.abspath(o.location_abs) == os.path.abspath(file_path)
 
         assert o.getcleanfile().decode('utf-8') == "First file\n"
