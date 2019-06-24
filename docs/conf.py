92dddf02dd60f7743b980f7e9853777446fda064
Leandro Regueiro <leandro.regueiro@gmail.com>
2019-06-24 13:10:09 +0200

    Docs: Update copyright dates

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Translate Toolkit'
-copyright = u'2002-2018, Translate'
+copyright = u'2002-2019, Translate'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
