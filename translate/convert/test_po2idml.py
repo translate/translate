039a85f42d881440de5944bca41168c65fb941b7
Michal Čihař <michal@cihar.com>
2020-05-14 09:24:20 +0200

    idml: Fix conversion and add tests

---
+++ translate/convert/test_po2idml.py
@@ -0,0 +1,23 @@
+import os
+
+from translate.convert import test_convert, po2idml
+
+
+class TestPo2IDMLCommand(test_convert.TestConvertCommand):
+    """Tests running actual po2idml commands on files"""
+
+    convertmodule = po2idml
+
+    def test_convert(self):
+        posource = """
+#: idPkg:Story[0]/%7B%7DStory[0]/%7B%7DXMLElement[0]/%7B%7DParagraphStyleRange[0]
+#: Stories/Story_mainmainmainmainmainmainmainmainmainmainmainu188.xml
+msgid "<g id=\"0\"><g id=\"1\">THE HEADLINE HERE</g></g>"
+msgstr "<g id=\"0\"><g id=\"1\">TADY JE NADPIS</g></g>"
+"""
+        self.create_testfile("simple.po", posource)
+        self.run_command(
+            i="simple.po",
+            o="simple.idml",
+            template=os.path.join(os.path.dirname(__file__), "test.idml"),
+        )
