affa7dd20f865f492ad5038bf21605a73b6d9c97
Michal Čihař <michal@cihar.com>
2020-08-04 13:23:02 +0200

    json: Factor out dictionary serialization
    Move it to the base class so that it can be reused by other storages.

--- translate/storage/base.py
+++ translate/storage/base.py
@@ -887,3 +887,34 @@ class TranslationStore:
         :rtype: string
         """
         return "id"
+
+
+class DictUnit(TranslationUnit):
+    def getvalue(self):
+        """Returns dictionary for serialization."""
+        raise NotImplementedError()
+
+
+class DictStore(TranslationStore):
+    def serialize_merge(self, d1, d2):
+        for k in d2:
+            if k in d1:
+                if isinstance(d1[k], dict) and isinstance(d2[k], dict):
+                    self.serialize_merge(d1[k], d2[k])
+                elif isinstance(d1[k], list) and isinstance(d2[k], tuple):
+                    if len(d1[k]) > d2[k][0]:
+                        d1[k][d2[k][0]].update(d2[k][1])
+                    else:
+                        d1[k].append(d2[k][1])
+                elif isinstance(d1[k], list) and isinstance(d2[k], list):
+                    d1[k].extend(d2[k])
+                else:
+                    d1[k] = d2[k]
+            elif isinstance(d2[k], tuple):
+                d1[k] = [d2[k][1]]
+            else:
+                d1[k] = d2[k]
+
+    def serialize_units(self, output):
+        for unit in self.unit_iter():
+            self.serialize_merge(output, unit.getvalue())
