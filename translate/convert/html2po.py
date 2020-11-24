d318bbebcec4b8776cef299cd17902907cf15c3e
Anders Kaplan <anders.kaplan@magasin1.se>
2020-11-24 21:59:32 +0100

    [html2po] Added --multifile=onefile option to produce a single po file instead of one per html file.

--- translate/convert/html2po.py
+++ translate/convert/html2po.py
@@ -24,6 +24,7 @@ for examples and usage instructions.
 """
 
 from translate.storage import html, po
+from translate.convert import convert
 
 
 class html2po:
@@ -34,16 +35,20 @@ class html2po:
         duplicatestyle="msgctxt",
         keepcomments=False,
     ):
-        """converts a html file to .po format"""
+        """Convert an html file to .po format."""
         thetargetfile = po.pofile()
+        self.convertfile_inner(inputfile, thetargetfile, keepcomments)
+        thetargetfile.removeduplicates(duplicatestyle)
+        return thetargetfile
+
+    def convertfile_inner(self, inputfile, outputstore, keepcomments):
+        """Extract translation units from an html file and add to a pofile object."""
         htmlparser = html.htmlfile(inputfile=inputfile)
         for htmlunit in htmlparser.units:
-            thepo = thetargetfile.addsourceunit(htmlunit.source)
+            thepo = outputstore.addsourceunit(htmlunit.source)
             thepo.addlocations(htmlunit.getlocations())
             if keepcomments:
                 thepo.addnote(htmlunit.getnotes(), "developer")
-        thetargetfile.removeduplicates(duplicatestyle)
-        return thetargetfile
 
 
 def converthtml(
@@ -68,26 +73,88 @@ def converthtml(
     return 1
 
 
-def main(argv=None):
-    from translate.convert import convert
+class Html2POOptionParser(convert.ConvertOptionParser):
+    def __init__(self):
+        formats = {
+            "html": ("po", self.convert),
+            "htm": ("po", self.convert),
+            "xhtml": ("po", self.convert),
+            None: ("po", self.convert),
+        }
+        super().__init__(formats, usetemplates=False, usepots=True, description=__doc__)
+        self.add_option(
+            "--keepcomments",
+            dest="keepcomments",
+            default=False,
+            action="store_true",
+            help="preserve html comments as translation notes in the output",
+        )
+        self.passthrough.append("keepcomments")
+        self.add_duplicates_option()
+        self.add_multifile_option()
+        self.passthrough.append("pot")
 
-    formats = {
-        "html": ("po", converthtml),
-        "htm": ("po", converthtml),
-        "xhtml": ("po", converthtml),
-        None: ("po", converthtml),
-    }
-    parser = convert.ConvertOptionParser(formats, usepots=True, description=__doc__)
-    parser.add_option(
-        "--keepcomments",
-        dest="keepcomments",
-        default=False,
-        action="store_true",
-        help="preserve html comments as translation notes in the output",
-    )
-    parser.passthrough.append("keepcomments")
-    parser.add_duplicates_option()
-    parser.passthrough.append("pot")
+    def convert(
+        self,
+        inputfile,
+        outputfile,
+        templates,
+        pot=False,
+        duplicatestyle="msgctxt",
+        multifilestyle="single",
+        keepcomments=False,
+    ):
+        """Extract translation units from one html file."""
+        convertor = html2po()
+        if hasattr(self, "outputstore"):
+            convertor.convertfile_inner(inputfile, self.outputstore, keepcomments)
+        else:
+            outputstore = convertor.convertfile(
+                inputfile,
+                getattr(inputfile, "name", "unknown"),
+                duplicatestyle=duplicatestyle,
+                keepcomments=keepcomments,
+            )
+            outputstore.serialize(outputfile)
+        return 1
+
+    def recursiveprocess(self, options):
+        """Recurse through directories and process files. (override)"""
+        if options.multifilestyle == "onefile":
+            self.outputstore = po.pofile()
+            super().recursiveprocess(options)
+            if not self.outputstore.isempty():
+                self.outputstore.removeduplicates(options.duplicatestyle)
+                outputfile = super().openoutputfile(options, options.output)
+                self.outputstore.serialize(outputfile)
+                if options.output:
+                    outputfile.close()
+        else:
+            super().recursiveprocess(options)
+
+    def isrecursive(self, fileoption, filepurpose="input"):
+        """Check if fileoption is a recursive file. (override)"""
+        if hasattr(self, "outputstore") and filepurpose == "output":
+            return True
+        return super().isrecursive(fileoption, filepurpose=filepurpose)
+
+    def checkoutputsubdir(self, options, subdir):
+        """Check if subdir under options.output needs to be created,
+        creates if neccessary. Do nothing if in single-output-file mode. (override)
+        """
+        if hasattr(self, "outputstore"):
+            return
+        super().checkoutputsubdir(options, subdir)
+
+    def openoutputfile(self, options, fulloutputpath):
+        """Open the output file, or do nothing if in single-output-file mode. (override)"""
+        if hasattr(self, "outputstore"):
+            return None
+        return super().openoutputfile(options, fulloutputpath)
+
+
+def main(argv=None):
+    parser = Html2POOptionParser()
     parser.run(argv)
 
 
