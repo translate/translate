186790492fe1bd50ecf9e99e5a5b22f60d75b079
Simon Urli <simon.urli@xwiki.com>
2020-12-16 09:40:00 +0100

    Update translate/storage/test_properties.py
    Co-authored-by: Michal Čihař <michal@cihar.com>

--- translate/storage/test_properties.py
+++ translate/storage/test_properties.py
@@ -638,7 +638,8 @@ job.log.begin=Starting job of type [{0}]
             == """# This is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as
 # published by the Free Software Foundation; either version 2.1 of
-# the License, or (at your option) any later version.\n"""
+# the License, or (at your option) any later version.
+"""
         )
         propunit = propfile.units[1]
         assert propunit.name == "job.log.begin"
