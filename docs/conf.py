d8a25e8a51871ff2c5c4a392f4731e1c1d0ec21f
Michal Čihař <michal@cihar.com>
2021-08-19 08:45:08 +0200

    docs: Release notes for 3.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = "Translate Toolkit"
 copyright = "2002-2021, Translate"
 
 # The short X.Y version.
-version = "3.3.6"
+version = "3.4.0"
 
 # The full version, including alpha/beta/rc tags
 release = version
