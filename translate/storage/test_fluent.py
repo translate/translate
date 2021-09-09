6b460d7c6af1c1e66f98259c1b8a86d41890c300
Jack Grigg <thestr4d@gmail.com>
2021-09-10 00:32:31 +0100

    Fluent: Add extra newlines around resource comment in tests
    fluent.syntax renders resource comments with extra whitespace; we
    defer to it for Fluent file formatting.

--- translate/storage/test_fluent.py
+++ translate/storage/test_fluent.py
@@ -108,7 +108,9 @@ key = value
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 ### Localization for Server-side strings of Firefox Screenshots
+
 
 ## Global phrases shared across pages
 
