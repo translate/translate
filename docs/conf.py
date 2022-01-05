a2ae2c9c52197bbb726b5e3b971789b774099482
Michal Čihař <michal@cihar.com>
2022-01-05 11:29:37 +0100

    docs: Release 3.5.2

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,10 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Translate Toolkit"
-copyright = "2002-2021, Translate"
+copyright = "2002-2022, Translate"
 
 # The short X.Y version.
-version = "3.5.1"
+version = "3.5.2"
 
 # The full version, including alpha/beta/rc tags
 release = version
