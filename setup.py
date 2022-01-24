a8b12c035b5d53e4162b99127f57f4ebec55081a
Michal Čihař <michal@cihar.com>
2022-01-24 14:05:40 +0100

    setup: Ipdate classifiers to include Python 3.10

--- setup.py
+++ setup.py
@@ -184,11 +184,14 @@ classifiers = [
     "Operating System :: MacOS :: MacOS X",
     "Operating System :: Unix",
     "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
     "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Software Development :: Internationalization",
     "Topic :: Software Development :: Localization",
 ]
 
