873d7f089ac353aac433a1366b57e4895bac0863
Michal Čihař <michal@cihar.com>
2022-12-08 12:41:49 +0100

    tests: Avoid need to overwrite version in test for each release

--- tests/cli/test_pofilter_manpage.sh
+++ tests/cli/test_pofilter_manpage.sh
@@ -2,5 +2,5 @@
 
 source $(dirname $0)/test.inc.sh
 
-python $PYTHON_ARGS $(which pofilter) --manpage
+python $PYTHON_ARGS $(which pofilter) --manpage | sed -E 's/"Translate Toolkit [0-9.]+"/"Translate Toolkit @VERSION@"/g'
 check_results
