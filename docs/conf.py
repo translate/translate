ad85964ddfae2378db9130f8bb5d5c81aa7d3ca9
Fabian Egli <fabianegli@users.noreply.github.com>
2023-01-13 21:41:50 +0100

    docs: update the year in the copyright footer

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Translate Toolkit"
-copyright = "2002-2022, Translate"
+copyright = "2002-2023, Translate"
 
 # The short X.Y version.
 version = "3.8.1"
