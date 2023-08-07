a21f7845192b9d1932b8f18877d1cab42d4ef009
Michal Čihař <michal@cihar.com>
2023-08-07 19:49:43 +0200

    Update translate/storage/xliff.py

--- translate/storage/xliff.py
+++ translate/storage/xliff.py
@@ -526,7 +526,7 @@ class xliffunit(lisa.LISAunit):
                 if sourceFile is not None and lineNumber is not None:
                     locations.append(sourceFile[1] + ":" + lineNumber[1])
 
-        return [x for x in locations if x is not None]
+        return locations
 
     def createcontextgroup(self, name, contexts=None, purpose=None):
         """Add the context group to the trans-unit with contexts a list with
