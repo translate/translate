translate/translate

commit 2a9c3253a9e013d34ccf1a07984730f9cfff25f4

storage: make UnitId parsing more robust
- Gracefully handle non-numeric index parts
- Add tests for parsing
- Add __eq__ and __repr__ to UnitId class

translate/storage/base.py
@@ -952,17 +952,27 @@ class UnitId:
         for item in text.split(cls.KEY_SEPARATOR):
             bracepos = item.find("[")
             endbracepos = item.find("]")
-            if bracepos != -1 and endbracepos != -1:
+            while (
+                (bracepos := item.find("[")) != -1
+                and (endbracepos := item.find("]")) != -1
+                and item[bracepos + 1 : endbracepos].isdigit()
+            ):
                 if bracepos > 0:
                     result.append(("key", item[:bracepos]))
                     item = item[bracepos:]
-                result.extend(
-                    ("index", int(pos))
-                    for pos in item.replace("[", " ").replace("]", " ").split()
-                )
-            else:
+                    endbracepos -= bracepos
+                    bracepos = 0
+                result.append(("index", int(item[bracepos + 1 : endbracepos])))
+                item = item[endbracepos + 1 :]
+            if item:
                 result.append(("key", item))
         return cls(result)
+
+    def __eq__(self, other):
+        return self.parts == other.parts
+
+    def __repr__(self):
+        return f"<UnitId:{self.parts}>"
 
 
 class DictUnit(TranslationUnit):

tests/translate/storage/test_jsonl10n.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 
-from pytest import raises
+from pytest import mark, raises
 
 from translate.misc.multistring import multistring
 from translate.storage import base, jsonl10n
@@ -667,6 +667,31 @@ class TestJSONNestedResourceStore(test_monolingual.TestMonolingualUnit):
         newstore.addunit(store.units[0])
 
         assert bytes(newstore).decode() == jsontext
+
+    @mark.parametrize(
+        ("id_string", "expected"),
+        [
+            ("[0]", [("index", 0)]),
+            ("test[0]", [("key", "test"), ("index", 0)]),
+            (
+                "test[0][1][2][3]",
+                [
+                    ("key", "test"),
+                    ("index", 0),
+                    ("index", 1),
+                    ("index", 2),
+                    ("index", 3),
+                ],
+            ),
+            ("[test]selection", [("key", "[test]selection")]),
+            ("[test][0]selection", [("key", "[test][0]selection")]),
+            ("[0][test]selection", [("index", 0), ("key", "[test]selection")]),
+        ],
+    )
+    def test_from_string(self, id_string, expected):
+        id_class = self.StoreClass.UnitClass.IdClass
+        from_string = id_class.from_string
+        assert from_string(id_string) == id_class(expected)
 
 
 class TestWebExtensionUnit(test_monolingual.TestMonolingualUnit):

commit ae47dc8e849c6c0e9035660437dc1e86454822b1

storage: fix parsing blank unit id
This is a regression in 2a9c3253a9e013d34ccf1a07984730f9cfff25f4.

translate/storage/base.py
@@ -966,6 +966,8 @@ class UnitId:
                 item = item[endbracepos + 1 :]
             if item:
                 result.append(("key", item))
+        if not result:
+            result.append(("key", ""))
         return cls(result)
 
     def __eq__(self, other):

tests/translate/storage/test_jsonl10n.py
@@ -686,6 +686,7 @@ class TestJSONNestedResourceStore(test_monolingual.TestMonolingualUnit):
             ("[test]selection", [("key", "[test]selection")]),
             ("[test][0]selection", [("key", "[test][0]selection")]),
             ("[0][test]selection", [("index", 0), ("key", "[test]selection")]),
+            ("", [("key", "")]),
         ],
     )
     def test_from_string(self, id_string, expected):