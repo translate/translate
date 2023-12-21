530740607f3c3ef7b64d8ea467d6ef5e948733b5
Michal Čihař <michal@cihar.com>
2023-12-21 09:24:15 +0100

    Releasing 3.12.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Translate Toolkit"
 copyright = "2002-2023, Translate"
 
 # The short X.Y version.
-version = "3.11.1"
+version = "3.12.0"
 
 # The full version, including alpha/beta/rc tags
 release = version
