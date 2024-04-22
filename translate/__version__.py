7c121d6500dd44b58399f4757b25bc9c0779d767
Michal Čihař <michal@cihar.com>
2024-04-22 11:02:44 +0200

    release 3.13.0

--- translate/__version__.py
+++ translate/__version__.py
@@ -18,7 +18,7 @@
 
 """This file contains the version of the Translate Toolkit."""
 
-ver = (3, 12, 2)
+ver = (3, 13, 0)
 """Machine readable version number. Used by tools that need to adjust code
 paths based on a Translate Toolkit release number."""
 
