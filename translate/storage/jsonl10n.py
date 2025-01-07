82af1d2f510a55309f4602013f83fe2e5225fc50
Michal Čihař <michal@cihar.com>
2025-01-07 08:54:16 +0100

    chore(json): inherit from FlatJsonUnit instead of overiding IdClass

--- translate/storage/jsonl10n.py
+++ translate/storage/jsonl10n.py
@@ -255,9 +255,7 @@ class JsonNestedFile(JsonFile):
     UnitClass = JsonNestedUnit
 
 
-class WebExtensionJsonUnit(BaseJsonUnit):
-    IdClass = FlatUnitId
-
+class WebExtensionJsonUnit(FlatJsonUnit):
     def storevalues(self, output):
         value = {"message": self.target}
         if self.notes:
@@ -812,9 +810,8 @@ class GoI18NV2JsonFile(JsonFile):
             yield unit
 
 
-class ARBJsonUnit(BaseJsonUnit):
+class ARBJsonUnit(FlatJsonUnit):
     ID_FORMAT = "{}"
-    IdClass = FlatUnitId
 
     def __init__(
         self,
