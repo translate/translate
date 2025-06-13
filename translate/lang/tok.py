24da4ad8decd8a1b3348b6d9a651b427df7f12b4
jnpojuwan <jptavaresreis@gmail.com>
2025-06-14 00:46:51 +0100

    Initialised Toki Pona settings

---
+++ translate/lang/tok.py
@@ -0,0 +1,15 @@
+"""
+This module represents the Toki Pona language.
+
+.. seealso:: :wp:`Toki_Pona`
+"""
+
+from translate.lang import common
+
+
+class tok(common.Common):
+    """This class represents Toki Pona."""
+
+    ignoretests = {
+        "all": ["simplecaps", "startcaps"]
+    }
