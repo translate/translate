ea3d1f9b734e9ddb53aa26b9b140fa4e70f96330
Michal Čihař <michal@cihar.com>
2025-06-16 08:34:16 +0200

    chore: release 3.15.5

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Translate Toolkit"
 copyright = "Translate Toolkit authors"
 
 # The short X.Y version.
-version = "3.15.4"
+version = "3.15.5"
 
 # The full version, including alpha/beta/rc tags
 release = version
