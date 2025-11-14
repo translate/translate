39069970ac6b43694cc3c6d2c96647f991f94012
Michal Čihař <michal@cihar.com>
2025-11-14 20:17:23 +0100

    fix(misc): implement pop for cidict
    It is used in the poheader and is broken for some time (perhaps since
    Python 3?).

--- tests/translate/misc/test_dictutils.py
+++ tests/translate/misc/test_dictutils.py
@@ -1,3 +1,5 @@
+import pytest
+
 from translate.misc import dictutils
 
 
@@ -8,3 +10,35 @@ def test_cidict_has_key():
     assert "Lower" in cid
     assert "LOWER" in cid
     assert "upper" not in cid
+
+
+def test_cidict_pop():
+    cid = dictutils.cidict()
+    cid["lower"] = "lowercase"
+    assert cid.pop("LOWER")
+    with pytest.raises(KeyError):
+        assert cid.pop("upper")
+
+
+def test_cidict_getitem():
+    cid = dictutils.cidict()
+    cid["lower"] = "lowercase"
+    assert cid["lower"] == "lowercase"
+    assert cid["LOWER"] == "lowercase"
+    assert cid["Lower"] == "lowercase"
+    with pytest.raises(KeyError):
+        assert cid["upper"]
+
+
+def test_cidict_setitem():
+    cid = dictutils.cidict()
+    cid["lower"] = "lowercase"
+    cid["Lower"] = "other"
+    assert cid["lower"] == "other"
+
+
+def test_cidict_delitem():
+    cid = dictutils.cidict()
+    cid["lower"] = "lowercase"
+    del cid["Lower"]
+    assert "lower" not in cid
