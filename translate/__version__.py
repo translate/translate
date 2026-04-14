386b89398034ea08b00082a77866b7e4021ea6a6
Michal Čihař <michal@cihar.com>
2026-04-14 12:40:00 +0200

    chore: release 3.19.5

--- translate/__version__.py
+++ translate/__version__.py
@@ -18,7 +18,7 @@
 
 """This file contains the version of the Translate Toolkit."""
 
-ver = (3, 19, 4)
+ver = (3, 19, 5)
 """Machine readable version number. Used by tools that need to adjust code
 paths based on a Translate Toolkit release number."""
 
