3fc5809e7e5c8630208ce43a9589062a28667056
Michal Čihař <michal@cihar.com>
2026-07-30 14:07:02 +0200

    chore: bump version to 3.19.16

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "Translate Toolkit"
 copyright = "Translate Toolkit authors"
 
 # The short X.Y version.
-version = "3.19.15"
+version = "3.19.16"
 
 # The full version, including alpha/beta/rc tags
 release = version
